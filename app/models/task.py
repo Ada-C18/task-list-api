76048c3fd0a0bfd98785cffb7b594d46986211a1
Miranda <miranda@Jays-MacBook-Pro.local>
2022-11-02 14:33:39 -0700

    created the Task Model(table), and registered the blueprint

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,9 @@
 from app import db
 
-
+# create the table with attributes
+# after created, run flask db init, flask db migrate, flask db upgrade
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime)
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,7 @@
 from flask import Blueprint
+from app import db
+from app.models.task import Task
+
+
+task_bp = Blueprint("Task_bp", __name__, url_prefix="tasks")
+
