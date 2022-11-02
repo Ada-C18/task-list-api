d02158d21ea4c0318a356001c2acc2f4e070a0cb
Tasha <tashachang2@gmail.com>
2022-11-02 14:11:54 -0700

    Created to_dict function within task model

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, make_response
+from app import db 
+from app.models.task import Task 
+
+task_list_bp = Blueprint("tasks", __name__, url_prefix = "/tasks")
+
+@task_list_bp.route("/tasks", methods=["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+    response = []
+    for task in tasks:
+        response.append({
+
+        })
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        abort(make_response({"message": f"task {task_id} invalid"}, 400))
+
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message": f"task {task_id} not found"}, 404))
+
+    return task
