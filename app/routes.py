75f1522b0ea77c5b90e3a145a34a86b0a097374a
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2022-11-02 15:04:04 -0700

    Post method with add_task func working

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, abort, make_response
+from app import db
+from app.models.task import Task
+
+task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+
+@task_bp.route("", methods=["POST"])
+def add_task():
+    request_body = request.get_json()
+    
+
+    if "completed_at" not in request_body:  
+        new_incomplete_task = Task(
+        title = request_body["title"],
+        description = request_body["description"])
+
+        db.session.add(new_incomplete_task)
+        db.session.commit()
+    
+        new_incomplete_task_dict = {"id": new_incomplete_task.task_id,
+        "title": new_incomplete_task.title,
+        "description": new_incomplete_task.description,
+        "is_complete": False}
+
+        task_dict = {"task": new_incomplete_task_dict}
+
+    return jsonify(task_dict), 201
