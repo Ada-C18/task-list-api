06e6a1ce10ce9144b936b7a0fe3a1524309cb97b
Lynn <lynn@jansheski.com>
2022-11-02 15:11:30 -0700

    created route create_one_task in app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -12,3 +12,18 @@ def get_all_tasks():
         response.append(task.to_dict())
     
     return jsonify(response), 200
+
+@tasks_bp.route("", methods=["POST"])
+def create_one_task():
+    request_body = request.get_json()
+
+    new_task = Task(
+        title=request_body["title"],
+        description=request_body["description"],
+        completed_at=request_body["completed_at"]
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify({"msg": f"{new_task.title} added to task list."}), 201
