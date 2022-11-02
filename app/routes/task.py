a507c40965103fe87a305546776cc15fc85fec95
Marilyn Thai <marilynthai15@gmail.com>
2022-11-02 16:14:39 -0700

    made changes to task model to have nullable value in completed at column. added conditional to get request to return false for is_complete when eual to null or none

--- app/routes/task.py
+++ app/routes/task.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify, make_response, request, abort,Response
 from app import db
 # from app.models import task
 from app.models.task import Task
+import json
 
 task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
 
@@ -36,7 +37,7 @@ def get_all_tasks():
 
     title_query = request.args.get("title")
     description_query = request.args.get("description")
-    completed_at_query = request.args.get("completed at")
+    completed_at_query = request.args.get("completed_at")
 
 
     if title_query:
@@ -44,27 +45,60 @@ def get_all_tasks():
     elif description_query:
         tasks = Task.query.filter_by(description = description_query)
     elif completed_at_query:
-        tasks = Task.query.filter_by(completed_at = completed_at_query)
+        tasks = Task.query.filter_by(completed_At = completed_at_query)
     else:
         tasks = Task.query.all()
     response = []
     for task in tasks:
-        task_dict = {
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "completed at": task.completed_at
-        }
-        response.append(task_dict)
+        if task.completed_at is None:
+            task_dict = {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": False
+            }
+            response.append(task_dict)
     return jsonify(response), 200
 
 @task_bp.route("/<task_id>", methods=["GET"])
 def get_one_bike(task_id):
     task = validate_task_id(task_id)
-    task_dict = jsonify({
-        "id": task.task_id,
-        "title": task.title,
-        "description": task.description,
-        "completed at":type.size
-    }) 
-    return bike_dict
+    if task.completed_at is None:
+        task_dict = {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": False
+}
+    else:
+        task_dict = {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": True}
+    
+    return jsonify({"task":task_dict})
+    # return json.dumps(task_dict)
+
+@task_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task_id(task_id)
+    request_body = request.get_json()
+
+    if "title" not in request_body or \
+        "description" not in request_body:
+                return jsonify({"message":"Request must include title, description, and is complete"}),400
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    task_dict = {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": False}
+
+    db.session.commit()
+    return jsonify({"task":task_dict}),200
+
+
