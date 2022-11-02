db021d8ed8bc60b3144a17e2d7e9750633074197
Milena Souto <yamadasouto@gmail.com>
2022-11-02 16:57:22 -0700

    Add method to Task class to convert task object to dict

--- app/models/task.py
+++ app/models/task.py
@@ -7,3 +7,15 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default=None, nullable=True)
+
+    # converts Task object into Dict
+    def to_dict(self):
+        task_dict = {
+            "task": {
+                "id": self.task_id,
+                "title": self.title,
+                "description": self.description,
+                "is_complete": False}
+        }
+
+        return task_dict
