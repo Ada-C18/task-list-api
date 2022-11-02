b0f164fd30ed414dc404a6a3d490437f85e765f7
Margaret <y.margaret.lu+github@gmail.com>
2022-11-02 14:19:07 -0700

    added basic CRUD routes

--- app/models/task.py
+++ app/models/task.py
@@ -7,3 +7,11 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default=None, nullable=True)
+
+    def dictionfy(self):
+        return{
+            "id":self.task_id,
+            "title":self.title,
+            "description":self.description,
+            "is_complete": True if self.completed_at is not None else False
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,70 @@
 from flask import Blueprint, jsonify, abort, make_response, request
 from app.models.task import Task
 from app import db
+
+task_bp = Blueprint("task_bp",__name__,url_prefix="/task")
+
+@task_bp.route("", methods=['POST'])
+def make_new_task():
+    response_body = request.get_json()
+    new_task = Task(
+        title=response_body["title"],
+        description=response_body["description"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify(f"task:{new_task.dictionfy()}"),201)
+
+
+
+@task_bp.route("", methods=['GET'])
+def get_all_tasks():
+    return_list=[]
+    tasks = Task.query.all()
+    for task in tasks:
+        return_list.append(task.dictionfy())
+    return make_response(jsonify(return_list),200)
+
+
+@task_bp.route("/<task_id>", methods=['GET'])
+def get_one_task(task_id):
+    task = validate_model(Task,task_id)
+    return make_response(task.dictionfy(),200)
+
+
+@task_bp.route("/<task_id>", methods=['PUT'])
+def update_one_task(task_id):
+    response_body = request.get_json()
+    task = Task.query.get(task_id)
+
+    task.title = response_body["title"]
+    task.description = response_body["description"]
+
+    db.session.commit()
+
+    return make_response("OK",200)
+
+
+@task_bp.route("/<task_id>", methods=['DELETE'])
+def delete_a_task(task_id):
+    task = validate_model(Task,task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return make_response("OK",200)
+
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"details":"Invalid data"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
