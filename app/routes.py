3921a777e0330218d69fdb73670efb8b5e986591
Lynn <lynn@jansheski.com>
2022-11-02 14:59:13 -0700

    created route get_all_tasks in app/routes.py, added function to Task model to return response body as dict in app/models/task.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    response = []
+    for task in tasks:
+        response.append(task.to_dict())
+    
+    return jsonify(response), 200
