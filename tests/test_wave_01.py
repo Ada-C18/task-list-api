5d52111e90436e1a9c531bf15cdcbf6bea5e014a
Ariel <gabriellaw15@gmail.com>
2022-11-02 13:44:13 -0700

    Updated test_wave_01 to complete tests

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -60,10 +60,11 @@ def test_get_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
+    assert "message" in response_body
 
 
 @pytest.mark.skip(reason="No way to test this feature yet")
@@ -131,11 +132,11 @@ def test_update_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
+    assert "message" in response_body
 
 @pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task(client, one_task):
@@ -160,13 +161,13 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
+    assert Task.query.all() == []
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
-    assert Task.query.all() == []
+    assert "message" in response_body
+    
 
 
 @pytest.mark.skip(reason="No way to test this feature yet")
