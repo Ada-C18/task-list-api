23608e6b18c6cc57a6ac5ffb0a0fe4d2f802aa21
Yangweiyi Li <lywy.amy@gmail.com>
2022-11-02 14:17:39 -0700

    implement POST

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def add_one_task():
+    request_body = request.get_json()
+
+    new_task = Task(
+        title=request_body["title"],
+        description=request_body["description"],
+        completed_at=None
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+    
+    is_complete = True if new_task.completed_at is not None else False
+    
+    response_body = {
+        "task": {
+            "id": new_task.task_id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": is_complete
+        }
+    }
+
+    return jsonify(response_body), 201
