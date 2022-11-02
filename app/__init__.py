9237690c8d5bf1691bb099ae93f5e94f3c59f471
Minh <minh.seikel@gmail.com>
2022-11-02 15:02:16 -0700

    register task blue print in app/__init__.py, adding get_all_tasks function to routes.py

--- app/__init__.py
+++ app/__init__.py
@@ -23,16 +23,15 @@ def create_app(test_config=None):
             "SQLALCHEMY_TEST_DATABASE_URI")
 
     # Import models here for Alembic setup
-    from app.models.task import Task
+    from app.models.task import Task     #importing model Task into our project. This line of code can be anywhere   
     from app.models.goal import Goal
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    from app.models.task import Task #importing model Breakfast into our project. This line of code can be anywhere
-    
-    # from .routes.task import task_bp
-    # app.register_blueprint(breakfast_bp)
+ 
+    from .routes import task_bp
+    app.register_blueprint(task_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,19 @@ from flask import Blueprint, jsonify, request, abort, make_response
 from app import db
 from app.models.task import Task
 
-task_bp = Blueprint("task", __name__, url_prefix="/task")
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 @task_bp.route('', methods=['GET'])
 def get_all_tasks():
-    pass
+    title_query_value = request.args.get("title")
+    if title_query_value is not None:
+        tasks = Task.query.filter_by(title=title_query_value)
+    
+    else:
+        tasks = Task.query.all()
+    
+    result = []
+    
+    for task in tasks:
+        result.append(task.to_dict())
+    
+    return jsonify(result), 200
