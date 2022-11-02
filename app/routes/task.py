7bf0992f41f47ce1db6eb45c9517336a7ad03fbc
masinakh <nakhjirimasi@gmail.com>
2022-11-02 14:40:58 -0700

    read all tasks routes and create models and finish setup parts

---
+++ app/routes/task.py
@@ -0,0 +1,26 @@
+
+
+from app import db
+from flask import Blueprint,jsonify,request,abort,make_response
+from app.models.task import Task
+   
+
+
+task_bp = Blueprint("task",__name__,url_prefix = "/tasks")
+
+@task_bp.route('',methods = ["GET"])
+def get_all_tasks():
+    title_query_value = request.args.get("title")
+
+    if title_query_value is not None:
+        tasks = Task.query.filter_by(title = title_query_value) 
+    else:
+        tasks = Task.query.all()
+
+    response = []
+    for task in tasks:
+        response.append(task.to_dict())
+
+    return jsonify(response),200  
+
+
