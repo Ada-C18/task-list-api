d9e9ccfc837889182ba3b9946cae3c84ceed3da8
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2022-11-03 11:32:45 -0700

    refactored to combine dicitonary responses into a helper function

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,19 @@ from app.models.task import Task
 
 task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
 
+# helper function to structure request response json
+def make_task_dict(task):
+        task_dict = {"id": task.task_id,
+        "title": task.title,
+        "description": task.description}
+
+        if task.completed_at is None:
+            task_dict["is_complete"] = False
+        else:
+            task_dict["is_complete"] = True
+            task_dict["completed_at"] = task.completed_at
+        
+        return task_dict
 
 @task_bp.route("", methods=["POST"])
 def add_task():
@@ -13,22 +26,18 @@ def add_task():
         abort(make_response(jsonify({"details":"Invalid data"}), 400))
     
     else:
-        if "completed_at" not in request_body:  
-            new_incomplete_task = Task(
-            title = request_body["title"],
-            description = request_body["description"])
+        new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"])
 
-            db.session.add(new_incomplete_task)
-            db.session.commit()
-        
-            new_incomplete_task_dict = {"id": new_incomplete_task.task_id,
-            "title": new_incomplete_task.title,
-            "description": new_incomplete_task.description,
-            "is_complete": False}
+        if "completed_at" in request_body:
+            new_task.completed_at = request_body["completed_at"]
 
-        task_dict = {"task": new_incomplete_task_dict}
+        db.session.add(new_task)
+        db.session.commit()
 
-        return jsonify(task_dict), 201
+        new_task_dict = {"task": make_task_dict(new_task)}
+        return jsonify(new_task_dict), 201
 
 @task_bp.route("", methods=["GET"])
 def get_all_tasks():
@@ -36,18 +45,12 @@ def get_all_tasks():
 
     response = []
     for task in tasks:
-        task_dict = {
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description}
-        if not task.completed_at:
-            task_dict["is_complete"] = False
-        else:
-            task_dict["completed_at"] = task.completed_at
+        task_dict = make_task_dict(task)
         response.append(task_dict)
     
     return jsonify(response), 200
 
+#helper function to validate that task ids are valid
 def validate_id(task_id):
     matching_task = Task.query.get(task_id)
 
@@ -61,45 +64,25 @@ def validate_id(task_id):
 @task_bp.route("/<task_id>", methods = ["GET"])
 def get_one_task(task_id):
     selected_task = validate_id(task_id)
-
-    task_dict =  {"id": selected_task.task_id,
-        "title": selected_task.title,
-        "description": selected_task.description}
-    
-    if not selected_task.completed_at:
-        task_dict["is_complete"] = False
-    else:
-        task_dict["completed_at"] = selected_task.completed_at
-    
+    task_dict = make_task_dict(selected_task)
     response_dict = {"task": task_dict}
-
     return response_dict, 200
+
 
 @task_bp.route("/<task_id>", methods = ["PUT"])
 def update_task_with_new_vals(task_id):
     selected_task = validate_id(task_id)
 
     request_body = request.get_json()
-
     if "title" in request_body:
         selected_task.title = request_body["title"]
-    
     if "description" in request_body:
         selected_task.description = request_body["description"]
     
     db.session.commit()
     
-    task_dict =  {"id": selected_task.task_id,
-        "title": selected_task.title,
-        "description": selected_task.description}
-    
-    if not selected_task.completed_at:
-        task_dict["is_complete"] = False
-    else:
-        task_dict["completed_at"] = selected_task.completed_at
-    
+    task_dict = make_task_dict(selected_task)
     response_dict = {"task": task_dict}
-
     return response_dict, 200
 
 @task_bp.route("/<task_id>", methods = ["DELETE"])
