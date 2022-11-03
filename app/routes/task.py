1a5aabb29c2d8134ce2d6514fabe52e8e88cc68b
masinakh <nakhjirimasi@gmail.com>
2022-11-02 21:27:41 -0700

    test wave1 is complete

--- app/routes/task.py
+++ app/routes/task.py
@@ -18,8 +18,6 @@ def create_task():
         # completed_at = request_body['completed_at']
     )
 
-    
-
     db.session.add(new_task)
     db.session.commit()
 
@@ -39,6 +37,32 @@ def get_all_tasks():
     for task in tasks:
         response.append(task.to_dict())
 
-    return jsonify(response),200  
+    return make_response(jsonify(response),200) 
+
+
+@task_bp.route('/<task_id>', methods = ["GET"])
+def get_one_task(task_id):
+
+    chosen_task = get_task_from_id(task_id)
+
+    return make_response(jsonify({"task":chosen_task.to_dict()}),200)
+
+
+
+def get_task_from_id(task_id):
+    try:
+        task_id = int(task_id)
+    except ValueError:  
+        return abort(make_response({"msg":f"Invalid data type: {task_id}"}, 400 ))
+
+    chosen_task = Task.query.get(task_id)
+
+    
+    if chosen_task is None:
+        return abort(make_response({"msg": f" Could not find task item with id : {task_id}"} , 404 ))
+            
+   
+    return chosen_task    
+
 
 
