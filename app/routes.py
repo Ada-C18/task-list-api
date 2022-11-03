67dffd1112b028ffee93e8bf43ef3410483edbc2
Yael <yaelechols@gmail.com>
2022-11-03 14:41:22 -0700

    Creates and registers tasks blueprint

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,5 @@
-from flask import Blueprint
+from flask import Flask, Blueprint, jsonify, abort, make_response, request
+from models.task import Task
+from app import db
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
