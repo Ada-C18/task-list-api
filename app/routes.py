303828c33218b6963cf25683515664913c5e3195
Yangweiyi Li <lywy.amy@gmail.com>
2022-11-03 11:54:46 -0700

    implement PATCH for /task_id/mark_complete and /task_id/mark_incomplete, remove is_complete variable

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, abort, make_response
 from app import db
 from app.models.task import Task
+from datetime import datetime
 
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
@@ -26,14 +27,12 @@ def add_one_task():
 
         abort(make_response(jsonify(response_body), 400))
     
-    is_complete = True if new_task.completed_at is not None else False
-
     response_body = {
         "task": {
             "id": new_task.task_id,
             "title": new_task.title,
             "description": new_task.description,
-            "is_complete": is_complete
+            "is_complete": True if new_task.completed_at else False
         }
     }
 
@@ -54,14 +53,13 @@ def get_all_tasks():
     response_body = []
 
     for task in tasks:
-        is_complete = True if task.completed_at is not None else False
 
         response_body.append(
             {
                 "id": task.task_id,
                 "title":task.title,
                 "description": task.description,
-                "is_complete": is_complete
+                "is_complete": True if task.completed_at else False
             }
         )
     
@@ -72,14 +70,12 @@ def get_all_tasks():
 def get_one_task(task_id):
     task = validate_task(task_id)
 
-    is_complete = True if task.completed_at is not None else False
-
     response_body = {
         "task": {
             "id": task.task_id,
             "title": task.title,
             "description": task.description,
-            "is_complete": is_complete
+            "is_complete": True if task.completed_at else False
         }
     }
 
@@ -95,14 +91,12 @@ def update_one_task(task_id):
     task.title = request_body["title"]
     task.description = request_body["description"]
 
-    is_complete = True if task.completed_at is not None else False
-
     response_body = {
         "task": {
             "id": task.task_id,
             "title": task.title,
             "description": task.description,
-            "is_complete": is_complete
+            "is_complete": True if task.completed_at else False
         }
     }
     
@@ -124,6 +118,45 @@ def delete_one_task(task_id):
 
     return jsonify(response_body), 200
 
+
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def mark_one_task_as_complete(task_id):
+    task = validate_task(task_id)
+
+    task.completed_at = datetime.now()
+
+    db.session.commit()
+
+    response_body = {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": True if task.completed_at else False
+        }
+    }
+
+    return jsonify(response_body), 200
+
+
+@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def mark_one_task_as_incomplete(task_id):
+    task = validate_task(task_id)
+
+    task.completed_at = None
+
+    db.session.commit()
+
+    response_body = {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": True if task.completed_at else False
+        }
+    }
+
+    return jsonify(response_body), 200
 
 def validate_task(task_id):
     try:
