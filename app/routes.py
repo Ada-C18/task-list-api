50c1b5c3b0a43c0f7ab4e56b2b99b82177a11331
Yael <yaelechols@gmail.com>
2022-11-03 14:48:07 -0700

    Sets up POST and GET for tasks and GET, PUT, and DELETE for tasks/<task_id>

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,23 @@ from models.task import Task
 from app import db
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    pass
+
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+    pass
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def get_one_task():
+    pass
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task():
+    pass
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task():
+    pass
