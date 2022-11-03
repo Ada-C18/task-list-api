14dc85adc6e8ff87ec140e048e8258bb2e704bb9
Maxwell <maxpasqualis@gmail.com>
2022-11-03 12:50:56 -0700

    created function to return a Task as a dict

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,18 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def as_dict(self):
+        task_dict = {}
+        task_dict["id"] = self.id
+        task_dict["title"] = self.title
+        task_dict["description"] = self.description
+        if not self.completed_at:
+            task_dict["is_complete"] = False
+        else:
+            task_dict["completed_at"] = self.completed_at
+        return task_dict
