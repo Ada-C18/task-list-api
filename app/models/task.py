0013d038ad66b7080576476dc7f7c8c255e2336a
Lisa <elizabethpham726@gmail.com>
2022-11-03 14:24:41 -0700

    set up project with dbs and Task model

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,9 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id =           db.Column(db.Integer, primary_key=True)
+    task_title =        db.Column(db.String,    nullable=False)
+    task_description =  db.Column(db.String,    nullable=True)
+    task_completed_at = db.Column(db.DateTime,  nullable=True,  default=None)
+    task_is_completed = db.Column(db.Boolean,   nullable=False, default=False)
+
