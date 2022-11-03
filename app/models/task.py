c92b0b0f7650fdde87287e5c35010faef889b8f2
June Valentino <vintydesign@gmail.com>
2022-11-03 12:34:18 -0700

    refactored, passed three wave 1 tests

--- app/models/task.py
+++ app/models/task.py
@@ -8,3 +8,11 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def task_dictify(self):
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": True if self.completed_at is not None else False
+        }
--- app/routes.py
+++ app/routes.py
@@ -12,18 +12,51 @@ def create_task():
     '''
     request_body = request.get_json()
     new_task = Task(title=request_body["title"],
-                    description=request_body["description"])#,
+                    description=request_body["description"])
 
     db.session.add(new_task)
     db.session.commit()
 
-    response_body = {
-        "task": {
-            "id": new_task.task_id,
-            "title": new_task.title,
-            "description": new_task.description,
-            "is_complete": True if new_task.completed_at is not None else False
-        }
-    }
+    response_body = {"task": new_task.task_dictify()}
 
-    return make_response(response_body, 201)
+    return make_response(jsonify(response_body), 201)
+
+
+@tasks_bp.route("", methods=["GET"])
+def view_all_tasks():
+    '''
+    GET method - allows client to view all tasks
+    '''
+    tasks = Task.query.all()
+
+    request_body = []
+    for task in tasks:
+        request_body.append(task.task_dictify())
+
+    return jsonify(request_body), 200
+
+
+def validate_task(task_id):
+    '''
+    helper function - throws an error code for an invalid ID
+    '''
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response(jsonify({"msg": f"task with {task_id} is invalid"}), 400))
+    
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response(jsonify({"msg": f"task with {task_id} is not found"}), 404))
+    return task
+
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def view_one_task(task_id):
+    '''
+    GET method - allows client to view one task by ID
+    '''
+    task = validate_task(task_id)
+
+    return task.task_dictify()
