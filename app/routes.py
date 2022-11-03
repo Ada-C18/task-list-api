ef9bb011bb03f32a9570c2349182fa879eee699c
Laura Skomerza <lskomerza@gmail.com>
2022-11-03 16:56:28 -0700

    made wave 1 tests available, started add task post method and committing migrations

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, abort, make_response
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+
+def add_task():
+    request_body = request.get_json()
+
+    new_task = Task(
+        title=request_body["A Brand New Task"],
+        description=request_body["Test Description"],
+        completed_at=request_body["null"]
+
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"id": new_task.id}, 201
