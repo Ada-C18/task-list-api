f9148261a5763a7d5df22566c1821dc64751e44e
allcomputersarebad <allcomputersarebad@users.noreply.github.com>
2022-11-03 16:05:27 -0700

    Task description should be not nullable

--- app/models/task.py
+++ app/models/task.py
@@ -4,7 +4,7 @@ from app import db
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String)
+    description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, default=None)
 
     def to_dict(self):
