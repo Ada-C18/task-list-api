b98d68a8464aa28dec66593b62aaf1537a1fa5c0
LP <lp.passarelli.wilson@gmail.com>
2022-11-03 10:00:46 -0700

    added get route to get one task with a given id

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 from app.models.task import Task
 
 #make a blueprint
@@ -13,10 +13,24 @@ def get_all_tasks():
         response.append(task_dict)
     return jsonify(response), 200
 
-"""
-@task_bp.route("", methods = ["GET"])
-def get_one_task():
-    task
-    """
+
+@task_bp.route("/<task_id>", methods = ["GET"])
+def get_one_task(task_id):
+    task = validate_task(task_id)
+    task_dict = task.make_dict()
+    return make_response({"task": task_dict}, 200)
+    
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        response_str = f"Task {task_id} must be an integer"
+        abort(make_response({"message": response_str}, 400))
+    task = Task.query.get(task_id)
+    if not task:
+        response_str = f"Task {task_id} not found"
+        abort(make_response({"message": response_str}, 404))
+    return task
 
 
