4c43c50aa9da2998e7d91f2fc54804064e3a9438
allcomputersarebad <allcomputersarebad@users.noreply.github.com>
2022-11-03 15:45:49 -0700

    wave 1 complete

--- app/__init__.py
+++ app/__init__.py
@@ -33,4 +33,8 @@ def create_app(test_config=None):
 
     from app.models.task import Task
 
+    from .routes import task_bp
+
+    app.register_blueprint(task_bp)
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,64 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app import db
+from app.models.task import Task
+
+
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+
+
+@task_bp.route("", methods=["POST"])
+def post_task():
+    request_body = request.get_json()
+    new_task = Task(
+        title=request_body["title"], description=request_body["description"]
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    return {"task": new_task.to_dict()}, 201
+
+
+@task_bp.route("", methods=["GET"])
+def get_all_tasks():
+    return jsonify([t.to_dict() for t in Task.query.all()])
+
+
+@task_bp.route("/<task_id>", methods=["GET"])
+def get_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    return {"task": task.to_dict()}
+
+
+@task_bp.route("/<task_id>", methods=["PUT"])
+def put_task(task_id):
+    form_data = request.get_json()
+    task = Task.query.get_or_404(task_id)
+    task.title = form_data["title"]
+    task.description = form_data["description"]
+    db.session.commit()
+    return {"task": task.to_dict()}
+
+
+@task_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return {"details": f'Task {task.task_id} "{task.title}" successfully deleted'}
+
+
+from psycopg2.errors import InvalidTextRepresentation
+
+
+@task_bp.errorhandler(404)
+def handle_task_not_found(e):
+    return {"details": "Task not found"}, 404
+
+
+@task_bp.errorhandler(InvalidTextRepresentation)
+def handle_task_invalid_id(e):
+    return {"details": "Task id must be numeric"}, 400
+
+
+@task_bp.errorhandler(KeyError)
+def task_handle_invalid_data(e):
+    return {"details": "Invalid data"}, 400
