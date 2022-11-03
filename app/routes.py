4065465d728e07f97790924b15f46f3d107d9af8
Carina <checarina@gmail.com>
2022-11-03 12:11:23 -0700

    set up get and post task routes, missing completed_at tho

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response, jsonify
 from app import db
 from app.models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods = ["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append({
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description
+        })
+    return jsonify(tasks_response)
+
+@tasks_bp.route("", methods = ["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(title = request_body["title"], description = request_body["description"])
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify(f"Task {new_task.title} successfully created."), 201)
 
