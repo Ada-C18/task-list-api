f59a57c2e54b8a5fc70605149feea077a0c0cc71
Puja Mukherjee <pujam836@gmail.com>
2022-11-02 23:08:38 -0700

    created a task

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint,request,jsonify
+from app import db
+from app.models.task import Task
+
+task_bp = Blueprint("task_bp", __name__, url_prefix ="/tasks")
+
+@task_bp.route("", methods=["POST"])
+def create_task():
+    
+    request_body = request.get_json()
+
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"]
+        
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    
+    is_completed = True
+    if new_task.completed_at is None:
+        is_completed = False
+
+
+    task_dict = {"id": new_task.task_id,
+    "title": new_task.title,
+    "description": new_task.description,
+    "is_complete": is_completed
+    }
+
+    return jsonify({"task":task_dict}), 201
+
