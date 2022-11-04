808979efe32dcd05cc21ddc6c41ed470ca9cf17f
Kat <kmoreno1205@gmail.com>
2022-11-03 19:52:41 -0600

    Added CRUD functionality for Tasks model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,47 @@
 from app import db
-
+from flask import make_response, abort
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    @classmethod
+    def instance_from_json(cls, task_req_body):
+        try:
+            # new_task = Task(title=task_req_body["title"],
+            # description=task_req_body["description"],
+            # completed_at = task_req_body["is_complete"])
+            if "is_complete" in task_req_body:
+                new_task = Task(title=task_req_body["title"],
+                            description=task_req_body["description"],
+                            completed_at = task_req_body["is_complete"])
+            else:
+                new_task = Task(title=task_req_body["title"],
+                            description=task_req_body["description"],
+                            completed_at = None)
+            return new_task
+        except KeyError:
+            abort(make_response({"details": "Invalid data"}, 400))
+
+    def to_dict(self):
+        task_as_dict = {}
+        if self.completed_at == None:
+            task_as_dict["id"] = self.task_id
+            task_as_dict["title"] = self.title
+            task_as_dict["description"] = self.description
+            task_as_dict["is_complete"] = False
+        else: 
+            task_as_dict["id"] = self.task_id
+            task_as_dict["title"] = self.title
+            task_as_dict["description"] = self.description,
+            task_as_dict["is_complete"] = self.completed_at
+        return task_as_dict
+
+    def update(self, req_body):
+        try:
+            self.title = req_body["title"]
+            self.description = req_body["description"]
+        except KeyError as error:
+            abort(make_response({"message": f"Missing attribute: {error}"}, 400))
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,61 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, abort, request
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+def validate_model(cls, id):
+    try:
+        model_id = int(id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {id} not found"}, 404))
+
+    return model
+
+@task_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return {"task": task.to_dict()}
+
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.instance_from_json(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify({"task": new_task.to_dict()}), 201)
+
+@task_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks = Task.query.all()
+
+    tasks_response = [task.to_dict() for task in tasks]
+    return jsonify(tasks_response)
+
+@task_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_model(Task, task_id)
+
+    request_body = request.get_json()
+
+    task.update(request_body)
+
+    db.session.commit()
+
+    return {"task": task.to_dict()}
+
+@task_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return make_response(jsonify({"details": f'Task {task.task_id} "{task.title}" successfully deleted'}))
