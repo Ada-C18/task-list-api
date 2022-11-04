86e117c79f8ac32dc2733bff6e50b35e2c56e6f7
Ariel <gabriellaw15@gmail.com>
2022-11-03 19:58:08 -0700

    Created a PUSH route and passed wave 3 tests. Finished last two wave 3 tests. Modified fxn determine_completion.

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, abort, make_response, request
 from app import db
 from app.models.task import Task
 from operator import itemgetter
+from datetime import date
 
 task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
 
@@ -19,11 +20,11 @@ def validate_task(task_id_input):
 
     return task
 
-def determine_completion(task, request_or_task):
+def determine_completion(task):
     if task.completed_at == None:
         task.completed_at = False
     else:
-        task.completed_at = request_or_task["completed_at"]
+        task.completed_at = True #task["completed_at"]
     
     return task.completed_at
     
@@ -53,7 +54,7 @@ def create_one_task():
             "id": new_task.task_id,
             "title": new_task.title,
             "description": new_task.description,
-            "is_complete": determine_completion(new_task, request_body)
+            "is_complete": determine_completion(new_task)
             
         }
     }, 201
@@ -79,7 +80,7 @@ def get_all_tasks():
             "id": task.task_id,
             "title": task.title,
             "description": task.description,
-            "is_complete": determine_completion(task, task)
+            "is_complete": determine_completion(task)
         }
         response.append(task_dict)
 
@@ -99,7 +100,7 @@ def get_one_task(task_id_input):
             "id": chosen_task.task_id,
             "title": chosen_task.title,
             "description": chosen_task.description,
-            "is_complete": determine_completion(chosen_task, chosen_task)
+            "is_complete": determine_completion(chosen_task)
         }
     }
 
@@ -110,7 +111,6 @@ def update_a_task(task_id_input):
     chosen_task = validate_task(task_id_input)
 
     request_body = request.get_json()
-
 
     if "title" not in request_body or "description" not in request_body:
         return jsonify({"message":"Request must include title and description"})
@@ -126,10 +126,61 @@ def update_a_task(task_id_input):
             "id": chosen_task.task_id,
             "title": chosen_task.title,
             "description": chosen_task.description,
-            "is_complete": determine_completion(chosen_task, request_body)
+            "is_complete": determine_completion(chosen_task)
             
         }
     }, 200
+
+# -------------------------------------------PATCH----------------------------------
+@task_bp.route("/<task_id_input>/<complete_status>", methods=["PATCH"])
+def mark_complete(task_id_input, complete_status):
+    chosen_task = validate_task(task_id_input)
+
+    request_body = request.get_json()
+
+    # if "title" in request_body:
+    #     new_title = chosen_task.title
+    # if "description" in request_body:
+    #     new_descrip = chosen_task.description
+    if complete_status == "mark_complete":
+        chosen_task.completed_at = date.today()
+    elif complete_status == "mark_incomplete":
+        chosen_task.completed_at = None
+    db.session.commit()
+    # print(f"DATE!!!!: {chosen_task.completed_at}")
+
+    return {
+    "task": {
+        "id": chosen_task.task_id,
+        "title": chosen_task.title,
+        "description": chosen_task.description,
+        "is_complete": determine_completion(chosen_task)
+        }
+    }
+
+# @task_bp.route("/<task_id_input>/<complete_status>", methods=["PATCH"])
+# def mark_incomplete(task_id_input, complete_status):
+#     chosen_task = validate_task(task_id_input)
+#     print(f"CHOSEN TASK = {chosen_task}")
+#     request_body = request.get_json()
+#     print(f"REQUEST BODY = {request_body}")
+#     # if "title" in request_body:
+    # #     new_title = chosen_task.title
+    # # if "description" in request_body:
+    # #     new_descrip = chosen_task.description
+    # if complete_status == "mark_incomplete":
+    #     chosen_task.completed_at = Task.completed_at
+    # db.session.commit()
+    # print(f"DATE!!!!: {chosen_task.completed_at}")
+
+    # return {
+    # "task": {
+    #     "id": chosen_task.task_id,
+    #     "title": chosen_task.title,
+    #     "description": chosen_task.description,
+    #     "is_complete": False #determine_completion(chosen_task)
+    #     }
+    # }
 # -------------------------------------------DELETE----------------------------------
 @task_bp.route("/<task_id_input>", methods=["DELETE"])
 def delete_a_task(task_id_input):
