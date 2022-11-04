4ac01dbe536f58cb7ab9a6f320725f6ad4df87df
Yangweiyi Li <lywy.amy@gmail.com>
2022-11-03 17:12:34 -0700

    add instance method to_dict(self)

--- app/models/task.py
+++ app/models/task.py
@@ -5,3 +5,11 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.task_id,
+            "title":self.title,
+            "description": self.description,
+            "is_complete": True if self.completed_at else False
+        }
