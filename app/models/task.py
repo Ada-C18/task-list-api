c07780dcb18caac097d8b82540cbd59489fe7c8e
allcomputersarebad <allcomputersarebad@users.noreply.github.com>
2022-11-03 18:29:56 -0700

    migration for task-goal relationship

--- app/models/task.py
+++ app/models/task.py
@@ -8,6 +8,8 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, default=None)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
+    goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
         return {
