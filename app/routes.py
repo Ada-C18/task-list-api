0bfda2dcea33bc2137f3778ea096c5ccb64570b0
Anjula Tamang <parkhrinj@gmail.com>
2022-11-04 16:58:39 -0400

    created Post successfully

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,18 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request
+
+tasks_bp = Blueprint('tasks_bp', __name__, url_prefix='/tasks')
+
+#POST request
+@tasks_bp.route("",methods = ['POST'])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(title = request_body['title'],
+                    description = request_body['description'],
+                    completed_at = request_body["completed_at"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify(f'Task {new_task.title} has been successfully created.', 201))
