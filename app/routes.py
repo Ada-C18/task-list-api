9937d602488fe725733eccf328ede8dbbc4dd483
Yangweiyi Li <lywy.amy@gmail.com>
2022-11-04 11:14:18 -0700

    refactor generate_response_body to accept cls variable

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, jsonify, request, abort, make_response
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from datetime import datetime
 
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+#***********************************************#
+#***************** Task Routes *****************#
+#***********************************************#
 
 @tasks_bp.route("", methods=["POST"])
 def add_one_task():
@@ -28,7 +33,7 @@ def add_one_task():
 
         abort(make_response(jsonify(response_body), 400))
     
-    return jsonify(generate_response_body(new_task)), 201
+    return jsonify(generate_response_body(Task, new_task)), 201
 
 
 @tasks_bp.route("", methods=["GET"])
@@ -42,14 +47,14 @@ def get_all_tasks():
     elif order == "desc":
         tasks = Task.query.order_by(Task.title.desc()).all()
 
-    return jsonify(generate_response_body(tasks)), 200
+    return jsonify(generate_response_body(Task, tasks)), 200
 
 
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
     task = validate_model(Task, task_id)
 
-    return jsonify(generate_response_body(task)), 200
+    return jsonify(generate_response_body(Task, task)), 200
 
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
@@ -63,7 +68,7 @@ def update_one_task(task_id):
     
     db.session.commit()
 
-    return jsonify(generate_response_body(task)), 200
+    return jsonify(generate_response_body(Task, task)), 200
 
 
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
@@ -88,7 +93,7 @@ def mark_one_task_as_complete(task_id):
 
     db.session.commit()
 
-    return jsonify(generate_response_body(task)), 200
+    return jsonify(generate_response_body(Task, task)), 200
 
 
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
@@ -99,28 +104,80 @@ def mark_one_task_as_incomplete(task_id):
 
     db.session.commit()
 
-    return jsonify(generate_response_body(task)), 200
+    return jsonify(generate_response_body(Task, task)), 200
+
+#***********************************************#
+#***************** Goal Routes *****************#
+#***********************************************#
+
+@goals_bp.route("", methods=["POST"])
+def add_one_goal():
+    request_body = request.get_json()
+
+    try:
+        new_goal = Goal(
+            title=request_body["title"]
+        )
+
+        db.session.add(new_goal)
+        db.session.commit()
+
+    except:
+        response_body = {
+            "details": "Invalid data"
+        }
+
+        abort(make_response(jsonify(response_body), 400))
+    
+    return jsonify(generate_response_body(Goal, new_goal)), 201
+
+
+@goals_bp.route("", methods=["GET"])
+def get_all_goals():
+    goals = Goal.query.all()
+
+    return jsonify(generate_response_body(Goal, goals)), 200
+
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def get_one_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    return jsonify(generate_response_body(Goal, goal)), 200
+
+
+@goals_bp.route("/<goal_id>", methods=["PUT"])
+def update_one_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+    
+    db.session.commit()
+
+    return jsonify(generate_response_body(Goal, goal)), 200
 
 #************************************************#
-#*************** helper functions ***************#
+#*************** Helper Functions ***************#
 #************************************************#
 
-def generate_response_body(tasks):
+def generate_response_body(cls, models):
     """
-    Return a list of task-detail dictionaries if @param tasks is a list of Task objects
-    Return a single dictionary {"task": task-detail} if @param tasks is a Task object
+    Return a list of model-detail dictionaries if @param models is a list of Model objects
+    Return a single dictionary {"model": model-detail} if @param models is a Model object
     """
-    if isinstance(tasks, list):
+    if isinstance(models, list):
         response = []
 
-        for task in tasks:
-            response.append(task.to_dict())
+        for model in models:
+            response.append(cls.to_dict(model))
 
         return response
     
     else:
         return {
-            "task": tasks.to_dict()
+            f"{cls.__name__}".lower(): cls.to_dict(models)
         }
 
 
