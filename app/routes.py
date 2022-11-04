deaaf599134070bcfabc752a6e3278f5545edf1f
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-11-04 16:42:01 -0700

    added PATCH route update_completed_task

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import db
 from app.models.task import Task
 from flask import abort, Blueprint, jsonify, make_response, request
+from datetime import date
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
@@ -47,13 +48,7 @@ def get_all_tasks():
         for task in asc_tasks:
             result.append(task.to_dict())
         return jsonify(result), 200
-    # desc_tasks = Task.query.order_by(Task.title.desc()).all()
-    # asc_tasks = Task.query.order_by(Task.title.asc()).all()
 
-    # if asc_tasks:
-    #     for task in asc_tasks:
-    #         result.append(task.to_dict())
-    #     return jsonify(result), 200
     if sort == "desc":
         desc_tasks = Task.query.order_by(Task.title.desc()).all()
         for task in desc_tasks:
@@ -64,9 +59,6 @@ def get_all_tasks():
     for task in tasks:
         result.append(task.to_dict())
     return jsonify(result), 200 
-
-
-
 
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
@@ -83,6 +75,14 @@ def update_task(task_id):
     task.description = request_body["description"]
 
     # task.completed_at = request_body["completed_at"]
+    db.session.commit()
+
+    return jsonify({"task":task.to_dict()}), 200
+
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def update_completed_at(task_id):
+    task = validate_task(task_id)
+    task.completed_at = date.today()
     db.session.commit()
 
     return jsonify({"task":task.to_dict()}), 200
