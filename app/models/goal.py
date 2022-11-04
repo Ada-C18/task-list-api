14aa32c51e5e7339da79b0c088d2c0030b3380ec
Yangweiyi Li <lywy.amy@gmail.com>
2022-11-04 11:56:08 -0700

    change goal_id to id

--- app/models/goal.py
+++ app/models/goal.py
@@ -2,12 +2,12 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
 
     @classmethod
     def to_dict(cls, self):
         return {
-            "id": self.goal_id,
+            "id": self.id,
             "title":self.title
         }
