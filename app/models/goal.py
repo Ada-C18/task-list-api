f68389c1017456a01613a6d31f2ced5299e088bd
Yangweiyi Li <lywy.amy@gmail.com>
2022-11-04 11:11:52 -0700

    create class method to_dict

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,10 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+
+    @classmethod
+    def to_dict(cls, self):
+        return {
+            "id": self.goal_id,
+            "title":self.title
+        }
