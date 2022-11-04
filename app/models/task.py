4c7737f46f2527d91bbeddbae457b9b1483656d3
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-11-04 16:41:29 -0700

    refactor to_dict to change is_complete boolean

--- app/models/task.py
+++ app/models/task.py
@@ -1,18 +1,27 @@
 from app import db
 
 
+
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
 
-    def to_dict(self): 
+    def to_dict(self):
+        if self.completed_at is None:
+            return {
+                        # "task": {
+                            "id": self.task_id,
+                            "title": self.title,
+                            "description": self.description,
+                            "is_complete": False
+                        }
         return {
-                    # "task": {
-                        "id": self.task_id,
-                        "title": self.title,
-                        "description": self.description,
-                        "is_complete": False
-                    }
+                        # "task": {
+                            "id": self.task_id,
+                            "title": self.title,
+                            "description": self.description,
+                            "is_complete": True
+                        }
                 
