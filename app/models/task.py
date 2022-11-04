b764443caf916157d272951ede745be9d6ab0823
Yangweiyi Li <lywy.amy@gmail.com>
2022-11-04 11:56:42 -0700

    change task_id to id

--- app/models/task.py
+++ app/models/task.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
@@ -9,7 +9,7 @@ class Task(db.Model):
     @classmethod
     def to_dict(cls, self):
         return {
-            "id": self.task_id,
+            "id": self.id,
             "title":self.title,
             "description": self.description,
             "is_complete": True if self.completed_at else False
