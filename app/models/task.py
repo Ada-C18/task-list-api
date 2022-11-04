d1f7c69c9f6e418958d7a6488dadc5722110c1a4
Lisa <elizabethpham726@gmail.com>
2022-11-03 22:10:55 -0700

    creates POST /tasks endpoint

--- app/models/task.py
+++ app/models/task.py
@@ -2,9 +2,26 @@ from app import db
 
 
 class Task(db.Model):
-    task_id =           db.Column(db.Integer, primary_key=True)
-    task_title =        db.Column(db.String,    nullable=False)
-    task_description =  db.Column(db.String,    nullable=True)
-    task_completed_at = db.Column(db.DateTime,  nullable=True,  default=None)
-    task_is_completed = db.Column(db.Boolean,   nullable=False, default=False)
+    task_id =      db.Column(db.Integer, primary_key=True)
+    title =        db.Column(db.String,    nullable=False)
+    description =  db.Column(db.String,    nullable=True)
+    completed_at = db.Column(db.DateTime,  nullable=True,  default=None)
+    is_complete =  db.Column(db.Boolean,   nullable=False, default=False)
 
+    def from_dict(self):
+        task_as_dict = {}
+        task_as_dict["task_id"] = self.task_id  
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["is_complete"] = self.is_complete
+        
+        return task_as_dict
+
+    @classmethod
+    def new_instance_from_dict(cls, req_body):
+        new_dict = cls(
+                        title = req_body["title"],
+                        description = req_body["description"],
+                        completed_at = req_body["completed_at"]
+                        )
+        return new_dict
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,29 @@
-from flask import Blueprint
+from app import db
+from flask import abort, Blueprint, jsonify, make_response, request
+from .models.task import Task
+
+
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+
+#==============================
+#       HELPER FUNCTIONS
+#==============================
+def validate_id(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response(jsonify({"message":f"{id} invalid."}), 400))
+
+
+#==============================
+#       CREATE RESOURCE
+#==============================
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.new_instance_from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(new_task.from_dict(), 201)
