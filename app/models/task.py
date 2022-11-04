f832529543d803b5ddcbf91abd39f99b385fab2d
Lisa <elizabethpham726@gmail.com>
2022-11-03 22:19:56 -0700

    changes the response for the POST /tasks endpoint

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class Task(db.Model):
-    task_id =      db.Column(db.Integer, primary_key=True)
+    id =      db.Column(db.Integer, primary_key=True)
     title =        db.Column(db.String,    nullable=False)
     description =  db.Column(db.String,    nullable=True)
     completed_at = db.Column(db.DateTime,  nullable=True,  default=None)
@@ -10,7 +10,7 @@ class Task(db.Model):
 
     def from_dict(self):
         task_as_dict = {}
-        task_as_dict["task_id"] = self.task_id  
+        task_as_dict["id"] = self.id  
         task_as_dict["title"] = self.title
         task_as_dict["description"] = self.description
         task_as_dict["is_complete"] = self.is_complete
