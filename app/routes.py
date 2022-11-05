208f85e9df95f18215acf0dc4a96f3cb2b3c1d5f
misha-joy <mishellajoy@gmail.com>
2022-11-04 17:17:08 -0700

    adds task class, goal class

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,10 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task 
+from flask import Blueprint, jsonify, abort, make_response, request
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET", "POST"])
+def handle_tasks():
+    request_body = request.get_json()
+    if 
