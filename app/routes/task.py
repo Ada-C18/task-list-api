a8f52fe768ecea65f82442b191a7b5e86fe320ef
Annie <annie.gallagher7@gmail.com>
2022-11-05 17:54:43 -0600

    added GET and POST endpoints for tasks

--- app/routes/task.py
+++ app/routes/task.py
@@ -1,3 +1,41 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, request, make_response, abort
 
 bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
+
+@bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    task_dict = new_task.to_dict()
+
+    return make_response(jsonify(f"task: {task_dict}"), 201)
+
+@bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = [task.to_dict() for task in tasks]
+    
+    return jsonify(tasks_response), 200
+
+@bp.route("/<task_id>", methods=["GET"])
+def read_one_task():
+    pass
