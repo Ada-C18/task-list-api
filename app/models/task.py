d721bfa1a9ffc7e7f46f527cc53851bdbd55d014
Liat <liathirsh@gmail.com>
2022-11-05 11:21:35 -0400

    added the class methods to task

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,4 @@
 from app import db
-from datetime import date
-
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,4 +6,17 @@ class Task(db.Model):
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
 
+    def to_dict(self):
+        return{
+            "title" : self.title,
+            "description" : self.description,
+            "completed at" : self.completed_at
+        }
 
+    @classmethod
+    def from_dict(cls, req_body):
+        return cls(
+            title = req_body['title'],
+            description = req_body['description'],
+            completed_at = req_body['completed_at']
+        )
