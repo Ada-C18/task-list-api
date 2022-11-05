1450e7fc765298911680b5e52b90a14d02eb6707
Milena Souto <yamadasouto@gmail.com>
2022-11-05 16:42:01 -0700

    Modify mark_complete to call Slack API

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,9 +1,11 @@
 import json
-from flask import abort, Blueprint, jsonify, make_response, request
 from app import db
 from app.models.task import Task
-from sqlalchemy import desc, asc
 from datetime import datetime
+from flask import abort, Blueprint, jsonify, make_response, request
+import requests
+import os
+from sqlalchemy import desc, asc
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
@@ -122,6 +124,13 @@ def delete_task(task_id):
     return make_response(jsonify(response))  # returns 200 by default
 
 
+# Modify mark_complete to call Slack API
+# => Our Slackbot token is an API key that needs to be protected
+# => Include your Slackbot token in your code in an intentional way, following best practices about API keys in code bases.
+# -- os.environ.get(SLACK_API_KEY) to get environ variable (like in database config)
+# "the Slack API documentation states that it prefers API keys to be sent in the "Authorization" request header"
+# -- import requests => requests.patch(), response.text, requests.post()
+# -- "Someone just completed the task My Beautiful Task". "My Beautiful Task" should be the title of the task
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def update_to_complete(task_id):
     task = validate_task(task_id)
@@ -131,6 +140,33 @@ def update_to_complete(task_id):
     updated_task = task.to_dict()
 
     db.session.commit()
+
+    # INTEGRATE SLACK API
+    # PATH = "https://slack.com/api/chat.postMessage"
+    PATH = "https://slack.com/api/chat.postMessage/task-notifications"
+    API_KEY = os.environ.get("SLACK_API_KEY")
+
+    # key: API_KEY in "Authorization" request header
+    autho_header = {"key": API_KEY}
+
+    notification_text = {
+        "text": f"Someone just completed the task {task.title}"}
+    # query_params = {
+    #     "channel": "task-notifications",
+    #     "text": f"Someone just completed the task {task.title}",
+    # }
+
+    # 1. Find a way to send message to Slack API =>
+    # slack_response = requests.post(PATH, params=query_params, headers)
+    requests.post(PATH, data=notification_text, headers=autho_header)
+
+    # Slack's sample code
+    # try:
+    #     # app.client.chat_postMessage
+    #     result = client.chat_postMessage(query_params)
+    #     logger.info(result)
+    # except SlackApiError as e:
+    #     logger.error(f"Error posting message: {e}")
 
     response = {
         "task": updated_task
