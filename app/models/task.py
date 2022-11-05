7709027ad20fe63ac108d17412803fe9ae3c9616
Presley <persleytieman@outlook.com>
2022-11-05 14:15:39 -0400

    Wrote create_task function and updated task model

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,24 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+
+
+    def to_dict(self):
+        return dict(
+            id=self.task_id,
+            title=self.title,
+            description=self.description,
+            is_complete=False
+        )
+    @classmethod
+    def from_dict(cls, response_dict):
+        return cls(
+            title=response_dict["title"],
+            description=response_dict["description"],
+            completed_at=None
+        )
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,16 @@
 from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request
 
 bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+@bp.route("", methods=["POST"])
+def create_a_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify({
+            "task": Task.to_dict(new_task)})), 201
