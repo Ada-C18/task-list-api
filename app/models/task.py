eaed3f7a18bb04feb67ee7a975a467e6fa504ca1
Intesar Siraj <ghideruk@gmail.com>
2022-11-04 19:25:33 -0700

    wave 1,made routes POST,GET,DELETE,PUT and used helper functions

--- app/models/task.py
+++ app/models/task.py
@@ -2,16 +2,25 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    task_id = db.Column(db.Integer, primary_key=True) 
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime)
+    completed_at = db.Column(db.DateTime, nullable = True)
 
     def to_dict(self):
         return {
             "id": self.task_id,
             "title": self.title,
             "description": self.description,
-            "is_complete": False
+            "is_complete": True if self.completed_at else False
         }
+
+    def one_task_in_dict(self):
+        result = {}
+        result["task"] = self.to_dict()
+        return result
+    
+    def one_saved_task(self, data_dict):
+        self.title = data_dict["title"]
+        self.description = data_dict["description"]
 
--- app/routes.py
+++ app/routes.py
@@ -4,26 +4,71 @@ from flask import Blueprint, jsonify, make_response, request, abort
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
+#helper function
+def validate_tasks(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"details":f"{task_id} invalid data"}, 400))
+
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message":f"task {task_id} not found"}, 404))
+    
+    return task 
+
+
 #route functions
 @tasks_bp.route("", methods=["POST"])
 def create_tasks():
-    request_body = request.get_json()
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"],
-                    completed_at= request_body["completed_at"]
-                    )
+    title = request.json.get("title", None)
+    description = request.json.get("description")
+    if not title or not description:
+        return jsonify({"details": "Invalid data"}), 400
 
+        new_task = Task(
+            title=request_body["title"],
+            description=request_body["description"]
+            )
     db.session.add(new_task)
     db.session.commit()
 
     return jsonify({"task": new_task.to_dict()}), 201
 
+@tasks_bp.route("", methods=["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+    request_body = []
+    result_list = [task.to_dict() for task in tasks]
 
-@tasks_bp.route("/tasks/1", methods=["DELETE"])
-def delete_tasks():
+    return jsonify(result_list), 200
+
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def get_one_task(task_id):
+    task = validate_tasks(task_id)
+
+    return jsonify(task.one_task_in_dict()), 200
+    
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def put_one_task(task_id):
+    task = validate_tasks(task_id)
     request_body = request.get_json()
+
+    task.one_saved_task(request_body)
+
+    db.session.commit()
+
+    return jsonify(task.one_task_in_dict()), 200
+
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_tasks(task_id)
 
     db.session.delete(task)
     db.session.commit()
 
-    return jsonify({"details": "Task 1 \"Go on my daily walk 🏞\" successfully deleted"}), 200
+    return make_response(f"Task# {task_id} successfully deleted"), 200
