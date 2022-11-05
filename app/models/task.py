7f95fc335da6d10d87ab89a757cc4e3b5e389b3b
Adelle Montalbo <adellemontalbo@gmail.com>
2022-11-04 20:08:12 -0700

    defined routes for GET all tasks and POST a new task

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,39 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    @classmethod
+    def from_dict(cls, response_dict):
+        return cls(
+            title=response_dict["title"],
+            description=response_dict["description"],
+            completed_at=response_dict["completed_at"]
+        )
+
+    # @classmethod
+    # def response_dict(cls, data_dict):
+    #     return cls(
+    #     id=self.task_id,
+    #     title=data_dict["title"],
+    #     description=data_dict["description"],
+    #     is_complete=data_dict["completed_at"],
+    #     )
+
+    def to_dict(self):
+        return_dict = {
+            "task": {  # how to separate this out?
+                "id": self.task_id,
+                "title": self.title,
+                "description": self.description,
+                "is_complete": self.completed_at
+            }}  # how to get T/F here?
+        return return_dict
+
+    def resp_all_dict(self):
+        return_dict = {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at
+        }  # how to get T/F here?
+        return return_dict
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,46 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, abort, make_response, request
+
+
+bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# Validate
+
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response(
+            {"message": f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+    if not model:
+        abort(make_response(
+            {"message": f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
+
+# CREATE
+
+
+@bp.route("", methods=["POST"], strict_slashes=False)
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify(new_task.to_dict()), 201)
+
+# READ ALL
+
+
+@bp.route("", methods=["GET"], strict_slashes=False)
+def read_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = [task.resp_all_dict() for task in tasks]
+
+    return jsonify(tasks_response)
