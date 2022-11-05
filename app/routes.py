031f00d538b0b426686cd6d977d63e756cf656c3
ashanti <ammanier2@gmail.com>
2022-11-05 17:40:42 -0400

    wave1 test_creat_task pass

--- app/routes.py
+++ app/routes.py
@@ -9,20 +9,32 @@ tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 #Creating helper function validate_task to handle errors foe get a task by id
 # Checks for valid data type (int)
 # Checks that id provided exists in records
-@tasks_bp.route("", methods=["POST"])
-def handle_tasks():
-    request_body = request.get_json()
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"])
 
+
+# Defining Endpoint and Creating Route Function to CREATE a task
+@tasks_bp.route("", methods=["POST"])
+def create_tasks():
+    request_body = request.get_json() #This method "Pythonifies" the JSON HTTP request body by converting it to a Python dictionary
+    new_task = Task(
+        title=request_body["title"],
+        description=request_body["description"],
+        
+        )
+
+    #communicating to the db to collect and commit the changes made in this function
+    #saying we want the database to add new_task
     db.session.add(new_task)
     db.session.commit()
 
-    return make_response(f"task {new_task.title} successfully created", 201)
+    return {
+        "task": {
+            "id": new_task.task_id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": False
+        }
+        }, 201
 
-
-# Defining Endpoint
-# Creating Route Function to Get all tasks
 
 
 # Defining Endpoint
