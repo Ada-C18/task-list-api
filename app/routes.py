233707f40a18d2c2a187c55eb2c3e9faaa3035d3
Shweet Abraha <sabraha0311@gmail.com>
2022-11-05 14:48:56 -0700

    Client can make a PUT method request to /tasks/1 when there is at least one saved task with 200 status code.

--- app/routes.py
+++ app/routes.py
@@ -40,5 +40,41 @@ def get_one_task(task_id):
 
     return make_response(jsonify(response_body), 200)
 
+@task_bp.route("/<task_id>", methods=["PUT"])
+def update_task_with_new_vals(task_id):
+
+    chosen_task = get_one_obj_or_abort(Task, task_id)
+
+    request_body = request.get_json()
+
+    if "title" not in request_body or \
+    "description" not in request_body:
+        return jsonify({"message":"Request must include title and description."}), 400
+
+    chosen_task.title = request_body["title"]
+    chosen_task.description = request_body["description"]
+
+    db.session.commit()
+
+    task_dict = chosen_task.to_dict()
+    response_body = {
+        "task": task_dict
+    }
+
+    return make_response(jsonify(response_body), 200)
+
+# @task_bp.route("/<task_id>", methods=["DELETE"])
+# def delete_one_task(task_id):
+#     chosen_task = get_one_obj_or_abort(Task, task_id)
+
+#     db.session.delete(chosen_task)
+
+#     db.session.commit()
+
+#     return jsonify({}), 200
+
+
+
+
 
 
