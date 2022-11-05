02c5f9ebb14395c71c99e67a3df7a9dbde399895
Nancy Lee <nramirez289@gmail.com>
2022-11-05 16:29:00 -0700

    cleaned up code with helper function

--- app/routes.py
+++ app/routes.py
@@ -4,25 +4,32 @@ from app.models.task import Task
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
+def get_one_task_or_error(task_id):
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        response_body = "Invalid data"
+        abort(make_response(jsonify({"details": response_body}), 400))
 
-# def get_one_task_or_error(task_id):
-#     try:
-#         task_id = int(task_id)
-#     except ValueError:
-#         response_body = "Invalid data"
-#         return make_response(jsonify({"details": response_body}), 400)
+    task_found = Task.query.get(task_id)
 
-#     task_found = Task.query.get(task_id)
+    if task_found is None:
+        response_body = "Task id not found"
+        abort(make_response(jsonify({"details": response_body}), 404))
 
-#     if task_found is None:
-#         response_body = "Task id not found"
-#         return make_response(jsonify({"details": response_body}), 404)
+    return task_found
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @tasks_bp.route("", methods=["POST"])
 def add_task():
 
     request_body = request.get_json()
+
+    if "title" not in request_body or \
+        "description" not in request_body:
+        # or \
+        # "completed_at" not in request_body:
+        return jsonify({"details": "Invalid data"}), 400
 
     new_task = Task(
         title = request_body["title"],
@@ -42,7 +49,7 @@ def add_task():
             }
         }
 
-    return jsonify(response), 201 # is this giving me the output needed?
+    return jsonify(response), 201 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @tasks_bp.route("", methods=["GET"])
@@ -53,42 +60,38 @@ def get_all_tasks():
     response = []
 
     for task in tasks:
-        task_dict = {
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": False # will need help figuring out how to use this
-        }
-        response.append(task_dict)
-
+        if task is None:
+            response_body = "Whoopsie daisy! Task id is lost and was not found"
+            return jsonify(response_body), 404
+        else:
+            task_dict = {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": False
+            }
+            response.append(task_dict)
     return jsonify(response), 200
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
+    chosen_task = get_one_task_or_error(task_id)
 
-    task = Task.query.get(task_id)
-
-    if task:
-        response_dict = {
-            "task": {
-                "id": task.task_id,
-                "title": task.title,
-                "description": task.description,
-                "is_complete": False
-                }
+    response_dict = {
+        "task": {
+            "id": chosen_task.task_id,
+            "title": chosen_task.title,
+            "description": chosen_task.description,
+            "is_complete": False
             }
-        return jsonify(response_dict), 200
-
-    if task is None:
-        response_body = "Whoopsie daisy! Task id is lost and was not found"
-        return jsonify(response_body), 404
+        }
+    return jsonify(response_dict), 200
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task_title_and_description(task_id):
-
-    task_to_update = Task.query.get(task_id)
+    task_to_update = get_one_task_or_error(task_id)
 
     request_body = request.get_json()
 
@@ -97,24 +100,14 @@ def update_task_title_and_description(task_id):
 
     db.session.commit()
 
-# this will need to be replaced with the updated instances
     response_body = {       
         "task": {
-            "id": 1,
-            "title": "Updated Task Title",
-            "description": "Updated Test Description",
+            "id": task_to_update.task_id,
+            "title": task_to_update.title,
+            "description": task_to_update.description,
             "is_complete": False
             }
         }
-    # this is what the response body should look like after updating the data
-    # response_body = {       
-    #     "task": {
-    #         "id": task.task_id,
-    #         "title": task.title,
-    #         "description": task.description,
-    #         "is_complete": False
-    #         }
-    #     }
     return jsonify(response_body), 200
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -122,10 +115,10 @@ def update_task_title_and_description(task_id):
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task_data_from_db(task_id):
 
-    task_to_delete = Task.query.get(task_id)
+    task_to_delete = get_one_task_or_error(task_id)
 
     db.session.delete(task_to_delete)
     db.session.commit()
 
-    response_body = "Task 1 \"Go on my daily walk 🏞\" successfully deleted"
+    response_body = f'Task {task_id} "{task_to_delete.title}" successfully deleted'
     return jsonify({"details": response_body}), 200
