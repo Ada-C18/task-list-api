74ec91b7a99dd03d550bf6c77d5f1dffa0a2c32c
Annie <annie.gallagher7@gmail.com>
2022-11-05 17:20:12 -0600

    added functions to_dict and from_dict to Task class

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,19 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.task_id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.completed_at
+
+        return task_as_dict
+    
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = cls(title=task_data["title"],
+                        description=task_data["description"],
+                        completed_at=False)
+
+        return new_task
