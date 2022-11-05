cc33cf07c19041ba15ce11507369b72636ec766b
Liat <liathirsh@gmail.com>
2022-11-05 10:59:07 -0400

    updated column

--- app/models/task.py
+++ app/models/task.py
@@ -1,10 +1,11 @@
 from app import db
+from datetime import date
 
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.datetime, nullable=True)
+    completed_at = db.Column(db.DateTime, nullable=True)
 
 
