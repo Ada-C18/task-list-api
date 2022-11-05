5839aedcaf1a7d47c6bd27eb260298c8fe4b532d
ashanti <ammanier2@gmail.com>
2022-11-05 19:16:19 -0400

    added delete_task function and wave1 test 8 pass

--- app/routes.py
+++ app/routes.py
@@ -102,3 +102,15 @@ def update_task(task_id):
             "is_complete": False
         }
         }
+
+# Defining Endpoint and Creating Route Function to UPDATE a Task
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return {
+        "details": f'Task {task.task_id} "{task.title}" successfully deleted'
+    }
