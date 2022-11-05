00edfb8cc4ee689d10094b5e341d3bcacb6dcb9e
Annie <annie.gallagher7@gmail.com>
2022-11-05 17:54:21 -0600

    added GET and POST endpoints for tasks

--- app/models/task.py
+++ app/models/task.py
@@ -8,17 +8,19 @@ class Task(db.Model):
     completed_at = db.Column(db.DateTime, nullable=True)
 
     def to_dict(self):
-        task_as_dict = {}
-        task_as_dict["id"] = self.task_id
-        task_as_dict["title"] = self.title
-        task_as_dict["description"] = self.completed_at
+        task_as_dict = {
+        "id": self.task_id,
+        "title": self.title,
+        "description": self.description,
+        "is_complete": False,
+        }
 
         return task_as_dict
     
     @classmethod
     def from_dict(cls, task_data):
         new_task = cls(title=task_data["title"],
-                        description=task_data["description"],
-                        completed_at=False)
+                        description=task_data["description"]
+                        )
 
         return new_task
