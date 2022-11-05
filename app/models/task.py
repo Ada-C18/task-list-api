13b632f607c0b9592aa72965f62a700243766fcc
abby <abbychoi97@gmail.com>
2022-11-04 17:59:50 -0700

    updated goal and task models to reflect one to many relationship

--- app/models/task.py
+++ app/models/task.py
@@ -6,6 +6,7 @@ class Task(db.Model):
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
     is_complete = db.Column(db.Boolean, default=False)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
 
     def to_dict(self):
         return {
