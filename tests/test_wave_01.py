1918d726b42a384a80c031d468a13ffb8b2666fe
Shweet Abraha <sabraha0311@gmail.com>
2022-11-05 15:09:02 -0700

    Completed assert tests for test_update_task_not_found by checking if 'me ssage' is in response_body and if 'was not found in the database' is in response_body['message'].

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -118,7 +118,7 @@ def test_update_task(client, one_task):
     assert task.completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_update_task_not_found(client):
     # Act
     response = client.put("/tasks/1", json={
@@ -130,10 +130,10 @@ def test_update_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
+    # raise Exception("Complete test with assertion about response body")
+    assert "message" in response_body
+    assert "was not found in the database" in response_body["message"] 
+  
 
 
 @pytest.mark.skip(reason="No way to test this feature yet")
