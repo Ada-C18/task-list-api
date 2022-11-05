8737bd4db7292fc960b7b84effea24b5c171ff2a
Adelle Montalbo <adellemontalbo@gmail.com>
2022-11-04 20:32:44 -0700

    added PUT, DELETE, GET one

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,6 @@ from flask import Blueprint, jsonify, abort, make_response, request
 bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
 # Validate
-
-
 def validate_model(cls, model_id):
     try:
         model_id = int(model_id)
@@ -23,8 +21,6 @@ def validate_model(cls, model_id):
     return model
 
 # CREATE
-
-
 @bp.route("", methods=["POST"], strict_slashes=False)
 def create_task():
     request_body = request.get_json()
@@ -36,11 +32,39 @@ def create_task():
     return make_response(jsonify(new_task.to_dict()), 201)
 
 # READ ALL
-
-
 @bp.route("", methods=["GET"], strict_slashes=False)
 def read_all_tasks():
     tasks = Task.query.all()
     tasks_response = [task.resp_all_dict() for task in tasks]
 
-    return jsonify(tasks_response)
+    return jsonify(tasks_response), 200
+
+# READ ONE TASK
+
+
+@bp.route("/<task_id>", methods=["GET"], strict_slashes=False)
+def read_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return jsonify(task.resp_all_dict()), 200
+
+# UPDATE ONE TASK
+@bp.route("/<task_id>", methods=["PUT"], strict_slashes=False)
+def update_one_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+    return jsonify(task.resp_all_dict()), 200
+
+# #DELETE ONE TASK
+@bp.route("/<task_id>", methods=["DELETE"], strict_slashes=False)
+def delete_one_task(task_id):
+    task = validate_model(Task, task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return {
+        "details": "Task 1 \"Go on my daily walk 🏞\" successfully deleted"
+            }
