70916a532d673f872e39a0bdb32c168302281c43
Carina <checarina@gmail.com>
2022-11-05 23:04:46 -0700

    fixed create_task function to pass test

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,17 @@ from app.models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+# def make_task_dict(task):
+    
+#     task_dict = {
+#         "id": task.task_id,
+#         "title": task.title,
+#         "description": task.description,
+#     }
+
+
+
+    
 @tasks_bp.route("", methods = ["GET"])
 def get_all_tasks():
     tasks = Task.query.all()
@@ -13,16 +24,23 @@ def get_all_tasks():
             "id": task.task_id,
             "title": task.title,
             "description": task.description,
-            "is_complete": bool(task.completed_at)
+            "is_complete": False
         })
     return jsonify(tasks_response)
 
 @tasks_bp.route("", methods = ["POST"])
 def create_task():
     request_body = request.get_json()
-    new_task = Task(title = request_body["title"], description = request_body["description"], completed_at = request_body["completed_at"])
+    new_task = Task(title = request_body["title"], description = request_body["description"], completed_at = None)
     db.session.add(new_task)
     db.session.commit()
 
-    return make_response(jsonify(f"Task {new_task.title} successfully created."), 201)
+    return make_response(jsonify({
+        "task": {
+            "id": new_task.task_id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": bool(new_task.completed_at)
+            }
+        }), 201)
 
