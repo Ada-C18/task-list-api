f0359781a9c746b878e4d0ea1fe501adaa1e84eb
Soleil <soleilherring@gmail.com>
2022-11-05 17:35:37 -0700

    Made GET, POST, DELETE, PUT requests

--- app/models/task.py
+++ app/models/task.py
@@ -2,8 +2,28 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
-    title = name = db.Column(db.String)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
 
+    def to_dict(self):
+            # task_as_dict = {}
+            # task_as_dict["id"] = self.id
+            # task_as_dict["title"] = self.title
+            # task_as_dict["description"] = self.description
+
+            return {
+                "id": self.task_id,
+                "title" : self.title,
+                "description" : self.description,
+                "is_complete": True if self.completed_at == True else False 
+            }
+
+    @classmethod
+    def from_dict(cls, task_data):
+        
+        new_task = Task(title=task_data["title"],
+                        description=task_data["description"])
+        return new_task
+        
---
+++ app/task_routes.py
@@ -0,0 +1,154 @@
+from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request, abort
+from sqlalchemy import asc 
+from sqlalchemy import desc
+
+
+
+tasks_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+#################################################
+#                WAVE 01
+#################################################
+
+#validate function 
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
+
+
+#Create a Task: Valid Task With null completed_at
+#################
+#       new
+###############
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    
+    try:
+        new_task = Task.from_dict(request_body)
+
+    except KeyError:
+        abort(make_response({"details": "Invalid data"}, 400)) 
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"task":new_task.to_dict()},201
+
+####################
+#       original 
+####################
+
+# @tasks_bp.route("", methods=["POST"])
+# def create_task():
+#     request_body = request.get_json()
+#     new_task = Task.from_dict(request_body)
+
+#     db.session.add(new_task)
+#     db.session.commit()
+
+#     # return make_response(jsonify(f"Task {new_task.title} successfully created"), 201)
+#     return {"task":new_task.to_dict()},201
+
+###########################
+#       helper function for get
+#########################
+# def sort_function():
+#     sort_query = request.args.get("sort")
+#     if sort_query == "asc":
+#         return Task.query.order_by(Task.title.asc())
+#     elif sort_query == "desc":
+#         return Task.query.order_by(Task.title.desc())
+#     else:
+#         return Task.query.all()
+# ###########################
+# #       new function for get w/helper
+# #########################
+
+# # Get Tasks: Getting Saved Tasks
+# @tasks_bp.route("", methods=["GET"])
+# def read_all_tasks():
+#     task = sort_function()
+#     if task:
+#         return task
+#     tasks = Task.query.all()
+
+#     tasks_response = []
+#     for task in tasks:
+#         tasks_response.append(task.to_dict())
+
+#     return jsonify(tasks_response)
+
+#########################
+# working original
+#########################
+# Get Tasks: Getting Saved Tasks
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    sort_query = request.args.get("sort")
+    title_query = request.args.get("title")
+
+    if sort_query == "asc":
+        tasks = Task.query.order_by(Task.title.asc())
+    elif sort_query == "desc":
+        tasks = Task.query.order_by(Task.title.desc())
+    elif title_query: 
+        tasks = Task.query.filter_by(title=title_query)
+    else:
+        tasks = Task.query.all()
+
+    ###old way???####
+    # title_query = request.args.get("title")
+    # if title_query:
+    #     tasks = Task.query.filter_by(title=title_query)
+    # else:
+    #     tasks = Task.query.all()
+
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(task.to_dict())
+
+    return jsonify(tasks_response)
+
+# Get Tasks: Get one task
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return {"task":task.to_dict()}
+
+# Update Task
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_book(task_id):
+    task = validate_model(Task, task_id)
+
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    return {"task":task.to_dict()}
+
+# Delete Task: Deleting a Task
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_book(task_id):
+    task = validate_model(Task, task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return {"details":f'Task {task.task_id} "{task.title}" successfully deleted'}
+
