91ddfa6b4d3b92bd3fe44535cef40ae6a21bad1c
Pavi <pavanirannulu@gmail.com>
2022-11-06 14:42:32 -0800

    passed all Wave 3 tests

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,15 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    def to_dict(self):
+        tasks_as_dict = {}
+        tasks_as_dict["id"] = self.task_id
+        tasks_as_dict["title"] = self.title
+        tasks_as_dict["description"] = self.description
+        if self.completed_at:
+            tasks_as_dict["is_complete"] = True
+        else:
+            tasks_as_dict["is_complete"] = False
+
+        return tasks_as_dict
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from app import db
 from app.models.task import Task
 from flask import Blueprint, jsonify, make_response, request, abort
+from datetime import datetime
 
 def validate_model(cls, model_id):
     try:
@@ -24,32 +25,21 @@ def read_all_tasks():
     tasks_response = []
     
     for task in tasks:
-        tasks_response.append(
-            {
-                "id":task.task_id,
-                "title": task.title,
-                "description": task.description,
-                "is_complete": False
-            }
-        )
-
-    # dealing with Query Params (Wave 2):
+        tasks_response.append(task.to_dict())
     sorting_type = request.args.get("sort")
-    if not sorting_type: #no sorting
-        return jsonify(tasks_response)
-    elif sorting_type == "asc":
-        sorted_tasks_response = sorted(tasks_response, key = lambda task: task["title"])
+    if sorting_type == "asc":
+        return jsonify(sorted(tasks_response, key = lambda task: task["title"]))
     elif sorting_type == "desc":
-        sorted_tasks_response = sorted(tasks_response, key = lambda task: task["title"], reverse=True)
-    return jsonify(sorted_tasks_response)
+        return jsonify(sorted(tasks_response, key = lambda task: task["title"], reverse=True))
+    else: 
+        return jsonify(tasks_response)
 
 @tasks_bp.route("", methods =["POST"])
 def create_task():
     request_body = request.get_json()
     try: 
         new_task = Task(title=request_body["title"], 
-                    description=request_body["description"],
-                    completed_at=request_body["completed_at"])
+                    description=request_body["description"])
     except KeyError:
         abort(make_response({"details":"Invalid data"}, 400))
     
@@ -57,46 +47,22 @@ def create_task():
     db.session.add(new_task)
     db.session.commit()
 
-    return make_response({"task":
-                {
-                "id":new_task.task_id,
-                "title": new_task.title,
-                "description": new_task.description,
-                "is_complete": False
-                }
-            }, 201)
+    return make_response({"task":new_task.to_dict()}, 201)
     
 @tasks_bp.route("/<task_id>", methods =["GET"])
 def read_one_task(task_id):
     task = validate_model(Task,task_id)
-    return {"task":
-                {
-                "id":task.task_id,
-                "title": task.title,
-                "description": task.description,
-                "is_complete": False
-                }
-            }
+    return {"task":task.to_dict()}
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
     task = validate_model(Task, task_id)
-
     request_body = request.get_json()
-
     task.title = request_body["title"]
     task.description = request_body["description"]
-    
     db.session.commit()
+    return {"task":task.to_dict()}
 
-    return {"task":
-                {
-                "id":task.task_id,
-                "title": task.title,
-                "description": task.description,
-                "is_complete": False
-                }
-            }
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
     task = validate_model(Task,task_id)
@@ -106,3 +72,16 @@ def delete_task(task_id):
 
     return make_response({"details": f'Task {task.task_id} "{task.title}" successfully deleted'})
 
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def mark_task_complete(task_id):
+    task = validate_model(Task, task_id)
+    task.completed_at = datetime.now()
+    db.session.commit()
+    return {"task":task.to_dict()}
+
+@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def mark_task_incomplete(task_id):
+    task = validate_model(Task, task_id)
+    task.completed_at = None
+    db.session.commit()
+    return {"task":task.to_dict()}
