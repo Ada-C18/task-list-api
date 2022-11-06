4edceff64709c6a6088d851f0ea47a6386176b7c
Brooke hill <brookeannstasia@gmail.com>
2022-11-06 12:03:30 -0500

    Is_complete is fucntioning properly

--- app/routes.py
+++ app/routes.py
@@ -11,10 +11,7 @@ def create_task():
     # Feel free to add a guard clause
     if "title" not in request_body or "description" not in request_body:
         return make_response("Invalid Request", 400)
-    # if "completed_at":
-    #     return True
-    # else:
-    #     return False
+
     # How we know about Dog
     new_task = Task(
         # You're looking for this key and assign it to name, breed, gender, age
@@ -22,7 +19,6 @@ def create_task():
         description=request_body["description"],
         completed_at=request_body["completed_at"]
     )
-
     # Add this new instance of dog to the database
     db.session.add(new_task)
     db.session.commit()
@@ -32,37 +28,17 @@ def create_task():
     "task": new_task.task_id,
     "title": new_task.title,
     "description": new_task.description,
-    "is_complete": new_task.completed_at
+    "is_complete": bool(new_task.completed_at)
     },201)
 
 @task_bp.route("", methods=["GET"])
-# Change name to handle dogs
 def get_all_tasks():
-    # task_query = Task.query
 
-    # breed_query = request.args.get("breed")
-    # if breed_query:
-    #     # exact string matching - case sensitive
-    #     # dog_query = dog_query.filter_by(breed=breed_query)
-    #     # partial filters - case sensitive
-    #     # dog_query = dog_query.filter(Dog.breed.contains(breed_query))
-    #     # partial filter - case insensitive
-    #     dog_query = dog_query.filter(Dog.breed.ilike(f"%{breed_query}%"))
-
-    # age_query = request.args.get("age")
-    # if age_query:
-    #     dog_query = dog_query.filter_by(age=age_query)
 
     tasks = Task.query.all()
 
-    tasks_response = []
-    for task in tasks:
-        tasks_response.append({
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": task.completed_at
-        })
+    tasks_response = [{"id": task.task_id, "title": task.title, "description": task.description, "is_complete": bool(task.completed_at)} for task in tasks]
+
     return jsonify(tasks_response), 200
 
 
