c721b1f37a8f9dd0c88ecd8f674b2afeedc0832c
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2022-11-05 17:02:36 -0700

    added get request

--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,5 @@
 from app import db
+# import json
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
@@ -6,4 +7,18 @@ class Task(db.Model):
     description = db.Column(db.String)
     # confirm null is default if task has not been completed
     completed_at = db.Column(db.DateTime, nullable=True)
+    # is_complete = db.Column(db.Boolean)
 # pretend code to push something up 
+
+    def to_dict(self):
+        task_dict = {}
+        task_dict["id"] = self.task_id
+        task_dict["title"] = self.title
+        task_dict["description"] = self.description
+        task_dict["is_complete"] = False
+        # if self.completed_at == None:
+        #     task_dict["is_complete"] = False
+
+        # return json.dumps(task_dict)
+        return task_dict
+        # task_dict["completed_at"] = self.completed_at
--- app/routes.py
+++ app/routes.py
@@ -8,11 +8,26 @@ tasks_bp = Blueprint('tasks_bp', __name__, url_prefix='/tasks')
 @tasks_bp.route("",methods = ['POST'])
 def create_task():
     request_body = request.get_json()
+    
     new_task = Task(title = request_body['title'],
                     description = request_body['description'],
                     completed_at = request_body["completed_at"])
-
+                        
+    
     db.session.add(new_task)
     db.session.commit()
 
-    return make_response(jsonify(f'Task {new_task.title} has been successfully created.', 201))
+    # task_response = [task.to_dict() for task in new_task]
+    return make_response("CREATED", 201)
+
+#Get Tasks: Getting Saved Tasks
+@tasks_bp.route("", methods=["GET"])
+def get_all_task():
+
+    all_tasks = Task.query.all()
+
+    task_response = [task.to_dict() for task in all_tasks]
+
+    return make_response(jsonify(task_response), 200)
+
+
