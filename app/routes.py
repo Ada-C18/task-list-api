8ba5532b6c9a25b2557b5793f9b46d38697c544e
Carina <checarina@gmail.com>
2022-11-05 23:18:19 -0700

    added from_dict and to_dict methods to Task class

--- app/routes.py
+++ app/routes.py
@@ -20,27 +20,25 @@ def get_all_tasks():
     tasks = Task.query.all()
     tasks_response = []
     for task in tasks:
-        tasks_response.append({
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": False
-        })
+        # tasks_response.append({
+        #     "id": task.task_id,
+        #     "title": task.title,
+        #     "description": task.description,
+        #     "is_complete": False
+        # })
+        tasks_response.append(task.to_dict())
     return jsonify(tasks_response)
 
 @tasks_bp.route("", methods = ["POST"])
 def create_task():
     request_body = request.get_json()
-    new_task = Task(title = request_body["title"], description = request_body["description"], completed_at = None)
+    # new_task = Task(title = request_body["title"], description = request_body["description"], completed_at = None)
+    new_task = Task.from_dict(request_body)
+
     db.session.add(new_task)
     db.session.commit()
 
     return make_response(jsonify({
-        "task": {
-            "id": new_task.task_id,
-            "title": new_task.title,
-            "description": new_task.description,
-            "is_complete": bool(new_task.completed_at)
-            }
+        "task": new_task.to_dict()
         }), 201)
 
