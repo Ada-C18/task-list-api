a6b91e0683cf64aee7664ca78b5fca8a06b7e476
mhc <mhchau0+ada@gmail.com>
2022-11-06 17:34:43 -0500

    created & imported tasks_bp to __init__

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.task import Task
+from app.models.goal import Goal
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    
