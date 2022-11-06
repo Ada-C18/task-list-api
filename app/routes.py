6c2ee9a8ca708aab61f24fdafa02577e6c5c80a1
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2022-11-06 13:07:16 -0800

    wave 1 completed

--- app/routes.py
+++ app/routes.py
@@ -4,32 +4,6 @@ from flask import Blueprint, jsonify, make_response, request, abort
 
 tasks_bp = Blueprint('tasks_bp', __name__, url_prefix='/tasks')
 
-def validate_model(cls, model_id):
-    try:
-        model_id = int(model_id)
-    except:
-        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
-
-    model = cls.query.get(model_id)
-
-    if not model:
-        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
-
-#POST request
-@tasks_bp.route("", methods=["POST"])
-def create_task():
-    request_body = request.get_json()
-    if  not len(request_body) == 2:
-        return {"details": "Invalid data"}, 400
-    
-    new_task= Task(title=request_body['title'], 
-        description=request_body['description'])
-
-    db.session.add(new_task)
-    db.session.commit()
-
-    return make_response(jsonify({'task': new_task.to_dict()}), 201)
-
 #Get Tasks: Getting Saved Tasks
 @tasks_bp.route("", methods=["GET"])
 def get_all_task():
@@ -40,9 +14,66 @@ def get_all_task():
 
     return make_response(jsonify(task_response), 200)
 
-@tasks_bp.route("/<task_id>", methods=["GET"])
-def read_one_task(task_id):
-    task = validate_model(Task, task_id)
-    return jsonify({'task': task.to_dict()})
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message":f"task {task_id} invalid"}, 400))
 
-#new code because we passed
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message":f"Task {task_id} not found"}, 404))
+
+    return task
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def handle_task(task_id):
+    task = validate_task(task_id)
+
+    return {"task": {
+    "id": task.id,
+    "title": task.title,
+    "description": task.description,
+    "is_complete": False}
+}
+
+@tasks_bp.route("", methods=["POST"])
+
+def create_task():
+    request_body = request.get_json()
+
+    # guard clause
+    if "title" not in request_body or "description" not in request_body:
+        return {"details": "Invalid data"}, 400
+    new_task= Task(
+        title=request_body['title'], 
+        description=request_body['description']
+        )
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify({'task': new_task.to_dict()}), 201)
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def edit_task(task_id):
+
+    task = validate_task(task_id)
+    request_body = request.get_json()
+
+    task.title=request_body["title"]
+    task.description=request_body["description"]
+
+    db.session.commit()
+
+    return make_response(jsonify({'task': task.to_dict()}), 200)
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return {"details": f'Task {task.id} "{task.title}" successfully deleted'}, 200
