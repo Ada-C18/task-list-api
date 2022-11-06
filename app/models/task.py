518c2cf06925acbf10476a55e6f043d244662779
mhc <mhchau0+ada@gmail.com>
2022-11-06 17:21:43 -0500

    added autoincrement=True to Task task_id attr

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True) #add autoincrement=True?
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True) #add autoincrement=True?
     title = db.Column(db.Text)
     description = db.Column(db.Text)
     completed_at = db.Column(db.DateTime, nullable=True, default=None)
