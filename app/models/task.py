7089d5712d9107457948ed0c7770e411e25e9e31
mhc <mhchau0+ada@gmail.com>
2022-11-06 18:57:12 -0500

    implemented create_task route

--- app/models/task.py
+++ app/models/task.py
@@ -2,21 +2,20 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True) #add autoincrement=True?
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True) #add autoincrement=True?
     title = db.Column(db.Text)
     description = db.Column(db.Text)
     completed_at = db.Column(db.DateTime, nullable=True, default=None)
     
-    @classmethod
-    def from_dict(cls, task_data):
-        new_task = Task(title=task_data["title"],
-                        description=task_data["description"],
-                        completed_at=task_data["completed_at"])
+    # @classmethod
+    # def from_dict(cls, task_data):
+    #     new_task = Task(title=task_data["title"],
+    #                     description=task_data["description"])
     
-    def to_dict(self):
-        return {
-            "id": self.task_id,
-            "title": self.title,
-            "description": self.description,
-            "completed_at": self.completed_at
-        }
+    # def to_dict(self):
+    #     return {
+    #         "id": self.task_id,
+    #         "title": self.title,
+    #         "description": self.description,
+    #         "completed_at": self.completed_at
+    #     }
--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,28 @@ tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
 @tasks_bp.route("", methods=["POST"])
 def create_task():
+    request_body = request.get_json()
     
+    # checks input for all required fields
+    if (("title") not in request_body
+        or ("description") not in request_body):
+        return make_response({"details": "Invalid data"}, 400)
+
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"])
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    task_response = Task.query.get(1)
+    return make_response(
+        {
+            "task": {
+                "id": task_response.id,
+                "title": task_response.title,
+                "description": task_response.description,
+                "is_complete": False
+                }
+            },
+        201
+    )
