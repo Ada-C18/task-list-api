32f91ee15936cdedec6989cf75db57fc9c171132
Younghee <kwak.younghee@gmail.com>
2022-11-05 18:00:01 -0700

    feat: register blueprints in app/__init__.py & helper function to_dict in app/models/task.py & get tasks (no data); test_get_tasks_no_saved_tasks passed

--- app/models/task.py
+++ app/models/task.py
@@ -5,3 +5,13 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime) # todo: nullable default=None?
+    is_complete: db.Column(db.Boolean) # False until wave 3
+
+    def to_dict(self):
+        return {
+            "task_id":self.task_id,
+            "title":self.title,
+            "description":self.description,
+            "completed_at": self.completed_at,
+            "is_complete": self.is_complete
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,20 @@
-from flask import Blueprint
+from flask import abort, Blueprint, jsonify, make_response, request
+from app import db
+
+from app.models.task import Task
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@task_bp.route('', methods=['GET'])
+def get_all_tasks():
+    rating_query_value = request.args.get("title") # has to be string
+    if rating_query_value is not None: # ! rather checking not None
+        tasks = Task.query.filter_by(rating=rating_query_value)
+    else:
+        tasks = Task.query.all()
+
+    result = []
+    for item in tasks:
+        result.append(item.to_dict())
+
+    return jsonify(result), 200
