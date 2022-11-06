c6483255d4c53dd19d79b20987c0c8614136113c
Soleil <soleilherring@gmail.com>
2022-11-06 11:28:24 -0800

    PATCH and POST slack updated code

--- app/task_routes.py
+++ app/task_routes.py
@@ -4,7 +4,9 @@ from app.models.task import Task
 from flask import Blueprint, jsonify, make_response, request, abort
 import datetime as dt
 import requests
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 tasks_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
@@ -129,7 +131,7 @@ def read_one_task(task_id):
 
 # Update Task
 @tasks_bp.route("/<task_id>", methods=["PUT"])
-def update_book(task_id):
+def update_task(task_id):
     task = validate_model(Task, task_id)
 
     request_body = request.get_json()
@@ -144,7 +146,7 @@ def update_book(task_id):
 # Delete Task: Deleting a Task
 
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
-def delete_book(task_id):
+def delete_task(task_id):
     task = validate_model(Task, task_id)
 
     db.session.delete(task)
@@ -155,12 +157,26 @@ def delete_book(task_id):
 ########################################
 #         PRACTICE  patch tasks/<task_id>mark_complete
 ########################################
+
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def updated_incomplete_task_to_complete(task_id):
     task = validate_model(Task, task_id)
     task.completed_at = (dt.date.today())
 
     db.session.commit()
+
+    slack_token = os.environ.get("SLACK_BOT_TOKEN")
+    path = "https://slack.com/api/chat.postMessage"
+    slack_channel = "task-notifications"
+    task_text = f"Someone just completed the task {task.title}"
+
+    query_params = {
+    "channel": slack_channel,
+    "text": task_text
+}
+
+    response = requests.post(path, params=query_params, headers={"Authorization" : slack_token})
+
     return {"task":task.to_dict()}, 200
 
 
