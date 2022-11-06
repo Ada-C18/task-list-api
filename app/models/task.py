773e264bc945b2c2e3deee387eb735a9b8294c01
Younghee <kwak.younghee@gmail.com>
2022-11-06 00:59:34 -0700

    style: to_dict output revised in task.py & json_sort_keys in create_app in __init__.py

--- app/models/task.py
+++ app/models/task.py
@@ -4,14 +4,22 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime) # todo: nullable default=None?
-    is_complete: db.Column(db.Boolean) # False until wave 3
+    completed_at = db.Column(db.DateTime, default=None) # todo: nullable default=None?
+    is_complete = db.Column(db.Boolean, default=False) # False until wave 3
 
     def to_dict(self):
-        return {
-            "task_id":self.task_id,
-            "title":self.title,
-            "description":self.description,
-            "completed_at": self.completed_at,
-            "is_complete": self.is_complete
-        }
+        if self.completed_at is not None: 
+            return {
+                "id":self.task_id,
+                "title":self.title,
+                "description":self.description,
+                "completed_at": self.completed_at,
+                "is_complete": self.is_complete
+            }
+        else:
+            return {
+                "id":self.task_id,
+                "title":self.title,
+                "description":self.description,
+                "is_complete": self.is_complete
+            }
