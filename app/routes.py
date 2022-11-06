2adbd092bb056b39b8e1999a9e598b800e30cd34
echo "PATH=/opt/homebrew/bin:$PATH\nexport PATH"  /Users/$(whoami)/.zprofile <PATH="/opt/homebrew/bin:$PATH>
2022-11-06 14:51:04 -0800

    Wave 4 passing yeahhhgit add app/models/task.py app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,6 @@ task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 @task_bp.route('', methods=['POST'])
 def create_one_task():
     request_body = request.get_json()
-
     if "title" not in request_body or "description" not in request_body:
         return jsonify({
             "details": "Invalid data"
@@ -27,12 +26,7 @@ def create_one_task():
     db.session.commit()
 
     return jsonify({ 
-        "task": {
-        "id": new_task.task_id,
-        "title": new_task.title,
-        "description": new_task.description,
-        "is_complete": False
-    }
+        "task": new_task.to_dict()
     }), 201 
 
 @task_bp.route('', methods=['GET'])
@@ -50,27 +44,16 @@ def get_all_tasks():
         tasks = Task.query.order_by(Task.title.desc()).all()
     else:
         tasks = Task.query.all()
-        
-    for task in tasks:
-        task_response.append({
-            "id":task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": False
-        })
-    return jsonify(task_response)
 
+    for task in tasks:
+        task_response.append(task.to_dict())
+    return jsonify(task_response)
 
 @task_bp.route('/<task_id>', methods=['GET'])
 def get_one_task(task_id):
     chosen_task = get_task_from_id(task_id)
     return jsonify ({ 
-        "task": {
-        "id": chosen_task.task_id,
-        "title": chosen_task.title,
-        "description": chosen_task.description,
-        "is_complete": False
-    }
+        "task": chosen_task.to_dict()
     })
 
 @task_bp.route("/<task_id>", methods=["PUT"])
@@ -82,22 +65,14 @@ def update_task(task_id):
     if "title" not in request_body or "description" not in request_body:
         return jsonify({"msg": "Request must include a title and description."}),400
 
-
     task.title = request_body["title"]
     task.description = request_body["description"]
 
     db.session.commit()
 
     return jsonify({
-        "task": {
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": False
-        }
-
+        "task": task.to_dict()
     })
-
 
 def get_task_from_id(task_id):
     try:
@@ -110,9 +85,6 @@ def get_task_from_id(task_id):
         return abort(make_response({"msg": f"Could not find task item with id: {task_id}"}, 404))
     return chosen_task
 
-
-
-
 #wave 3 creating a custom endpoint--mark a task as complete = True 
 @task_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def patch_a_complete_task(task_id):
@@ -120,29 +92,23 @@ def patch_a_complete_task(task_id):
     
     task = get_task_from_id(task_id)
     
-    task.is_complete = True
     task.completed_at = date.today()
 
     db.session.add(task)
     db.session.commit()
 
     SLACK_TOKEN = os.environ.get("MY_SLACK_TOKEN")
-    message = f"Someone just completed the task {task.title}"
-    headers = {"Authorization": "Bearer" + SLACK_TOKEN}
-    params = {"Channel": "task-notifications", "text": message}
+    query_params = {"text": f"Someone just completed the task {task.title}",
+    "channel": "task-notifications"}
 
-    request_to_slack = requests.post('url=https://slack.com/api/chat.postMessage',data=params,headers=headers)
+    headers = {"Authorization": "Bearer "+ SLACK_TOKEN }
+
+    request_to_slack = requests.post(url='https://slack.com/api/chat.postMessage',json=query_params, headers=headers)
     print(request_to_slack)
     print(request_to_slack.status_code)
 
     return jsonify({
-        "task": {
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": task.is_complete
-        }
-
+        "task": task.to_dict()
     }), 200
 
 #wave 3 creating a custom endpoint--mark a task as incomplete = False
@@ -150,17 +116,11 @@ def patch_a_complete_task(task_id):
 def patch_an_uncomplete_task(task_id):
     # a task is_complete=True when there is a datemine for the task's completed_at value.
     task = get_task_from_id(task_id)
-    task.is_complete = False
     task.completed_at = None #date.today()
     db.session.add(task)
     db.session.commit()
     return jsonify({
-        "task": {
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": task.is_complete
-        }
+        "task": task.to_dict()
     }),200
 
 @task_bp.route("/<task_id>", methods=["DELETE"])
