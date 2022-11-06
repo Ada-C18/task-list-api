e3fe3e562f746102a9f29d195488a51676b8e549
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2022-11-05 19:58:08 -0700

    passing 5 tests

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,7 @@ from app import db
 # import json
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     # confirm null is default if task has not been completed
@@ -12,7 +12,7 @@ class Task(db.Model):
 
     def to_dict(self):
         task_dict = {}
-        task_dict["id"] = self.task_id
+        task_dict["id"] = self.id
         task_dict["title"] = self.title
         task_dict["description"] = self.description
         task_dict["is_complete"] = False
@@ -22,3 +22,10 @@ class Task(db.Model):
         # return json.dumps(task_dict)
         return task_dict
         # task_dict["completed_at"] = self.completed_at
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"],
+                        description=task_data["description"])
+
+        return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,34 @@
 from app import db
 from app.models.task import Task
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort
 
 tasks_bp = Blueprint('tasks_bp', __name__, url_prefix='/tasks')
 
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
 #POST request
-@tasks_bp.route("",methods = ['POST'])
+@tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
+    if  not len(request_body) == 2:
+        return {"details": "Invalid data"}, 400
     
-    new_task = Task(title = request_body['title'],
-                    description = request_body['description'],
-                    completed_at = request_body["completed_at"])
-                        
-    
+    new_task= Task(title=request_body['title'], 
+        description=request_body['description'])
+
     db.session.add(new_task)
     db.session.commit()
 
-    # task_response = [task.to_dict() for task in new_task]
-    return make_response("CREATED", 201)
+    return make_response(jsonify({'task': new_task.to_dict()}), 201)
 
 #Get Tasks: Getting Saved Tasks
 @tasks_bp.route("", methods=["GET"])
@@ -30,4 +40,8 @@ def get_all_task():
 
     return make_response(jsonify(task_response), 200)
 
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return jsonify({'task': task.to_dict()})
 
