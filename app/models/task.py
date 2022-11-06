4b528e9df7baf684b103ebd1b621cf02e6916983
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2022-11-06 11:34:56 -0800

    Made make_task_dict a class method insstead of a helber function in routes

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,17 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    # helper function to structure request response json
+    def make_task_dict(self):
+        task_dict = {"id": self.task_id,
+        "title": self.title,
+        "description": self.description}
+
+        if not self.completed_at:
+            task_dict["is_complete"] = False
+        else:
+            task_dict["is_complete"] = True
+            # task_dict["completed_at"] = task.completed_at
+        
+        return task_dict
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, abort, make_response
 from app import db, Key
 from app.models.task import Task
+from app.models.goal import Goal
 import datetime
 import requests
 
@@ -8,7 +9,7 @@ import requests
 task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
 goal_bp = Blueprint("goal_bp", __name__, url_prefix="/goals")
 
-# helper function to structure request response json
+# # helper function to structure request response json
 def make_task_dict(task):
         task_dict = {"id": task.task_id,
         "title": task.title,
@@ -41,7 +42,7 @@ def add_task():
         db.session.add(new_task)
         db.session.commit()
 
-        new_task_dict = {"task": make_task_dict(new_task)}
+        new_task_dict = {"task": new_task.make_task_dict()}
         return jsonify(new_task_dict), 201
 
 @task_bp.route("", methods=["GET"])
@@ -52,7 +53,7 @@ def get_all_tasks():
 
     response = []
     for task in tasks:
-        task_dict = make_task_dict(task)
+        task_dict = task.make_task_dict()
         response.append(task_dict)
     
     if sort_param == "asc":
@@ -77,7 +78,7 @@ def validate_id(task_id):
 @task_bp.route("/<task_id>", methods = ["GET"])
 def get_one_task(task_id):
     selected_task = validate_id(task_id)
-    task_dict = make_task_dict(selected_task)
+    task_dict = selected_task.make_task_dict()
     response_dict = {"task": task_dict}
     return response_dict, 200
 
@@ -94,7 +95,7 @@ def update_task_with_new_vals(task_id):
     
     db.session.commit()
     
-    task_dict = make_task_dict(selected_task)
+    task_dict = selected_task.make_task_dict()
     response_dict = {"task": task_dict}
     return response_dict, 200
 
@@ -123,7 +124,7 @@ def mark_task_complete(task_id):
     requests.patch("https://slack.com/api/chat.postMessage", data=payload, headers=headers)
 
 
-    task_dict = make_task_dict(selected_task)
+    task_dict = selected_task.make_task_dict()
     response_dict = {"task": task_dict}
     return response_dict, 200
 
@@ -134,6 +135,30 @@ def mark_task_incomplete(task_id):
     selected_task.completed_at = None
     db.session.commit()
 
-    task_dict = make_task_dict(selected_task)
+    task_dict = selected_task.make_task_dict()
     response_dict = {"task": task_dict}
     return response_dict, 200
+
+
+"""
+GOAL ROUTES
+"""
+
+@goal_bp.route("", methods = ["GET"])
+def get_all_goals():
+    goals = Goal.query.all()
+
+    sort_param = request.args.get("sort")
+
+
+    response = []
+    for goal in goals:
+        task_dict = make_task_dict(task)
+        response.append(task_dict)
+    
+    if sort_param == "asc":
+        response = sorted(response, key=lambda i:i['title'])
+    if sort_param == "desc":
+        response = sorted(response, key=lambda i:i['title'], reverse=True)
+
+    return jsonify(response), 200
