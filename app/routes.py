fb8fbb155b19bb172ba12ac343350251e205d27c
takhat <takhat014@gmail.com>
2022-11-05 20:31:34 -0700

    Wave 2 complete: added query param: sort, added addl. tests to ignore case, passed all tests.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, jsonify, make_response, request, abort
 from app import db
 from app.models.task import Task
+from sqlalchemy import desc #(used in read_all_tasks for sorting)
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-#CREATE Routes
+#CREATE Routes (Wave 1: CRUD Routes)
 @tasks_bp.route("", methods=["POST"])
 def create_task():
         request_body = request.get_json()
@@ -16,15 +17,20 @@ def create_task():
         db.session.commit()
         return jsonify({"task": new_task.to_dict()}), 201
 
-#READ Routes
+#READ Routes (Wave 1: CRUD routes)
 @tasks_bp.route("", methods=["GET"])
 def read_all_tasks():
-    title_query_val = request.args.get("title")
-    date_query_val = request.args.get("completed_at")
-    if title_query_val is not None:
-        tasks = Task.query.filter_by(title=title_query_val)
-    elif date_query_val is not None:
-        tasks = Task.query.filter_by(completed_at=date_query_val)
+    #(Wave 2: Query param: sort)
+    if "sort"in request.args or "SORT" in request.args:
+        sort_query_val = request.args.get("sort") if "sort"in request.args else \
+            request.args.get("SORT") 
+        if sort_query_val.lower() == "asc":
+            tasks = Task.query.order_by(Task.title).all()
+        elif sort_query_val.lower() == "desc":
+            tasks = Task.query.order_by(Task.title.desc()).all() 
+            # Source: https://stackoverflow.com/questions/4186062/sqlalchemy-order-by-descending
+        else:
+            return jsonify({"msg": f"Invalid query value: {sort_query_val}"}), 400
     else:
         tasks = Task.query.all()
     tasks_response = []
@@ -33,7 +39,7 @@ def read_all_tasks():
     return jsonify(tasks_response), 200
 
 @tasks_bp.route("<task_id>", methods=["GET"])
-def read_task_by_id(task_id):
+def read_task_by_id(task_id): 
     task = validate_task(task_id)
     return jsonify({"task":task.to_dict()}), 200
 
@@ -51,7 +57,7 @@ def validate_task(id):
 
     return task
 
-#UPDATE Routes
+#UPDATE Routes (Wave 1: CRUD Routes)
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
 
@@ -71,7 +77,7 @@ def update_task(task_id):
             "is_complete": False if task.completed_at is None else True
         }}), 200
 
-# DELETE Routes    
+# DELETE Routes (Wave 1: CRUD Routes)
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_route(task_id):
     task = validate_task(task_id)
--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+#@pytest.mark.skip(reason="No way to test this feature yet")
 def test_get_tasks_sorted_asc(client, three_tasks):
     # Act
     response = client.get("/tasks?sort=asc")
@@ -27,12 +27,167 @@ def test_get_tasks_sorted_asc(client, three_tasks):
             "description": "",
             "is_complete": False}
     ]
+#@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_tasks_sorted_asc_ignore_case_SORT(client, three_tasks):
+    # Act
+    response = client.get("/tasks?SORT=asc")
+    response_body = response.get_json()
 
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "id": 2,
+            "title": "Answer forgotten email 📧",
+            "description": "",
+            "is_complete": False},
+        {
+            "id": 3,
+            "title": "Pay my outstanding tickets 😭",
+            "description": "",
+            "is_complete": False},
+        {
+            "id": 1,
+            "title": "Water the garden 🌷",
+            "description": "",
+            "is_complete": False}
+    ]
+#@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_tasks_sorted_asc_ignore_case_ASC(client, three_tasks):
+    # Act
+    response = client.get("/tasks?sort=ASC")
+    response_body = response.get_json()
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "id": 2,
+            "title": "Answer forgotten email 📧",
+            "description": "",
+            "is_complete": False},
+        {
+            "id": 3,
+            "title": "Pay my outstanding tickets 😭",
+            "description": "",
+            "is_complete": False},
+        {
+            "id": 1,
+            "title": "Water the garden 🌷",
+            "description": "",
+            "is_complete": False}
+    ]
+#@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_tasks_sorted_asc_ignore_case_SORT_ASC(client, three_tasks):
+    # Act
+    response = client.get("/tasks?SORT=ASC")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "id": 2,
+            "title": "Answer forgotten email 📧",
+            "description": "",
+            "is_complete": False},
+        {
+            "id": 3,
+            "title": "Pay my outstanding tickets 😭",
+            "description": "",
+            "is_complete": False},
+        {
+            "id": 1,
+            "title": "Water the garden 🌷",
+            "description": "",
+            "is_complete": False}
+    ]
+#@pytest.mark.skip(reason="No way to test this feature yet")
 def test_get_tasks_sorted_desc(client, three_tasks):
     # Act
     response = client.get("/tasks?sort=desc")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "description": "",
+            "id": 1,
+            "is_complete": False,
+            "title": "Water the garden 🌷"},
+        {
+            "description": "",
+            "id": 3,
+            "is_complete": False,
+            "title": "Pay my outstanding tickets 😭"},
+        {
+            "description": "",
+            "id": 2,
+            "is_complete": False,
+            "title": "Answer forgotten email 📧"},
+    ]
+
+#@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_tasks_sorted_desc_ignore_case_SORT(client, three_tasks):
+    # Act
+    response = client.get("/tasks?SORT=desc")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "description": "",
+            "id": 1,
+            "is_complete": False,
+            "title": "Water the garden 🌷"},
+        {
+            "description": "",
+            "id": 3,
+            "is_complete": False,
+            "title": "Pay my outstanding tickets 😭"},
+        {
+            "description": "",
+            "id": 2,
+            "is_complete": False,
+            "title": "Answer forgotten email 📧"},
+    ]
+#@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_tasks_sorted_desc_ignore_case_DESC(client, three_tasks):
+    # Act
+    response = client.get("/tasks?sort=DESC")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "description": "",
+            "id": 1,
+            "is_complete": False,
+            "title": "Water the garden 🌷"},
+        {
+            "description": "",
+            "id": 3,
+            "is_complete": False,
+            "title": "Pay my outstanding tickets 😭"},
+        {
+            "description": "",
+            "id": 2,
+            "is_complete": False,
+            "title": "Answer forgotten email 📧"},
+    ]
+#@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_tasks_sorted_desc_ignore_case_SORT_DESC(client, three_tasks):
+    # Act
+    response = client.get("/tasks?SORT=DESC")
     response_body = response.get_json()
 
     # Assert
