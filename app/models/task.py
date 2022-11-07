6bcf1da3cdafcf8958a0a037342c17a6a82ae8ff
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-11-07 10:42:26 -0500

    Create Migration

--- app/models/task.py
+++ app/models/task.py
@@ -3,3 +3,14 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.breed,
+            "is_complete": True if self.completed_at else False
+        }
