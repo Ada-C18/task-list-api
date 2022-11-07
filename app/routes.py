ec42cb6c6bd51eb7d7f79395121da5da474579e1
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-07 15:31:24 -0800

    created routes bp and validate_model()

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,19 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, abort, make_response, request
+
+bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response(
+            {"message": f"{cls.name} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response(
+            {"message": f"{cls.name} {model_id} not found"}, 404))
+
