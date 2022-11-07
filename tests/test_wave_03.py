e1a809981dafe8b218aa4b558c69ea029b024352
Puja Mukherjee <pujam836@gmail.com>
2022-11-06 22:12:31 -0800

    ran another test case

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -63,40 +63,40 @@ def test_mark_incomplete_on_complete_task(client, completed_task):
 
 
 # @pytest.mark.skip(reason="No way to test this feature yet")
-# def test_mark_complete_on_completed_task(client, completed_task):
-#     # Arrange
-#     """
-#     The future Wave 4 adds special functionality to this route,
-#     so for this test, we need to set-up "mocking."
+def test_mark_complete_on_completed_task(client, completed_task):
+    # Arrange
+    """
+    The future Wave 4 adds special functionality to this route,
+    so for this test, we need to set-up "mocking."
 
-#     Mocking will help our tests work in isolation, which is a
-#     good thing!
+    Mocking will help our tests work in isolation, which is a
+    good thing!
 
-#     We need to mock any POST requests that may occur during this
-#     test (due to Wave 4).
+    We need to mock any POST requests that may occur during this
+    test (due to Wave 4).
 
-#     There is no action needed here, the tests should work as-is.
-#     """
-#     with patch("requests.post") as mock_get:
-#         mock_get.return_value.status_code = 200
+    There is no action needed here, the tests should work as-is.
+    """
+    with patch("requests.post") as mock_get:
+        mock_get.return_value.status_code = 200
 
-#         # Act
-#         response = client.patch("/tasks/1/mark_complete")
-#     response_body = response.get_json()
+        # Act
+        response = client.patch("/tasks/1/mark_complete")
+    response_body = response.get_json()
 
-#     # Assert
-#     assert response.status_code == 200
-#     assert "task" in response_body
-#     assert response_body["task"]["is_complete"] == True
-#     assert response_body == {
-#         "task": {
-#             "id": 1,
-#             "title": "Go on my daily walk 🏞",
-#             "description": "Notice something new every day",
-#             "is_complete": True
-#         }
-#     }
-#     assert Task.query.get(1).completed_at
+    # Assert
+    assert response.status_code == 200
+    assert "task" in response_body
+    assert response_body["task"]["is_complete"] == True
+    assert response_body == {
+        "task": {
+            "id": 1,
+            "title": "Go on my daily walk 🏞",
+            "description": "Notice something new every day",
+            "is_complete": True
+        }
+    }
+    assert Task.query.get(1).completed_at
 
 
 # @pytest.mark.skip(reason="No way to test this feature yet")
