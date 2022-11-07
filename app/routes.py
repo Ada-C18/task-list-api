6387aa5b3d5d4f39e981bd7c31f11e9a76e4e83f
Evelyn <hay.evelyn@gmail.com>
2022-11-06 21:16:57 -0800

    added get one task route

--- app/routes.py
+++ app/routes.py
@@ -24,3 +24,9 @@ def handle_tasks():
         for task in tasks:
             response_body.append(task.to_dict())
         return make_response(jsonify(response_body), 200)
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def handle_task(task_id):
+    task = Task.query.get(task_id)
+
+    return make_response(jsonify(task.to_dict()))
