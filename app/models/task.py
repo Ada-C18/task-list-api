e92c33e336659273e1259fab5d7c7ab934cca0b1
Emily <sonemily+github@gmail.com>
2022-11-07 14:27:44 -0500

    Create Task model.

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,9 @@
 from app import db
+from datetime import datetime
 
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.string)
+    description = db.Column(db.string)
+    completed_at = db.Column(db.datetime, nullable=True, default=datetime.utcnow)
