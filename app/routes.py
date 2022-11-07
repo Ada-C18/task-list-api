76cc4d08daacbbaf09e05585d28f1bc8185583e0
Younghee <kwak.younghee@gmail.com>
2022-11-07 13:14:24 -0800

    test: test_delete_task passed

--- app/routes.py
+++ app/routes.py
@@ -61,7 +61,7 @@ def delete_one_task(task_id):
     db.session.delete(task_to_delete)
     db.session.commit()
 
-    return jsonify({"details": f"Task {task_to_delete.task_id} '{task_to_delete.description}' successfully deleted"}), 200
+    return jsonify({"details": f'Task {task_to_delete.task_id} "{task_to_delete.title}" successfully deleted'}), 200
 
 def get_task_from_id(cls, model_id):
     try:
