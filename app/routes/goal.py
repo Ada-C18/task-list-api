b23714f60c26fcf613dc0d85a92530346d6091ef
Annie <annie.gallagher7@gmail.com>
2022-11-07 15:04:28 -0700

    added DELETE method for goals

--- app/routes/goal.py
+++ app/routes/goal.py
@@ -53,3 +53,14 @@ def update_goal(goal_id):
     return make_response(jsonify({
         "goal": goal.to_dict()
     }))
+
+@bp.route("<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response(jsonify({
+        "details": f'Goal {goal_id} "{goal.title}" successfully deleted'
+    }))
