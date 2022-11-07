5ab5cde79858359f7caeff808b04fbe144ad23f5
Tami <tamiadetayo@gmail.com>
2022-11-07 02:32:40 -0800

    Defined tasks post and get routes

--- app/models/task.py
+++ app/models/task.py
@@ -3,3 +3,23 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, default=None)
+
+    def to_dict(self):
+        return {
+            "task_id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": False
+        }
+    
+    @classmethod
+    def from_dict(cls, task_dict):
+        return cls(
+            title=task_dict["title"],
+            description=task_dict["description"],
+            is_complete=False
+        )
+
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response({"task":f"{new_task}"}, 201)
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(task.to_dict())
+    return jsonify(tasks_response)
+
+def validate_task(task_id):
+    task = Task.query.get(task_id)
+   
+    if not task:
+        abort(make_response({"message":f"Task {task_id} not found"}, 404))
+    
+    return task
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    chosen_task = validate_task(task_id)
+
+    return chosen_task.to_dict()
+
+# @tasks.bp.route("/<task_id>", methods=["PUT"])
+# def 
