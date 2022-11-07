58967b0d80c39190e9dcdd6c6a5067a19334f7cc
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-11-07 10:42:07 -0500

    Add Read/Create. Update Task Model to add to_json

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,67 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from app.models.goal import Goal
+from flask import Blueprint, jsonify, make_response, request
+
+tasks_bp = Blueprint('tasks_bp', __name__, url_prefix='/tasks')
+
+@tasks_bp.route("", methods=["GET", "POST"])
+def handle_tasks():
+    if request.method == "GET":
+        tasks = Task.query.all()
+        tasks_response = [task.to_dict() for task in tasks]
+
+        if not tasks_response:
+            return make_response(jsonify(f"There are no tasks"))
+        return jsonify(tasks_response), 200
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+
+        new_task = Task(
+            title = request_body['title'],
+            description = request_body['description'],
+            completed_at = request_body['completed_at']
+        )
+
+        # Add this new instance of task to the database
+        db.session.add(new_task)
+        db.session.commit()
+
+        # Successful response
+        return make_response(f"Task {new_task.title} has been successfully created!", 201)
+
+# Path/Endpoint to get a single task
+# Include the id of the record to retrieve as a part of the endpoint
+@tasks_bp.route("/<task_id>", methods=["GET", "PUT", "DELETE"])
+
+# GET /task/id
+def handle_task(task_id):
+    # Query our db to grab the task that has the id we want:
+    task = task.query.get(task_id)
+
+    if request.method == "GET":
+        return {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "completed_at": task.completed_at
+        }
+    elif request.method == "PUT":
+        request_body = request.get_json()
+
+        # Updated task attributes are set:
+        task.title = request_body['title']
+        task.description = request_body['description']
+        task.completed_at = request_body['completed_at']
+
+        # Update this task in the database
+        db.session.commit()
+
+        # Sucessful response
+        return make_response(f"task {task.title} has been successfully updated!", 200)
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+
+        return make_response(f"task {task.title} successfully deleted", 202)
