bed1cf2f01737e6c8b3acd57f65ba9e5bdbaac81
ivystrayed <ivystrayed@gmail.com>
2022-11-07 10:39:09 -0800

    created post method and to dict helper function in the model init

--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,5 @@
 from app import db
+from flask import jsonify
 
 
 class Task(db.Model):
@@ -6,3 +7,20 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
+    
+
+
+    def is_complete(self):
+        if self.completed_at == None:
+            return False
+        else:
+            return True
+    
+    def to_dict(self):
+        task_dict = {
+            "id" :self.id,
+            "title" : self.title,
+            "description" : self.description, 
+            "is_complete" : self.is_complete()
+            }
+        return task_dict
--- app/routes.py
+++ app/routes.py
@@ -7,34 +7,35 @@ task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
 
 
 @task_bp.route("", methods=["GET"])
-def get_task():
-    task_list = Task.query.all()
+def get_all_task():
     response = []
-
+    task_list = Task.query.all()
     for task in task_list:
-        task_dict = {
-            "id" :task.id,
-            "title" : task.title,
-            "description" : task.description, 
-            "is_complete" : False
-        }
-        response.append(task_dict)
-
+        response.append(task.to_dict())
     return jsonify(response), 200
 
+@task_bp.route("/<id>", methods=["GET"])
+def get_one_task(id):
+    try:
+        task = Task.query.get(id)
+        response = {"task" : task.to_dict()}
+    except:
+        return jsonify("Task Not Found"), 404
+    return jsonify(response), 200
     
 
-
-
-@task_bp.route("", methods = ["POST"])
+@task_bp.route("", methods=["POST"])
 def add_task():
     request_body = request.get_json()
 
-    new_task = {
-        "title" : request_body["title"],
-        "description" : request_body["description"],
-    }
+    new_task = Task (title = request_body["title"],
+        description = request_body["description"]) 
 
     db.session.add(new_task)
     db.session.commit()
+
+    return make_response({"task" : new_task.to_dict()}, 201)
+
+    
+
 
