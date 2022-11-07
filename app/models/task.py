0f0d9c359201b36f606a17e30a3c99c745e8717f
Maxwell <maxpasqualis@gmail.com>
2022-11-07 15:59:33 -0800

    adjusted Task model to better fit requirements

--- app/models/task.py
+++ app/models/task.py
@@ -4,18 +4,19 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+    is_complete = db.Column(db.Boolean)
     completed_at = db.Column(db.DateTime, nullable=True)
 
     def as_dict(self):
         task_dict = {
             "id": self.task_id,
             "title": self.title,
-            "description": self.description
+            "description": self.description,
+            "is_complete": self.is_complete
         }
-        if not self.completed_at:
+        if not self.is_complete:
             task_dict["is_complete"] = False
-        else:
-            task_dict["completed_at"] = self.completed_at
+
         return task_dict
 
     @classmethod
@@ -23,9 +24,9 @@ class Task(db.Model):
         if "title" in input_dict and "description" in input_dict:
             new_task = cls(title=input_dict["title"], 
                 description=input_dict["description"])
-        if "completed_at" in input_dict:
-            new_task.completed_at = input_dict["completed_at"]
+        if "is_complete" in input_dict:
+            new_task.is_complete = input_dict["is_complete"]
         else:
-            new_task.completed_at = None
+            new_task.is_complete = False
         
         return new_task
