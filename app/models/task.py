969e3118564eb193defa93873c657d5a410d37fa
Tiffany Tran <tif.tran86@gmail.com>
2022-11-07 09:42:41 -0800

    complete wave 01

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,30 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime)
+    is_complete = db.Column(db.Boolean, default=False)
+
+    def create_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.task_id  
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["is_complete"] = self.is_complete
+        
+        return task_as_dict
+
+    def update(self,req_body):
+        self.title = req_body["title"]
+        self.description = req_body["description"]
+
+    @classmethod 
+    def new_instance_from_dict(cls, req_body):
+        new_dict = cls(
+                        title = req_body["title"],
+                        description = req_body["description"],
+                        # is_complete = req_body["is_complete"]
+                        )
+        return new_dict
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,76 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request, abort
+from .models.task import Task
+from app import db
+
+# ===================
+# BLUEPRINTS
+# ===================
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# ===================
+# HELPER FUNCTIONS
+# ===================
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message": f"Task {task_id} invalid"}, 400))
+
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message": f"Task {task_id} not found"}, 404))
+
+    return task
+
+# ===================
+# ROUTES
+# ===================
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    try:
+        request_body = request.get_json()
+        new_task = Task.new_instance_from_dict(request_body)
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        return {"task": new_task.create_dict()}, 201
+        
+    except KeyError:
+        return {"details": "Invalid data"}, 400
+
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = [task.create_dict() for task in tasks]
+
+    return jsonify(tasks_response)
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def get_one_task(task_id):
+    task = validate_task(task_id)
+    response = {"task": task.create_dict()}
+    return make_response(response)
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+    task.update(request_body)
+
+    db.session.commit()
+
+    return {"task": task.create_dict()}
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return {"details": f"Task {task.task_id} \"{task.title}\" successfully deleted"}
