40fa594379687229eb7e9d585a73927b576b3784
Evelyn <hay.evelyn@gmail.com>
2022-11-06 21:07:43 -0800

    made get and post routes

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,20 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        task_dict = {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+        }
+
+        if self.completed_at == None:
+            task_dict["is_complete"] = False
+
+        return task_dict
+
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST", "GET"])
+def handle_tasks():
+    if request.method == "POST":
+        request_body = request.get_json()
+    
+        task = Task(title = request_body["title"], description = request_body["description"])
+
+        db.session.add(task)
+        db.session.commit()
+
+        response_body = {"task": task.to_dict()}
+
+        return make_response(jsonify(response_body), 201)
+
+    elif request.method == "GET":
+        tasks = Task.query.all()
+        response_body = []
+        for task in tasks:
+            response_body.append(task.to_dict())
+        return make_response(jsonify(response_body), 200)
