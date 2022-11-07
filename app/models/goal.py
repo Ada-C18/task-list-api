d73049ab70bd7667692c33edc5c2daac7a3a47d6
Anjula Tamang <parkhrinj@gmail.com>
2022-11-06 21:58:34 -0500

    started Wave 5

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,12 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+
+
+    def to_dict(self):
+        goal_dict = {}
+        goal_dict["id"] = self.id
+        goal_dict["title"] = self.title
+    
+        return goal_dict
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from flask import Blueprint, jsonify, make_response, request, abort
 from sqlalchemy import asc, desc
 
@@ -116,3 +117,52 @@ def delete_task(task_id):
     db.session.commit()
 
     return {"details": f'Task {task.id} "{task.title}" successfully deleted'}, 200
+
+# **************************************(for goals)
+goals_bp = Blueprint('goals_bp', __name__, url_prefix='/goals')
+
+@goals_bp.route("", methods=["GET"])
+def get_all_goals():
+    all_goals = Goal.query.all()
+    goal_response = [goal.to_dict() for goal in all_goals]
+
+    return make_response(jsonify(goal_response), 200)
+
+def validate_goal(goal_id):
+    try:
+        goal_id = int(goal_id)
+    except:
+        abort(make_response({"message":f"goal {goal_id} invalid"}, 400))
+
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        abort(make_response({"message":f"Goal {goal_id} not found"}, 404))
+
+    return goal
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def handle_goal(goal_id):
+    goal = validate_goal(goal_id)
+
+    return {"goal": {
+    "id": goal.id,
+    "title": goal.title}
+}
+
+@goals_bp.route("", methods=["POST"])
+
+def create_goal():
+    request_body = request.get_json()
+
+    # guard clause
+    if "title" not in request_body:
+        return {"details": "Invalid data"}, 400
+    new_goal= Goal(
+        title=request_body['title']
+       )
+    
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return make_response(jsonify({'goal': new_goal.to_dict()}), 201)
