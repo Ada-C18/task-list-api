7b36d05d996604fbd2ade185b1323595a1ffa790
Carina <checarina@gmail.com>
2022-11-07 15:16:41 -0800

    added route to get task by ID

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,31 @@
-from flask import Blueprint, request, make_response, jsonify
+from flask import Blueprint, request, make_response, jsonify, abort
 from app import db
 from app.models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-# def make_task_dict(task):
-    
-#     task_dict = {
-#         "id": task.task_id,
-#         "title": task.title,
-#         "description": task.description,
-#     }
-
-
-
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+    model = cls.query.get(model_id)
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+    return model
     
 @tasks_bp.route("", methods = ["GET"])
 def get_all_tasks():
     tasks = Task.query.all()
     tasks_response = []
     for task in tasks:
-        # tasks_response.append({
-        #     "id": task.task_id,
-        #     "title": task.title,
-        #     "description": task.description,
-        #     "is_complete": False
-        # })
         tasks_response.append(task.to_dict())
     return jsonify(tasks_response)
+
+@tasks_bp.route("/<task_id>", methods = ["GET"])
+def get_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return make_response(jsonify({"task": task.to_dict()}), 200)
 
 @tasks_bp.route("", methods = ["POST"])
 def create_task():
