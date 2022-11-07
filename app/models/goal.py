4c22a5a0ab07dc438a7f28c4aa16547d0b4d8ad6
Annie <annie.gallagher7@gmail.com>
2022-11-07 14:22:35 -0700

    added title to Goal and added to_dict and from_dict

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,19 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+
+    def to_dict(self):
+        goal_as_dict = {
+        "id": self.goal_id,
+        "title": self.title,
+        }
+
+        return goal_as_dict
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = cls(title=task_data["title"],
+                        )
+
+        return new_task
