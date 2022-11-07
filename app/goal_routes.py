a25a022eed1049a987318c02d0c2e3a1c6e8c029
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-11-07 15:44:06 -0800

    added new POST route create_one_goal and new function validate_goal

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -2,3 +2,26 @@ from app import db
 from app.models.goal import Goal
 from flask import abort, Blueprint, jsonify, make_response, request
 import os
+
+goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
+
+def validate_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        abort(make_response({"msg":f"Task {goal_id} not found"}, 404))
+
+    return goal
+
+@goals_bp.route("", methods=['POST'])
+def create_one_goal():
+    request_body = request.get_json()
+    try:
+        new_goal=Goal(
+            title=request_body["title"]
+        )
+    except:
+        return abort(make_response({"details": "Invalid data"}, 400))
+    db.session.add(new_goal)
+    db.session.commit()
+    return jsonify({"goal":new_goal.to_dict()}), 201
