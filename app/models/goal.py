b548ed4a448a62ad11d457e72f082d1cf5f02718
Andrea-Galindo <andreagalindo.guerra@gmail.com>
2022-11-07 15:05:29 -0500

    fixed Goal model

--- app/models/goal.py
+++ app/models/goal.py
@@ -7,18 +7,18 @@ class Goal(db.Model):
     title = db.Column(db.String)
 
 
-@classmethod
-def from_dict(cls, data_dict):
-    return cls(title=data_dict["title"])
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(title=data_dict["title"])
 
-def to_dict(self):
-    return {
-        "id": self.id,
-        "title": self.title    
-    }
+    def to_dict(self):
+        return {
+            "id": self.goal_id,
+            "title": self.title    
+        }
 
-def update(self,req_body):
-    try:
-        self.title = req_body["title"]
-    except KeyError:
-        abort(make_response(jsonify(dict(details="Invalid data")), 400))
+    def update(self,req_body):
+        try:
+            self.title = req_body["title"]
+        except KeyError:
+            abort(make_response(jsonify(dict(details="Invalid data")), 400))
