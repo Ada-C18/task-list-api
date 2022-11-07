e21c0a7dfddb2a5d778d202ff16f2af2b6d27219
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-07 15:40:32 -0800

    added to_dict feature

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,10 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "description": self.description,
+            "completed_at": self.completed_at
+        }
