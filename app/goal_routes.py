25d543e8c3141b5b03db61f191f4e2ab616a5b00
Intesar Siraj <ghideruk@gmail.com>
2022-11-06 17:20:02 -0800

    goal_routes, created helper function with status code 400 and 404

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -5,5 +5,17 @@ from datetime import datetime
 import os  
 import requests
 
-
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
+
+def validate_goals(goal_id):
+    try:
+        goal_id = int(goal_id )
+    except:
+        abort(make_response({"message":f"{goal_id } invalid"}, 400))
+
+    goal = Goal.query.get(goal_id )
+
+    if not goal:
+        abort(make_response({"message":f"goal{goal_id} not found"}, 404))
+    
+    return goal
