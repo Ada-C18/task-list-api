2a02e92f9dfc1eda72c8449a5db4b606b6bdee5d
Annie <annie.gallagher7@gmail.com>
2022-11-07 14:23:28 -0700

    completed POST method for goal

--- app/routes/goal.py
+++ app/routes/goal.py
@@ -1,8 +1,21 @@
 from app import db
-from app.models.task import Task
+from app.models.goal import Goal
 from flask import Blueprint, jsonify, request, make_response, abort
-from datetime import datetime as dt
+from app.routes.task import validate_model
 import requests
 import os
 
 bp = Blueprint("goal_bp", __name__, url_prefix="/goals")
+
+@bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+    new_goal = Goal.from_dict(request_body)
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    goal_dict = new_goal.to_dict()
+
+    return make_response(jsonify({
+        "goal": goal_dict}), 201)
