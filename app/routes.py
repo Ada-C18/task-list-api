daaae0c9dfd23aaf7ed53a285bb2940fd6f248ec
Nina <nina.sohn@gmail.com>
2022-11-08 13:50:39 -0800

    Wave 5 complete! Created routes for Goal model, registered bp, and updated validate id function

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, make_response, jsonify, abort
 from app.models.task import Task
+from app.models.goal import Goal
 from app import db
 from datetime import datetime
 import requests, os
@@ -43,13 +44,13 @@ def read_all_tasks():
 
 @task_bp.route("/<task_id>", methods=["GET"])
 def read_one_task(task_id):
-    task = validate_task_id(task_id)
+    task = validate_model_id(Task, task_id)
 
     return make_response(jsonify({"task": task.to_dict()})), 200
 
 @task_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
-    task = validate_task_id(task_id)
+    task = validate_model_id(Task, task_id)
 
     request_body = request.get_json()
 
@@ -63,7 +64,7 @@ def update_task(task_id):
 
 @task_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def mark_task_complete(task_id):
-    task = validate_task_id(task_id)
+    task = validate_model_id(Task, task_id)
 
     task.completed_at = datetime.utcnow()
 
@@ -84,7 +85,7 @@ def mark_task_complete(task_id):
 
 @task_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_task_incomplete(task_id):
-    task = validate_task_id(task_id)
+    task = validate_model_id(Task, task_id)
 
     task.completed_at = None
 
@@ -95,25 +96,79 @@ def mark_task_incomplete(task_id):
 
 @task_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
-    task = validate_task_id(task_id)
+    task = validate_model_id(Task, task_id)
 
     db.session.delete(task)
     db.session.commit()
 
     return make_response(jsonify({"details": f"Task {task_id} \"{task.title}\" successfully deleted"}), 200)
 
-#helper function
-def validate_task_id(task_id):
+###################helper function############################
+def validate_model_id(cls, model_id):
     try:
-        task_id = int(task_id)
+        model_id = int(model_id)
     except:
-        abort(make_response({"message": f"task {task_id} invalid"}, 400))
+        abort(make_response({"message": f"{cls.__name__} {model_id} invalid"}, 400))
     
-    task = Task.query.get(task_id)
+    chosen_object = cls.query.get(model_id)
 
-    if not task:
-        abort(make_response({"message": f"task {task_id} not found"}, 404))
+    if not chosen_object:
+        abort(make_response({"message": f"{cls.__name__} {model_id} not found"}, 404))
     
-    return task
+    return chosen_object
 
 
+#########goal routes############
+
+goal_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+@goal_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+
+    try:
+        new_goal = Goal(title=request_body["title"])
+    except KeyError:
+        return jsonify({"details": "Invalid data"}), 400
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return make_response(jsonify({"goal": new_goal.to_dict()}), 201)
+
+@goal_bp.route("", methods=["GET"])
+def read_all_goals():
+    goals = Goal.query.all()
+
+    goals_response = []
+    for goal in goals:
+        goals_response.append(goal.to_dict())
+    
+    return jsonify(goals_response), 200
+
+@goal_bp.route("/<goal_id>", methods=["GET"])
+def read_one_goal(goal_id):
+    goal = validate_model_id(Goal, goal_id)
+
+    return make_response(jsonify({"goal": goal.to_dict()})), 200
+
+@goal_bp.route("/<goal_id>", methods=["PUT"])
+def update_goal(goal_id):
+    goal = validate_model_id(Goal, goal_id)
+
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+
+    db.session.commit()
+
+    return make_response(jsonify({"goal": goal.to_dict()}), 200)
+
+@goal_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_model_id(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response(jsonify({"details": f"Goal {goal_id} \"{goal.title}\" successfully deleted"}), 200)
