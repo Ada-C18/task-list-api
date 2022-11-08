bc23364d712e29b6495f2c4715838ee39f776936
Minh <minh.seikel@gmail.com>
2022-11-08 15:03:55 -0800

    updating goal to_dict()

--- app/models/task.py
+++ app/models/task.py
@@ -6,17 +6,27 @@ class Task(db.Model):
     title = db.Column(db.String,)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True )#default = None 
-    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable = True)
     goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
-        return {
+        # return {
+        #     "id": self.task_id,
+        #     "title": self.title,
+        #     "description": self.description,
+        #     "is_complete": True if self.completed_at else False                                              
+        # }
+        dict = {        
             "id": self.task_id,
             "title": self.title,
             "description": self.description,
             "is_complete": True if self.completed_at else False                                              
         }
-    
+        
+        if self.goal_id:
+            dict["goal_id"] = self.goal_id
+        return dict
+            
     def to_dict_goal_id(self):
         dict = self.to_dict()
         dict["goal_id"] = self.goal_id
