7927a411c765d88a024304f6ad1ed984c03a1a15
Irene <choireneh@gmail.com>
2022-11-07 22:24:59 -0500

    Updated model, added post route, validate model

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,17 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True, default = None)
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(title = data_dict["title"], 
+        description = data_dict["description"],
+        completed_at = data_dict["completed_at"])
+    
+    def to_dict(self):
+        return dict(
+            task_id = self.task_id,
+            title = self.title,
+            description = self.description,
+            completed_at = self.completed_at
+        )
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from .models.task import Task
+
+bp = Blueprint("tasks", __name__, url_prefix="/planets")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
+
+@bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"Task {new_task.name} successfully created", 201)
