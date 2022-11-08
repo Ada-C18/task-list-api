b6c0e19272614b06eee120e65027d4b797b6683c
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-07 16:18:20 -0800

    added from_dict() class method

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,5 @@
 from app import db
 
-
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -9,7 +8,15 @@ class Task(db.Model):
 
     def to_dict(self):
         return {
+            "id": self.task_id,
             "title": self.title,
             "description": self.description,
             "completed_at": self.completed_at
         }
+
+    @classmethod
+    def from_dict(cls, task_data):
+        return cls(
+            title=task_data["title"],
+            description=task_data["description"],
+            completed_at=task_data["completed_at"])
