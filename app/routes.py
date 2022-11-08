00fbfded75d9ebcc4a33c7a817545c8dc8c719d5
Intesar Siraj <ghideruk@gmail.com>
2022-11-08 13:01:25 -0800

    passed delete_task test, modified my 200 status code response :)

--- app/routes.py
+++ app/routes.py
@@ -24,11 +24,6 @@ def validate_tasks(task_id):
 
 @tasks_bp.route("", methods=["POST"])
 def create_tasks():
-
-    # request_body = request.get_json()
-    # title = request.json.get("title", None)
-    # description = request.json.get("description", None)
-    
     try:
         request_body = request.get_json()
         new_task = Task(
@@ -85,7 +80,8 @@ def delete_task(task_id):
     db.session.delete(task)
     db.session.commit()
 
-    return make_response(f"Task# {task_id} successfully deleted"), 200
+    return make_response({"details": f"Task {task_id} \"{task.title}\" successfully deleted"}, 200)
+    
 
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def mark_complete(task_id):
@@ -118,74 +114,4 @@ def mark_incomplete(task_id):
     response_body["task"] = task.to_dict()
     
     return jsonify(response_body), 200
-
-
-# goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
-
-# def validate_goals(goal_id):
-#     try:
-#         goal_id = int(goal_id )
-#     except:
-#         abort(make_response({"message":f"{goal_id } invalid"}, 400))
-
-#     goal = Goal.query.get(goal_id )
-
-#     if not goal:
-#         abort(make_response({"message":f"goal{goal_id} not found"}, 404))
-    
-#     return goal
-
-# @goals_bp.route("", methods=["POST"])
-# def create_goal():
-#     request_body = request.get_json()
-#     try:
-#         new_goal= Goal(title=request_body["title"])
-#     except KeyError:
-#         return jsonify({"details": "Invalid data"}), 400
-
-#     db.session.add(new_goal)
-#     db.session.commit()
-    
-#     return jsonify({"goal": new_goal.to_dict()}), 201
-
-# @goals_bp.route("", methods=["GET"])
-# def read_all_goals():
-#     goal_param = request.args
-#     goals = Goal.query.all()
-#     goals_response = []
-#     for goal in goals:
-#         goals_response.append({
-#             "id": goal.goal_id,
-#             "title": goal.title
-#         })
-
-#     return jsonify(goals_response)
-
-# @goals_bp.route("/<goal_id>", methods=["GET"])
-# def read_one_goal(goal_id):
-#     goal = validate_goals(goal_id)
-#     return goal.to_dict()
-
-
-# @goals_bp.route("/<goal_id>", methods=["PUT"])
-# def update_goal(goal_id):
-#     goal = validate_goals(goal_id)
-#     request_body = request.get_json()
-#     goal.title = request_body["title"]
-#     db.session.commit()
-#     return goal.to_dict()
-
-# @goals_bp.route("/<goal_id>", methods=["DELETE"])
-# def delete_goal(goal_id):
-#     goal = validate_goals(goal_id)
-#     db.session.delete(goal)
-#     db.session.commit()
-
-#     return {"details": f"Task {goal.goal_id} \"{goal.title}\" successfully deleted"}
-
-
-
-
-
-
 
