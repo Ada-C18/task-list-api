0d88cd2799d97ea0197d7a02213d3242cb70c8f1
Maxwell <maxpasqualis@gmail.com>
2022-11-08 15:03:52 -0800

    added POST route for goals

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -4,3 +4,16 @@ from .route_helpers import validate_model, send_slack_message
 from flask import Blueprint, jsonify, abort, make_response, request
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
+
+@goals_bp.route("", methods=["POST"])
+# Creates a new goal and returns it as json
+def create_new_goal():
+    request_body = request.get_json()
+    if "title" in request_body:
+        new_goal = Goal.from_dict(request_body)
+
+        db.session.add(new_goal)
+        db.session.commit()
+        return {"goal": new_goal.as_dict()}, 201
+    else:
+        return {"details": "Invalid data"}, 400
