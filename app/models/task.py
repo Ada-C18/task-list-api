18b4451831f4cb95329d5c05d34b293725432643
Lee Reyes <reyeslee94@gmail.com>
2022-11-07 17:00:28 -0800

    completed wave 1 and 2

--- app/models/task.py
+++ app/models/task.py
@@ -2,9 +2,9 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    description = db.Column(db.String)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable = False)
+    description = db.Column(db.String, nullable = False)
     completed_at = db.Column(db.DateTime, nullable=True)
 
     def to_dict(self):
--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,9 @@ def validate_task(task_id):
     try:
         task_id = int(task_id)
     except:
-        abort(make_response({"message":f"task {task_id} invalid"}, 400))
+        abort(make_response({"details":f"Invalid data"}, 400))
 
+    
     task = Task.query.get(task_id)
 
     if not task:
@@ -21,20 +22,35 @@ def validate_task(task_id):
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
+    # handles if title or description not included in post
+    if "title" not in request_body or "description" not in request_body:
+        return make_response({"details":"Invalid data"},400)
+
     new_task = Task(title=request_body["title"],
-                    description=request_body["description"],
-                    completed_at=request_body["completed_at"])
+                    description=request_body["description"])
 
     db.session.add(new_task)
     db.session.commit()
 
-    return make_response(jsonify(f"Task {new_task.title} successfully created", 201))
+    task_response = {"task": new_task.to_dict()}
+
+
+    return make_response(jsonify(task_response), 201)
 
 # Returns all tasks
 @tasks_bp.route("", methods=["GET"])
 def return_all_tasks():
     tasks_response = []
     tasks = Task.query.all()
+
+
+    sort_by_alpha_title_query = request.args.get("sort")
+
+    if sort_by_alpha_title_query == "asc":
+        tasks = Task.query.order_by(Task.title.asc()).all()
+    if sort_by_alpha_title_query == "desc":
+        tasks = Task.query.order_by(Task.title.desc()).all()
+
     for task in tasks:
         tasks_response.append(task.to_dict())
     return make_response(jsonify(tasks_response),200)
@@ -43,6 +59,37 @@ def return_all_tasks():
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def read_one_task(task_id):
     task = validate_task(task_id)
-    task_response = {task: task.to_dict()}
+    task_response = {"task": task.to_dict()}
     return jsonify(task_response), 200
-        
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_tasks(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    db.session.commit()
+ 
+    task_response = {"task": task.to_dict()}
+
+
+    return make_response(jsonify(task_response), 200)
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+    task_response = {"details": f'Task {task_id} "{task.title}" successfully deleted'}
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify(task_response), 200
+
+# wave 3 
+@tasks_bp.route("/<task_id>", methods = ["PATCH"])  
+def partial_task_update(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+
+
+
+
