f8aa01a1486593646edc28ec3c80788fa5c6227d
Emily <sonemily+github@gmail.com>
2022-11-08 11:21:21 -0500

    Pass Wave 1.

--- app/task_routes.py
+++ app/task_routes.py
@@ -17,26 +17,91 @@ def validate_model(cls, model_id):
     
     return model
 
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    try:
+        new_task = Task.from_dict(request_body)
+    except KeyError:
+        return make_response(jsonify({
+            "details": "Invalid data"
+        }), 400)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    task_response = {
+        "task": new_task.to_dict()
+    }
+
+    return make_response(jsonify(task_response), 201)
+
+
 @tasks_bp.route("", methods=["GET"])
 def read_all_tasks():
-
     title_query = request.args.get("title")
     description_query = request.args.get("description")
-    completed_at_query = request.args.get("completed_by")
-
+    completed_at_query = request.args.get("completed_at")
+    limit_query = request.args.get("limit")
+    
     task_query = Task.query
 
     if title_query:
-        tasks = Task.query.filter_by(title=title_query)
+        tasks = task_query.filter_by(title=title_query)
     
     if description_query:
-        tasks = Task.query.filter_by(description=description_query)
+        tasks = task_query.filter_by(description=description_query)
     
     if completed_at_query:
-        tasks = Task.query.filter_by(completed_by=completed_at_query)
+        tasks = task_query.filter_by(completed_at=completed_at_query)
+
+    if limit_query:
+        tasks = task_query.limit(limit_query)
 
     tasks = task_query.all()
 
     tasks_response = [task.to_dict() for task in tasks]
 
     return jsonify(tasks_response)
+
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = validate_model(Task, task_id)
+    task_response = {
+        "task": task.to_dict()
+    }
+
+    return make_response(jsonify(task_response), 200)
+
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    task_response = {
+        "task": task.to_dict()
+    }
+
+    return make_response(jsonify(task_response), 200)
+
+@tasks_bp.route("<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    # validate, commit, make response
+    task = validate_model(Task, task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    task_response = {
+        "details": f'Task {task_id} "{task.title}" successfully deleted'
+    }
+
+    return make_response(jsonify(task_response), 200)
+
