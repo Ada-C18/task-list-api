68358a6453bea0b20b38d56b4386acfb823ddd63
JuliaPark <pje106@gmail.com>
2022-11-08 14:40:19 -0800

    wave 1 : CRUD for One model

---
+++ app/routes_helper.py
@@ -0,0 +1,14 @@
+from flask import  jsonify, abort, make_response
+
+def get_one_obj_or_abort(cls, obj_id):
+    try:
+        obj_id = int(obj_id)
+    except ValueError:
+        abort(make_response(jsonify({"details": "Invalid data"}), 400))
+    
+    matching_obj = cls.query.get(obj_id)
+
+    if not matching_obj:
+        response_str = f"{cls.__name__} with id `{obj_id}` was not found in the database."
+        abort(make_response(jsonify({"message":response_str}), 404))
+    return matching_obj
