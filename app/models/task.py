6e08fc6e4bbdaf5c84db7b0b5cb50be488a2d895
Irene <choireneh@gmail.com>
2022-11-08 14:26:50 -0500

    Refactored and fixed some bugs

--- app/models/task.py
+++ app/models/task.py
@@ -2,21 +2,30 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True, default = None)
 
     @classmethod
     def from_dict(cls, data_dict):
-        return cls(title = data_dict["title"], 
-        description = data_dict["description"],
-        completed_at = data_dict["completed_at"])
+        if "completed_at" in data_dict:
+            return cls(title = data_dict["title"], 
+            description = data_dict["description"],
+            completed_at = data_dict["completed_at"])
+        else:
+            return cls(title = data_dict["title"], 
+            description = data_dict["description"])
+
     
     def to_dict(self):
+        if self.completed_at:
+            flag = True
+        else:
+            flag = False
         return dict(
-            task_id = self.task_id,
+            id = self.id,
             title = self.title,
             description = self.description,
-            completed_at = self.completed_at
+            is_complete = flag
         )
--- app/routes.py
+++ app/routes.py
@@ -16,12 +16,71 @@ def validate_model(cls, model_id):
 
     return model
 
+def validate_task_dict(request_body):
+    request_body = dict(request_body)
+    if not (request_body.get("title", False) and request_body.get("description", False)):
+        abort(make_response({'details': 'Invalid data'}, 400))
+    # if request_body.get("copmleted_at", 1) == 1:
+    #     abort(make_response({'details': 'Invalid data'}, 400))
+
 @bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
+    validate_task_dict(request_body)
     new_task = Task.from_dict(request_body)
 
     db.session.add(new_task)
     db.session.commit()
+    
 
-    return make_response(f"Task {new_task.name} successfully created", 201)
+    return make_response({"task": new_task.to_dict()}, 201)
+
+@bp.route("", methods=["GET"])
+def read_all_tasks():
+    title_query = request.args.get("title")
+    description_query = request.args.get("description")
+    limit_query = request.args.get("limit")
+
+    task_query = Task.query
+
+    if title_query:
+        cat_query = cat_query.filter_by(personality=title_query)
+
+    if description_query:
+        cat_query = cat_query.filter_by(color=description_query)
+
+    if limit_query:
+        cat_query = cat_query.limit(limit_query)
+
+    tasks = task_query.all()
+
+    tasks_response = [task.to_dict() for task in tasks]
+
+    return jsonify(tasks_response)
+
+@bp.route("/<id>", methods=["GET"])
+def read_one_task(id):
+    task = validate_model(Task, id)
+    return make_response({"task": task.to_dict()}, 200)
+
+
+@bp.route("/<id>", methods=["PUT"])
+def update_task(id):
+    task = validate_model(Task, id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    # task.completed_at = request_body["completed_at"]
+
+    db.session.commit()
+
+    return make_response({"task": task.to_dict()}, 200)
+
+@bp.route("/<id>", methods=["DELETE"])
+def delete_task(id):
+    task = validate_model(Task, id)
+    db.session.delete(task)
+    db.session.commit()
+    # return make_response(f"Task #{task.id} successfully deleted"), 200
+    return make_response({"details":f"Task {task.id} \"{task.title}\" successfully deleted"}, 200)
