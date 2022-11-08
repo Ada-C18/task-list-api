6dae171a0bd2c90d212557b67d5bf94052a37790
Rose Le <rosehle@protonmail.com>
2022-11-08 14:28:50 -0800

    Completed create_new_task function

--- app/models/task.py
+++ app/models/task.py
@@ -3,3 +3,20 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String) 
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        task_to_dict = {}
+        task_to_dict["id"] = self.task_id
+        task_to_dict["title"] = self.title
+        task_to_dict["description"] = self.description
+        task_to_dict["completed_at"] = self.completed_at
+
+        return task_to_dict
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"], description=task_data["description"], completed_at=task_data["completed_at"])
+        return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_new_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"Title {new_task.title} successfully created", 201)
