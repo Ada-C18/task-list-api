6f88851eae9c2cfa69348fc10d30612a9771dcf4
Brooke hill <brookeannstasia@gmail.com>
2022-11-07 19:28:27 -0500

    Done

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+#client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 
 
