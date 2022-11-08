c78c0543cf5a0661add32d0c7b3390b1df79a3b3
hang tran <hangmtran101@gmail.com>
2022-11-07 20:35:23 -0800

    goal update/delete/get/goal_id route added

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -8,7 +8,7 @@ from app.models.task import Task
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
-def validate_task(goal_id):
+def validate_goal(goal_id):
     try:
         goal_id = int(goal_id)
     except:
@@ -27,14 +27,19 @@ def validate_task(goal_id):
 @goals_bp.route("", methods=["POST"])
 def create_goal():
     request_body = request.get_json()
+    if "title" not in request_body:
+        return make_response({"details": "Invalid data"},400)
 
     new_goal = Goal.from_dict(request_body)
 
     db.session.add(new_goal)
     db.session.commit()
+    
+    response = {"goal": new_goal.to_dict()}
 
-    return{"goal":Goal.to_dict},201
-
+    return make_response(jsonify(response)), 201 
+   
+    
 
 @goals_bp.route("", methods=["GET"])
 def get_goals():
@@ -44,3 +49,37 @@ def get_goals():
 
   
     return jsonify(goal_response), 200
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def get_one_goal(goal_id):
+    goal = validate_goal(goal_id)
+
+    return {"goal" : goal.to_dict()}, 200
+
+
+@goals_bp.route("/<goal_id>", methods=["PUT"])
+def update_goal(goal_id):
+    goal = validate_goal(goal_id)
+
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+   
+
+    db.session.commit()
+
+    response = {"goal": goal.to_dict()}
+    
+
+  
+    return make_response(jsonify(response)), 200
+
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_task(goal_id):
+    goal = validate_goal(goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return jsonify({"details": f'Goal {goal_id} "{goal.title}" successfully deleted'}), 200
