98cde09360e28cb8d16239a691de7029fe13323f
Tami <tamiadetayo@gmail.com>
2022-11-08 14:02:16 -0800

    Refactored read route to use sort query params

--- app/models/task.py
+++ app/models/task.py
@@ -2,6 +2,7 @@ from app import db
 
 
 class Task(db.Model):
+
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
@@ -9,17 +10,17 @@ class Task(db.Model):
 
     def to_dict(self):
         return {
-            "task_id": self.task_id,
+            "id": self.task_id,
             "title": self.title,
             "description": self.description,
             "is_complete": False
         }
     
     @classmethod
-    def from_dict(cls, task_dict):
-        return cls(
-            title=task_dict["title"],
-            description=task_dict["description"],
-            is_complete=False
+    def from_dict(cls, task_data):
+        new_task = Task(
+            title=task_data["title"],
+            description=task_data["description"],
         )
+        return new_task
 
--- app/routes.py
+++ app/routes.py
@@ -7,19 +7,35 @@ tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
-    new_task = Task.from_dict(request_body)
+
+    if request_body.get("title") and request_body.get("description"):
+        new_task = Task.from_dict(request_body)
+    else:
+        return jsonify({"details": "Invalid data"}), 400
 
     db.session.add(new_task)
     db.session.commit()
-
-    return make_response({"task":f"{new_task}"}, 201)
+    
+    response_dict = {
+        "id": new_task.task_id,
+        "title": new_task.title,
+        "description": new_task.description,
+        "is_complete": False
+    }
+    return jsonify({"task": response_dict}), 201
 
 @tasks_bp.route("", methods=["GET"])
 def read_all_tasks():
     tasks = Task.query.all()
+    
     tasks_response = []
     for task in tasks:
         tasks_response.append(task.to_dict())
+    
+    sort_query_value = request.args.get("sort")
+    tasks_response.sort(
+        key=lambda task: task["title"], reverse=sort_query_value == "desc")
+    
     return jsonify(tasks_response)
 
 def validate_task(task_id):
@@ -33,8 +49,28 @@ def validate_task(task_id):
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def read_one_task(task_id):
     chosen_task = validate_task(task_id)
+    return jsonify({"task": chosen_task.to_dict()})
 
-    return chosen_task.to_dict()
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    chosen_task = validate_task(task_id)
+    request_body = request.get_json()
 
-# @tasks.bp.route("/<task_id>", methods=["PUT"])
-# def 
+    try:
+        chosen_task.title = request_body["title"]
+        chosen_task.description = request_body["description"]
+    except KeyError:
+        return jsonify({"details": "Invalid data"}), 400
+    
+    db.session.commit()
+    
+    return jsonify({"task": chosen_task.to_dict()})
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    chosen_task = validate_task(task_id)
+    
+    db.session.delete(chosen_task)
+    db.session.commit()
+
+    return jsonify({"details": f"Task {chosen_task.task_id} \"Go on my daily walk 🏞\" successfully deleted"})
