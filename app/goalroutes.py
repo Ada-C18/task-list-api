a7e7650c446bb0833fe8716343dda4ba9912e763
Reyna Diaz <reynamdiazz@gmail.com>
2022-11-08 11:28:29 -0500

    "adds GET route + read_all_goals() funct"

--- app/goalroutes.py
+++ app/goalroutes.py
@@ -1,6 +1,6 @@
-from app.models.goal import Goal
 from app import db
-from flask import Blueprint, request, make_response
+from app.models.goal import Goal
+from flask import Blueprint, request, make_response, jsonify
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
@@ -17,3 +17,12 @@ def create_goal():
     db.session.commit()
 
     return {"goal":new_goal.to_dict()}, 201
+
+@goals_bp.route("", methods=["GET"])
+def read_all_goals():
+    goals = Goal.query.all()
+    goals_response = []
+    for goal in goals:
+        goals_response.append(goal.to_dict())
+    
+    return jsonify(goals_response)
