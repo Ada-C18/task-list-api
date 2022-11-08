aa685f6fecdbe88a3befb73ec7c36b2e6e80eb45
kimiahasani <kn.improve@gmail.com>
2022-11-07 19:57:43 -0800

    change goal.py and task.py

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,19 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+    # tasks = db.relationship("Tasks", back_populates="goals")
+
+    def to_dict(self):
+        # tasks_list = [task.to_dict()for task in self.tasks]
+        goal_dict = {
+            "id": self.id,
+            "title": self.title,
+        }
+        return goal_dict
+
+    # @classmethod
+    # def from_dict(cls, data_dict):
+    #     # CHECK data_dict has all valid bike attributes
+    #     if "title" in data_dict:
+    #         new_obj = cls(title=data_dict["title"])
+    #         return new_obj
--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,20 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": True if self.completed_at else False
+        }
+
+    # @classmethod
+    # def from_dict(cls, data_dict):
+    #     # CHECK data_dict has all valid bike attributes
+    #     if "title" in data_dict and "description" in data_dict and "completed_at" in data_dict:
+    #         new_obj = cls(title=data_dict["title"],
+    #                       description=data_dict["description"],
+    #                       completed_at=data_dict["completed_at"],)
+    #         return new_obj
