bc419c3ab7b1d70c1fda5810c54430cc2c052603
Ariel <gabriellaw15@gmail.com>
2022-11-08 13:59:35 -0800

    Resetting up migrations and fixing relationship between models goal and task

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,7 +5,7 @@ class Goal(db.Model):
     # __tablename__="goals"
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-    tasks = db.relationship("Task", backref="goal")
+    tasks = db.relationship("Task", back_populates="goal")
 
     def to_dict(self):
         return {
