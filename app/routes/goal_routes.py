f74b0786a9ca5fc1eca88941e7d89fefbb2e0491
Aviva <avivajoy.gars@gmail.com>
2022-11-08 07:54:21 -0700

    cleaned up the code a little

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -8,29 +8,30 @@ goal_bp = Blueprint('goal_bp', __name__, url_prefix='/goals')
 
 @goal_bp.route("", methods=["GET"])
 def get_all_goals():
+
     goals = Goal.query.all()
 
     goal_response = [{"id": goal.goal_id,"title": goal.title}for goal in goals]
 
     return jsonify(goal_response)
 
+
 @goal_bp.route("/<goal_id>", methods=["GET"])
-# GET /dog/id
 def get_one_goal(goal_id):
     
-    # Query our db to grab the dog that has the id we want:
     goal = Goal.query.get(goal_id)
     if not goal:
         return make_response({"details":"Id not found"}), 404
 
-    # Send back a single JSON object (dictionary):
     return make_response({ "goal":{
         "id": goal.goal_id,
         "title": goal.title,
     }},200)
 
+
 @goal_bp.route("", methods=["POST"])
 def create_goal():
+
     request_body = request.get_json()
 
     if "title" not in request_body:
@@ -39,25 +40,26 @@ def create_goal():
     new_goal = Goal(
         title=request_body["title"],
     )
-    # Add this new instance of goal to the database
+    
     db.session.add(new_goal)
     db.session.commit()
 
-    # Successful response
     return make_response({ "goal":{
         "id": new_goal.goal_id,
         "title": new_goal.title,
     }},201)
 
+
+
 @goal_bp.route("/<goal_id>", methods=["PUT"])
 def edit_goal(goal_id):
+
     goal = Goal.query.get(goal_id)
+
     request_body = request.get_json(goal_id)
     if not goal:
 
         return make_response({"details":"Id not found"}), 400
-
-
 
     goal.title = request_body["title"]
 
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -20,22 +20,19 @@ task_bp = Blueprint('task_bp', __name__, url_prefix='/tasks')
 def create_task():
     request_body = request.get_json()
 
-    # Feel free to add a guard clause
     if "title" not in request_body or "description" not in request_body:
         return make_response({"details":"Invalid data"}), 400
 
-    # How we know about Dog
     new_task = Task(
-        # You're looking for this key and assign it to name, breed, gender, age
+    
         title=request_body["title"],
         description=request_body["description"],
         #completed_at=request_body["completed_at"]
     )
-    # Add this new instance of dog to the database
+    
     db.session.add(new_task)
     db.session.commit()
 
-    # Successful response
     return ({ "task":{
     "id": new_task.task_id,
     "title": new_task.title,
@@ -43,9 +40,13 @@ def create_task():
     "is_complete": bool(new_task.completed_at)
     }},201)
 
+
+
 @task_bp.route("", methods=["GET"])
 def get_all_tasks():
+
     title_query = request.args.get("sort")
+
     if title_query == "asc":
         tasks = Task.query.order_by(Task.title.asc()).all()
     elif title_query == "desc":
@@ -58,19 +59,15 @@ def get_all_tasks():
     return jsonify(tasks_response), 200
 
 
-# Path/Endpoint to get a single dog
-# Include the id of the record to retrieve as a part of the endpoint
+
 @task_bp.route("/<task_id>", methods=["GET"])
-# GET /dog/id
 def get_one_task(task_id):
     
-    # Query our db to grab the dog that has the id we want:
     task = Task.query.get(task_id)
     if not task:
         return make_response({"details":"Id not found"}), 404
 
 
-    # Send back a single JSON object (dictionary):
     return { "task":{
         "id": task.task_id,
         "title": task.title,
@@ -81,7 +78,9 @@ def get_one_task(task_id):
 
 @task_bp.route("/<task_id>", methods=["PUT"])
 def edit_task(task_id):
+
     task = Task.query.get(task_id)
+
     if not task:
         return make_response({"details":"Id not found"}), 404
     request_body = request.get_json(task_id)
@@ -114,7 +113,9 @@ def delete_task(task_id):
 
 @task_bp.route("/<task_id>/<complete>", methods=["PATCH"])
 def patch_task_complete(task_id, complete):
+
     task = Task.query.get(task_id)
+    
     if not task:
         return make_response({"details":"Id not found"}), 404 
 
