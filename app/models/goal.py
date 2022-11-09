7d250ca8e96ca0aaf5defaf2ac053a4cda0233f1
Carina <checarina@gmail.com>
2022-11-09 14:48:28 -0800

    implemented POST route for goals

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,14 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
+
+    @classmethod
+    def from_dict(cls, goal_data):
+        new_goal = Goal(title = goal_data["title"])
+        return new_goal
+
+    def to_dict(self):
+        goal_dict = {}
+        goal_dict["id"] = self.goal_id
+        goal_dict["title"] = self.title
+        return goal_dict
--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -11,3 +11,34 @@ from datetime import datetime
 # load_dotenv()
 
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+    model = cls.query.get(model_id)
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+    return model
+
+#CREATE
+@goals_bp.route("", methods = ["POST"])
+def create_goal():
+    request_body = request.get_json()
+    
+    if "title" not in request_body:
+        return make_response(jsonify({"details": "Invalid data"}), 400)
+
+    new_goal = Goal.from_dict(request_body)
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return make_response(jsonify({"goal": new_goal.to_dict()}), 201)
+
+#READ
+
+#UPDATE
+
+#DELETE
