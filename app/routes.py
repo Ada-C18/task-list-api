c98b41b7d5a913c318db23dee1f635e50ccdadf3
ivystrayed <ivystrayed@gmail.com>
2022-11-09 13:07:03 -0800

    Added update and Delete functions and created the validate task helper function to eliminate redundancy

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,23 @@
 from app import db
 from .models.task import Task
-from flask import Blueprint, request, make_response, jsonify
+from flask import Blueprint, request, make_response, jsonify, abort
 
 
 task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message" : f"task id: {task_id} is invalid"}, 400))
+    
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message" : f"task {task_id} not found"}, 404))
+    
+    return task
 
 
 @task_bp.route("", methods=["GET"])
@@ -14,14 +28,7 @@ def get_all_task():
         response.append(task.to_dict())
     return jsonify(response), 200
 
-@task_bp.route("/<id>", methods=["GET"])
-def get_one_task(id):
-    try:
-        task = Task.query.get(id)
-        response = {"task" : task.to_dict()}
-    except:
-        return jsonify("Task Not Found"), 404
-    return jsonify(response), 200
+
     
 
 @task_bp.route("", methods=["POST"])
@@ -36,6 +43,33 @@ def add_task():
 
     return make_response({"task" : new_task.to_dict()}, 201)
 
+@task_bp.route("/<task_id>", methods=["GET"])
+def get_one_task(task_id): 
     
+    task = validate_task(task_id)
+    
+    return {"task" : task.to_dict()}, 200
+
+@task_bp.route ("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task(task_id)
+    
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    return {"task" : task.to_dict()}, 200
+
+@task_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+    
+    return make_response({"details" : f"Task {task_id} \"{task.title}\" successfully deleted"}, 200)
 
 
