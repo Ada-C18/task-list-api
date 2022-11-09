bbf61c7f4c9d953be37ab3aebdad50b49708a89e
Intesar Siraj <ghideruk@gmail.com>
2022-11-08 23:18:07 -0800

    wave 6 POST goals to tasks and GET tasks of goal _ work stillin progress

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -1,17 +1,18 @@
 from app import db
 from .models.goal import Goal
 from flask import Blueprint, jsonify, make_response, request, abort
+from app.routes import validate_tasks
 
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
 def validate_goals(goal_id):
     try:
-        goal_id = int(goal_id )
+        goal_id = int(goal_id)
     except:
         abort(make_response({"message":f"Goal {goal_id} invalid."}, 400))
 
-    goal = Goal.query.get(goal_id )
+    goal = Goal.query.get(goal_id)
 
     if not goal:
         abort(make_response({"message":f"Goal {goal_id} not found."}, 404))
@@ -76,6 +77,35 @@ def delete_goal(goal_id):
 
     return jsonify({"details": f"Goal {goal_id} \"{goal.title}\" successfully deleted"}), 200
 
+@goals_bp.route("/<goal_id>/tasks", methods=["POST"])
+def post_goal_belonging_to_tasks(goal_id):
+    goal = validate_goals(goal_id)
+
+    request_body = request.get_json()
+    task_list = []
+    
+    for task_id in request_body["task_ids"]:
+        task = validate_tasks(task_id)
+        task.goal = goal
+
+        task_list.append(task_id)
+
+    db.session.commit()
+
+    return jsonify({
+        "id": goal.goal_id,
+        "task_ids": task_list
+        }), 200
 
 
+@goals_bp.route("/<goal_id>/tasks", methods=["GET"])
+def get_tasks_of_goal(goal_id):
+    goal = validate_goals(goal_id)
+
+    tasks_dict = [task.to_dict() for task in goal.tasks]
+
+    result = goal.to_dict()
+    result["tasks"] = tasks_dict
+
+    return jsonify(result), 200
 
