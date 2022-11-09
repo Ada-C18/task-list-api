fc2a116eb5e74b9ba2124e71c7ae39a95ecd92f3
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2022-11-09 14:00:39 -0500

    Completed wave 5

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -1,126 +1,92 @@
 from app import db
 from flask import Blueprint, jsonify, abort, make_response, request
 import requests
-from app.models.task import Task
+from app.models.goal import Goal
 import datetime
 import os
 
-task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+goal_bp = Blueprint("goal", __name__, url_prefix="/goals")
 
-def validate_task(cls,task_id):
+def validate_goal(cls,id):
     try:
-        task_id=int(task_id)
+        id=int(id)
     except:
-        abort(make_response({"message": f"{cls.__name__} {task_id} invalid. Must be an integer"}, 400))
+        abort(make_response({"message": f"{cls.__name__} {id} invalid. Must be an integer"}, 400))
         
-    task = Task.query.get(task_id)
+    goal = Goal.query.get(id)
 
-    if not task:
-        abort(make_response({"message": f"{cls.__name__} {task_id} not found"}, 404))
+    if not goal:
+        abort(make_response({"message": f"{cls.__name__} {id} not found"}, 404))
 
-    return task
+    return goal
 
 
-@task_bp.route("/", strict_slashes=False, methods =["GET"])
-def read_all_task():
-    
+@goal_bp.route("/", strict_slashes=False, methods =["GET"])
+def read_all_goal():
     sort_by_title_query = request.args.get("sort")
-    tasks = ""
-    tasks_response = []
+    goals = ""
+    goals_response = []
     if sort_by_title_query:
         if sort_by_title_query == "asc":
-            tasks = Task.query.order_by(Task.title.asc()).all()
+            goals = Goal.query.order_by(Goal.title.asc()).all()
         elif sort_by_title_query == "desc":
-            tasks = Task.query.order_by(Task.title.desc()).all()
+            goals = Goal.query.order_by(Goal.title.desc()).all()
     else:
-        tasks = Task.query.all()
-    tasks_response = [task.to_dict() for task in tasks]
-        # for task in tasks:
-        #     tasks_response.append(task.to_dict())
+        goals = Goal.query.all()
+    goals_response = [goal.to_dict() for goal in goals]
+        # for goal in goals:
+        #     goals_response.append(goal.to_dict())
 
-    return jsonify(tasks_response)
+    return jsonify(goals_response)
 
 
-@task_bp.route("/<id>", strict_slashes=False, methods =["GET"])
-def read_one_task(id):
+@goal_bp.route("/<id>", strict_slashes=False, methods =["GET"])
+def read_one_goal(id):
     
-    task = validate_task(Task,id)
-    return {"task":task.to_dict()}, 200
+    goal = validate_goal(Goal,id)
+    return {"goal":goal.to_dict()}, 200
 
-@task_bp.route("/", strict_slashes=False, methods =["POST"])
-def create_task():
+@goal_bp.route("/", strict_slashes=False, methods =["POST"])
+def create_goal():
     try:
         request_body = request.get_json()
-        new_task = Task.from_dict(request_body)
+        new_goal = Goal.from_dict(request_body)
     except:
 
         return make_response(jsonify({"details": "Invalid data"}),400)
 
-    db.session.add(new_task)
+    db.session.add(new_goal)
     db.session.commit()
 
-    return {"task":new_task.to_dict()},201
+    return {"goal":new_goal.to_dict()},201
 
 
-@task_bp.route("/<id>", strict_slashes=False, methods =["PUT"])
-def update_task(id):
+@goal_bp.route("/<id>", strict_slashes=False, methods =["PUT"])
+def update_goal(id):
 
-    task = validate_task(Task,id)
+    goal = validate_goal(Goal,id)
     request_body = request.get_json()
 
     try:
-        task.title = request_body["title"]
-        task.description = request_body["description"]
+        goal.title = request_body["title"]
     except:
         return make_response(jsonify({'warning':'Enter both title and description or use patch method'}),400)
 
     db.session.commit()
-    return make_response(jsonify({"task":task.to_dict()}),200)
+    return make_response(jsonify({"goal":goal.to_dict()}),200)
 
 
-@task_bp.route("/<id>", strict_slashes=False, methods =["DELETE"])
-def delete_task(id):
+@goal_bp.route("/<id>", strict_slashes=False, methods =["DELETE"])
+def delete_goal(id):
 
-    task = validate_task(Task,id)
+    goal = validate_goal(Goal,id)
 
-    db.session.delete(task)
+    db.session.delete(goal)
     db.session.commit()
 
     response_body = {
-        "details": f"{Task.__name__} {task.id} \"{task.title}\" successfully deleted"
+        "details": f"{Goal.__name__} {goal.id} \"{goal.title}\" successfully deleted"
     }
 
     return make_response(jsonify(response_body),200)
-
-@task_bp.route("/<id>/mark_incomplete", strict_slashes=False, methods =["PATCH"])
-def update_incomplete(id):
-    task = validate_task(Task,id)
-    task.completed_at = None
-    
-
-    db.session.commit()
-
-    return make_response(jsonify({"task":task.to_dict()}),200)
-
-def slack_bot(message):
-    PATH = "https://slack.com/api/chat.postMessage"
-    SLACK_API_KEY = os.environ.get("API_KEY")
-
-    query_params = {
-        "channel":"list",
-        "text": message
-        }
-    requests.post(PATH, params=query_params, headers={"Authorization":f"Bearer {SLACK_API_KEY }"})
-
-
-@task_bp.route("/<id>/mark_complete", strict_slashes=False, methods =["PATCH"])
-def update_complete(id):
-    task = validate_task(Task,id)
-    task.completed_at = datetime.datetime.now()
-
-    db.session.commit()
-
-    slack_bot(f"Someone just completed the task {task.title}")
-
-    return make_response(jsonify({"task":task.to_dict()}),200)
 
--- app/models/goal.py
+++ app/models/goal.py
@@ -2,7 +2,17 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
-    # title= db.Column(db.String, nullable=False)
-    # description = db.Column(db.Integer, primary_key=True)
-    # completed_at = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title= db.Column(db.String, nullable=False)
+
+    @classmethod
+    def from_dict(cls,data_dict):
+        return cls(
+            title=data_dict["title"]
+            )
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            title=self.title
+        )
