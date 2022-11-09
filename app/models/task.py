2323d0e6e4180fa947e5fe022312b56d483a2693
Carmen Vega <cvega23@horizon.csueastbay.edu>
2022-11-09 13:53:32 -0800

    Added nullable=true in column goal_id and edited to_dict_task so it checks if there is a self.goal_id present to check if the task has a goal assigned to it to return dictionary with goal_id

--- app/models/task.py
+++ app/models/task.py
@@ -1,12 +1,11 @@
 from app import db
 
-
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
-    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"), nullable=True)
     goal = db.relationship("Goal", back_populates = "tasks")
 
     def to_dict_task(self):
@@ -14,12 +13,23 @@ class Task(db.Model):
             completed = True
         elif self.completed_at is None:
             completed = False
-        return {"task": {
-        "id":self.task_id, 
-        "title":self.title, 
-        "description": self.description,
-        "is_complete": completed
-        }}
+        if self.goal_id is None:
+
+            return {"task": {
+            "id":self.task_id, 
+            "title":self.title, 
+            "description": self.description,
+            "is_complete": completed
+            }}
+        else: #self.goal_id is True ,aka this task has a goal
+            return {"task": {
+            "id":self.task_id, 
+            "goal_id": self.goal_id,
+            "title":self.title, 
+            "description": self.description,
+            "is_complete": completed
+            }}
+
 
     def to_dict_all_tasks(self):
         return {
