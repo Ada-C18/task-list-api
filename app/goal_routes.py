eb866ca18cb8705d104ab1cc30913db39d18c228
Isabella <isabella.vanstedum@gmail.com>
2022-11-09 11:34:43 -0600

    refactored def read_tasks_for_a_goal(goal_id)

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -67,29 +67,8 @@ def give_tasks_to_a_goal(goal_id):
 @goal_bp.route("/<goal_id>/tasks", methods=["GET"])
 def read_tasks_for_a_goal(goal_id):
     goal = validate_model(Goal, goal_id)
-    tasks = Task.query.filter_by(goal_id = goal_id)
-    task_list = []
-    for task in tasks:
-        added_goal_id_to_dict = task.to_dict()
-        added_goal_id_to_dict["goal_id"] = task.goal_id
-        task_list.append(added_goal_id_to_dict)
-
-    #task_list = [task.to_dict() for task in tasks]
-    goal_complete_dict =  goal.to_dict()
-    goal_complete_dict["tasks"] = task_list
-    return goal_complete_dict
-    #tasks_data = task.to_dict() f
-    # id": 1,
-    #     "title": "Build a habit of going outside daily",
-    #     "tasks": [
-    #         {
-    #             "id": 1,
-    #             "goal_id": 1,
-    #             "title": "Go on my daily walk 🏞",
-    #             "description": "Notice something new every day",
-    #             "is_complete": False
-    #         }
-    #     ]
-    
-        
+    goal_dict = goal.to_dict()
+    get_one_task =[task.to_dict() for task in goal.tasks]
+    goal_dict["tasks"] = get_one_task
+    return goal_dict, 200
     
