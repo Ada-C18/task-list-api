134bbd7d2edfcac4c2b057007cfc6524200fc583
reneesu99 <reneesu22@gmail.com>
2022-11-09 10:09:07 -0800

    minor refactor

--- app/routes.py
+++ app/routes.py
@@ -12,11 +12,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+# Blueprints
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix = "/tasks")
 goals_bp = Blueprint("goals_bp", __name__, url_prefix = "/goals")
 
+# Helper Functions
+def validate_id(class_obj, id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"details":f"invalid {class_obj.__name__()}id"},400))
+    obj = class_obj.query.get(id)
+    if not obj:
+        abort(make_response({"details":f"{class_obj.__name__} not found"},404))
+    else:
+        return obj
 
+def mark_truthy_falsy(mark):
+    if mark == "mark_complete":
+        return True
+    elif mark == "mark_incomplete":
+        return False
+
+
+# Routes
+
+# Tasks Endpoint
 @tasks_bp.route("", methods=["POST", "GET"])
 def handle_tasks():
 
@@ -40,26 +61,17 @@ def handle_tasks():
             tasks = Task.query.order_by(Task.title.asc()).all()
         elif sort == "desc":
             tasks = Task.query.order_by(Task.title.desc()).all()
-            
+        
         else:
             tasks = Task.query.all()
         response_body = [task.to_dict() for task in tasks]
         return make_response(jsonify(response_body), 200)
 
-def validate_id(class_obj, id):
-    try:
-        id = int(id)
-    except:
-        abort(make_response({"details":f"invalid {class_obj.__name__()}id"},400))
-    obj = class_obj.query.get(id)
-    if not obj:
-        abort(make_response({"details":f"{class_obj.__name__} not found"},404))
-    else:
-        return obj
-
+# Individual Task Endpoints
 @tasks_bp.route("/<id>", methods=["GET","DELETE","PUT"])
 def handle_individual_task(id):
     task = validate_id(Task, id)
+    
     if request.method == "GET":
         return make_response({"task":task.to_dict()}, 200)
 
@@ -71,30 +83,23 @@ def handle_individual_task(id):
     elif request.method == "PUT":
         new_task = request.get_json()
         task.description = new_task["description"]
-        task.title= new_task["title"]
+        task.title = new_task["title"]
         db.session.commit()
-
         return make_response({"task":task.to_dict()}, 200)
-
-
-def mark_reducer(mark):
-    if mark == "mark_complete":
-        return True
-    elif mark == "mark_incomplete":
-        return False
 
 @tasks_bp.route("/<id>/<mark>", methods=["PATCH"])
 def mark_task(id, mark):
     task = validate_id(Task, id)
-    mark = mark_reducer(mark)
+    mark = mark_truthy_falsy(mark)
     if mark == True:
         if task.completed_at == None:
+            # connect to Slack API to send a message to the task-notifications channel
             SLACK_URL = "https://slack.com/api/chat.postMessage"
             PARAMS = {"channel":"task-notifications",
-                    "text":f"Someone just completed the task {task.to_dict()['title']}"
-                    }
+                     "text":f"Someone just completed the task {task.to_dict()['title']}"
+                     }
             HEADERS ={"Authorization": f"Bearer {os.environ.get('SLACK_BOT_TOKEN')}"}
-            r = requests.post(url = SLACK_URL, headers=HEADERS, params=PARAMS,)
+            requests.post(url = SLACK_URL, headers=HEADERS, params=PARAMS,)
         task.completed_at = date.today()
         db.session.commit()
         return make_response({"task":task.to_dict()},200)
@@ -103,7 +108,7 @@ def mark_task(id, mark):
         db.session.commit()
         return make_response({"task":task.to_dict()},200)
 
-
+# Goals Endpoints
 @goals_bp.route("", methods=["POST", "GET"])
 def handle_goals():
     if request.method == "POST":
@@ -114,7 +119,6 @@ def handle_goals():
         
         db.session.add(goal)
         db.session.commit()
-
         db.session.refresh(goal)
         goal = goal.to_dict()
         response = {"goal":goal}
@@ -137,14 +141,13 @@ def handle_individual_goal(id):
         return make_response({'details': f'Goal {goal.goal_id} "{goal.title}" successfully deleted'}, 200)
     
     elif request.method == "PUT":
-        new_goal = request.get_json()
-        goal.title= new_goal["title"]
+        goal.title = request.get_json()["title"]
         db.session.commit()
 
         return make_response({"goal":goal.to_dict()}, 200)
 
 @goals_bp.route("/<id>/tasks", methods=["POST", "GET"])
-def goals_tasks(id):
+def goals_and_tasks(id):
     goal = validate_id(Goal, id)
     if request.method == "POST":
         task_ids = request.get_json()["task_ids"]
