704fcc7d316ff2a12dafd55975888e27aa609bde
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2022-11-09 01:07:23 -0800

    Creates POST endpoint in routes.py, registers task bp in app/__init__.py, adds to_dict method to Task model

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,13 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        return dict(
+            task = dict(
+                id = self.id,
+                title = self.title,
+                description = self.description,
+                is_complete = False
+            )
+        )
