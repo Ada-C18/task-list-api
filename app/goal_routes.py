103770409e7e73863d1938e957eb5ac86fa1da46
mhc <mhchau0+ada@gmail.com>
2022-11-09 15:05:11 -0500

    implemented create_goal imported goals_bp into __init__ fixed validate_model import into goal_routes

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -3,6 +3,23 @@ from app import db
 from app.models.task import Task
 from app.models.goal import Goal
 from datetime import datetime
-from task_routes import validate_model
+from .task_routes import validate_model
 import requests
 import os
+
+
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+    
+    if (("title") not in request_body):
+        return make_response({"title": "Invalid data"}, 400)
+    
+    new_goal = Goal.from_dict(request_body)
+    db.session.add(new_goal)
+    db.session.commit()
+    
+    goal_response = Goal.query.get(1)
+    return make_response({"goal": goal_response.to_dict()}, 201)
--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,14 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.Text)
+    
+    @classmethod
+    def from_dict(cls, goal_data):
+        new_task = Goal(title=goal_data["title"])
+        return new_task
+    
+    def to_dict(self):
+        return {
+            "id": self.goal_id,
+            "title": self.title
+            }
