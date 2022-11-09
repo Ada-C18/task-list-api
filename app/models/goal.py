05fa46a34a0e03ba2eadf753de6645d6434c2348
Intesar Siraj <ghideruk@gmail.com>
2022-11-08 23:20:32 -0800

    made FK and relationship in Task and Goal classes

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,14 +4,14 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
+    tasks = db.relationship("Task", back_populates = "goal", lazy = True)
 
     def to_dict(self):
         return {
-            "goal":{
             "id": self.goal_id,
             "title": self.title
             }
-        }
+        
     def one_goal_in_dict(self):
         result = {}
         result["goal"] = self.to_dict()
