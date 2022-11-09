c7dd93958d1f0e6636ed37dc9759e446ebda3419
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-11-09 12:47:20 -0500

    Complete goal model

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,11 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String)
+    tasks = db.relationship("Task", backref="goal", lazy=True)
+    
+    def to_json(self):
+        return {
+            "id": self.goal_id,
+            "title": self.title,
+        }
--- app/models/task.py
+++ app/models/task.py
@@ -6,15 +6,17 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
-
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
+    
     def to_json(self):
         return {
             "id": self.task_id,
             "title": self.title,
             "description": self.description,
-            "is_complete": True if self.completed_at else False
+            "is_complete": True if self.completed_at else False,
+            "goal_id": self.goal_id if self.goal_id else None
         }
-
+        
     @classmethod
     def from_dict(cls, req_body):
         return cls(
