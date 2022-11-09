26d694bbd06e67d5b0b374b9ed002d0f397d95be
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2022-11-09 01:28:40 -0800

    Adds GET endpoints for task by ID and all tasks, includes validation by task ID and appropriate HTTP status codes

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,44 @@ def create_task():
     new_task = Task(
         title=request_body["title"],
         description=request_body["description"],
-        completed_at=request_body["completed at"])
+        completed_at=request_body["completed at"]
+        )
     db.session.add(new_task)
     db.session.commit()
-    return make_response(f"Task {new_task.title} has been successfully created", 201)
+    return make_response(f"Task '{new_task.title}' has been successfully created", 201)
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    title_query = request.args.get("title")
+    completed_at_query = request.args.get("completed at")
+    task_query = Task.query
+    if title_query:
+        task_query = Task.query.filter_by(title=title_query)
+    if completed_at_query:
+        task_query = Task.query.filter_by(completed_at=completed_at_query)
+
+    tasks = task_query.all()
+
+    tasks_response = [task.to_dict() for task in tasks]
+
+    return jsonify(tasks_response)
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def task_endpoint(task_id):
+    task = validate_task(task_id)
+
+    return jsonify(task.to_dict())
+
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message":f"Task {task_id} invalid"}, 400))
+    
+    task = Task.query.get(task_id)
+    
+    if not task:
+        abort(make_response({"message":f"Task {task_id} not found"}, 404))
+
+    return task
