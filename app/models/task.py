f6f3a1a9f8ec63207276789033df118e7615f10b
Jan li <bingox29@gmail.com>
2022-11-08 22:45:25 -0500

    finished 1-5 waves

--- app/models/task.py
+++ app/models/task.py
@@ -1,8 +1,18 @@
 from app import db
+from flask import abort, make_response
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.task_id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["is_complete"] = bool(self.completed_at)
+
+        return task_as_dict
