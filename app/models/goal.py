411d18fbfe3fd0714c7526c6a1a99d61ead1a6be
Carmen Vega <cvega23@horizon.csueastbay.edu>
2022-11-09 13:13:15 -0800

    Created new Goal class and two helper functions to return dictionaries

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,5 +1,20 @@
 from app import db
 
-
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    goal_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    title = db.Column(db.String)
+    tasks = db.relationship("Task", back_populates = "goal") # is a list
+
+    def to_dict_goal(self):
+        return {"goal": {
+        "id":self.goal_id, 
+        "title":self.title,
+        }}
+
+    def to_dict_all_goals(self):
+        return {
+        "id":self.goal_id, 
+        "title":self.title
+        }
+
+
