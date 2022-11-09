871f65f02d2b1d0d6c851b3766dd742dbb95785a
June Valentino <vintydesign@gmail.com>
2022-11-08 16:07:23 -0800

    added goal end points and updated goal model

---
+++ app/goal_routes.py
@@ -0,0 +1,91 @@
+from flask import abort, Blueprint, jsonify, make_response, request
+from app.models.goal import Goal
+from app.routes import validate_model
+from app import db
+
+
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    '''
+    POST method - allows client to post goals to the goals record
+    '''
+    request_body = request.get_json()
+
+    try:
+        new_goal = Goal.from_dict(request_body)
+    except:
+        return make_response({"details": "Invalid data"}, 400)
+    
+    db.session.add(new_goal)
+    db.session.commit()
+
+    response_body = {"task": new_goal.to_dict()}
+
+    return make_response(jsonify(response_body), 201)
+
+
+@goals_bp.route("", methods=["GET"])
+def view_all_goals():
+    '''
+    GET method - allows client to view all goals
+    allows client to sort goals alphabetically by title, in ascending and descending order
+    '''
+    sorted_query = request.args.get("sort")
+    if sorted_query:
+        if sorted_query == "asc":
+            all_goals = Goal.query.order_by(Goal.title.asc()).all()
+        
+        elif sorted_query == "desc":
+            all_goals = Goal.query.order_by(Goal.title.desc()).all()
+
+    else:
+        all_goals = Goal.query.all()
+
+    request_body = []
+    for goal in all_goals:
+        request_body.append(goal.to_dict())
+
+    return jsonify(request_body), 200
+
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def view_one_goal(goal_id):
+    '''
+    GET method - allows client to view one goal by ID
+    '''
+    goal = validate_model(Goal, goal_id)
+
+    return {"goal": goal.to_dict()}
+
+
+@goals_bp.route("/<goal_id>", methods=["PUT"])
+def update_one_goal(goal_id):
+    '''
+    PUT method - allows user to update one goal record
+    '''
+    goal = validate_model(Goal, goal_id)
+
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+    goal.description = request_body["description"]
+
+    db.session.commit()
+
+    return {"goal": goal.to_dict()}
+
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    '''
+    DELETE method - allows user to remove specified goal record
+    '''
+    goal = validate_model(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return {"details": f"Goal {goal.goal_id} \"{goal.title}\" successfully deleted"}
--- app/models/goal.py
+++ app/models/goal.py
@@ -2,4 +2,19 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    
+    
+    
+    def to_dict(self):
+        return {
+            "id": self.goal_id,
+            "title": self.title
+        }
+    
+    @classmethod
+    def from_dict(cls, goal_data):
+        new_goal = Goal(title=goal_data["title"])
+
+        return new_goal
