3de53435dd64aa0b036307422cc1fc7b4b74ea1b
Carmen Vega <cvega23@horizon.csueastbay.edu>
2022-11-09 13:12:33 -0800

    added relationship columns goal_id and goal and created new dictionary to return tasks associated with goal id, each seperated by dictionary

--- app/models/task.py
+++ app/models/task.py
@@ -6,8 +6,10 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
+    goal = db.relationship("Goal", back_populates = "tasks")
 
-    def to_dict(self):
+    def to_dict_task(self):
         if self.completed_at is not None:
             completed = True
         elif self.completed_at is None:
@@ -16,11 +18,20 @@ class Task(db.Model):
         "id":self.task_id, 
         "title":self.title, 
         "description": self.description,
-        "is_complete": completed}}
+        "is_complete": completed
+        }}
 
     def to_dict_all_tasks(self):
         return {
-        "id":self.task_id, 
+        "id":self.task_id,
+        "title":self.title, 
+        "description": self.description,
+        "is_complete": False}
+
+    def to_dict_all_tasks_with_goal(self):
+        return {
+        "id":self.task_id,
+        "goal_id": self.goal_id, 
         "title":self.title, 
         "description": self.description,
         "is_complete": False}
