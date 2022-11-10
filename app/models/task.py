2a9da81dda8930acc7dd19e3e2b4d1aefcbdd731
Carina <checarina@gmail.com>
2022-11-09 20:08:21 -0800

    implemented many-to-one relationship for tasks and goals

--- app/models/task.py
+++ app/models/task.py
@@ -2,10 +2,12 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True, default = None)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"), nullable = True)
+    goal = db.relationship("Goal", back_populates = "tasks")
 
     @classmethod
     def from_dict(cls, task_data):
