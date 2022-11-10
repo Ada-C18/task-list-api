ef24f61e463741b44e86eb3da5de6835ffe4955a
Tami <tamiadetayo@gmail.com>
2022-11-09 16:21:52 -0800

    Implemented CRUD routes for goal model

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,21 @@
 from flask import Blueprint, jsonify, abort, make_response, request
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from datetime import date
 import requests
 import os
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+def validate_model(cls, model_id):
+    model = cls.query.get(model_id)
+   
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+    
+    return model
 
 @tasks_bp.route("", methods=["POST"])
 def create_task():
@@ -25,6 +35,7 @@ def create_task():
         "description": new_task.description,
         "is_complete": False
     }
+
     return jsonify({"task": response_dict}), 201
 
 @tasks_bp.route("", methods=["GET"])
@@ -41,22 +52,15 @@ def read_all_tasks():
     
     return jsonify(tasks_response)
 
-def validate_task(task_id):
-    task = Task.query.get(task_id)
-   
-    if not task:
-        abort(make_response({"message":f"Task {task_id} not found"}, 404))
-    
-    return task
-
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def read_one_task(task_id):
-    chosen_task = validate_task(task_id)
+    chosen_task = validate_model(Task, task_id)
+
     return jsonify({"task": chosen_task.to_dict()})
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task(task_id):
-    chosen_task = validate_task(task_id)
+    chosen_task = validate_model(Task, task_id)
     request_body = request.get_json()
 
     try:
@@ -71,12 +75,12 @@ def update_task(task_id):
 
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
-    chosen_task = validate_task(task_id)
+    chosen_task = validate_model(Task, task_id)
     
     db.session.delete(chosen_task)
     db.session.commit()
 
-    return jsonify({"details": f"Task {chosen_task.task_id} \"Go on my daily walk 🏞\" successfully deleted"})
+    return jsonify({"details": f"Task {task_id} \"{chosen_task.title}\" successfully deleted"})
 
 def call_slack_api(text):
     url = "https://slack.com/api/chat.postMessage"
@@ -87,12 +91,13 @@ def call_slack_api(text):
         }
 
     response = requests.post(url, headers=headers, params=query_params)
+
     return response
 
 
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def mark_complete(task_id):
-    chosen_task = validate_task(task_id)
+    chosen_task = validate_model(Task, task_id)
     chosen_task.completed_at = date.today()
     call_slack_api(f"Someone just completed the task {chosen_task.title}")
 
@@ -105,12 +110,70 @@ def mark_complete(task_id):
 
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_incomplete(task_id):
-    chosen_task = validate_task(task_id)
+    chosen_task = validate_model(Task, task_id)
     chosen_task.completed_at = None
 
     db.session.commit()
 
     return jsonify({"task": chosen_task.to_dict()})
+
+
+# **************************** CRUD ROUTES FOR GOALS *****************************************
+
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+
+    if request_body:
+        new_goal = Goal.from_dict(request_body)
+    else:
+        return jsonify({"details": "Invalid data"}), 400
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return jsonify({"goal": new_goal.to_dict()}), 201
+
+@goals_bp.route("", methods=["GET"])
+def read_all_goals():
+    goals = Goal.query.all()
+
+    goals_response = []
+    for goal in goals:
+        goals_response.append(goal.to_dict())
+
+    return jsonify(goals_response), 200
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def read_one_goal(goal_id):
+    chosen_goal = validate_model(Goal, goal_id)
+
+    return jsonify({"goal": chosen_goal.to_dict()})
+
+@goals_bp.route("/<goal_id>", methods=["PUT"])
+def update_goal(goal_id):
+    chosen_goal = validate_model(Goal, goal_id)
+    request_body = request.get_json()
+
+    try:
+        chosen_goal.title = request_body["title"]
+    except KeyError:
+        return jsonify({"details": "Invalid data"}), 400
+
+    db.session.commit()
+
+    return jsonify({"goal": chosen_goal.to_dict()})
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    chosen_goal = validate_model(Goal, goal_id)
+
+    db.session.delete(chosen_goal)
+    db.session.commit()
+
+    return jsonify({"details": f"Goal {goal_id} \"{chosen_goal.title}\" successfully deleted"})
+
+
 
 
 
