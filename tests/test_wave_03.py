9d5593a30f8740df7a0bc710d11b963127493f3d
Sunny <1sunnymuniz@gmail.com>
2022-11-09 23:10:51 -0700

    cleaned up test files to remove comments

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -7,18 +7,6 @@ import pytest
 
 def test_mark_complete_on_incomplete_task(client, one_task):
     # Arrange
-    """
-    The future Wave 4 adds special functionality to this route,
-    so for this test, we need to set-up "mocking."
-
-    Mocking will help our tests work in isolation, which is a
-    good thing!
-
-    We need to mock any POST requests that may occur during this
-    test (due to Wave 4).
-
-    There is no action needed here, the tests should work as-is.
-    """
     with patch("requests.post") as mock_get:
         mock_get.return_value.status_code = 200
 
@@ -62,18 +50,7 @@ def test_mark_incomplete_on_complete_task(client, completed_task):
 
 def test_mark_complete_on_completed_task(client, completed_task):
     # Arrange
-    """
-    The future Wave 4 adds special functionality to this route,
-    so for this test, we need to set-up "mocking."
 
-    Mocking will help our tests work in isolation, which is a
-    good thing!
-
-    We need to mock any POST requests that may occur during this
-    test (due to Wave 4).
-
-    There is no action needed here, the tests should work as-is.
-    """
     with patch("requests.post") as mock_get:
         mock_get.return_value.status_code = 200
 
