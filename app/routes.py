f56b8ecfe0fc3bd1667c4ae665fc4be962240b6c
Sierra <sierra.r.jordan@gmail.com>
2022-11-10 02:32:59 -0800

    adds update_task

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,18 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort, make_response
 from . import db
 from .models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"msg": f"Task {task_id} invalid"}, 400))
+    task = Task.query.get(task_id)
+    if not task:
+        abort(make_response({"msg": f"Task {task_id} not found"}, 404))
+    return task
 
 @tasks_bp.route("", methods=['POST'])
 def add_task():
@@ -42,8 +52,26 @@ def get_tasks():
 
 @tasks_bp.route("/<task_id>", methods=['GET'])
 def get_one_task(task_id):
-    # add validate id
-    task = Task.query.get(task_id)
+    task = validate_task(task_id)
+
+    is_complete = False
+    if task.completed_at:
+        is_complete = True
+    return {"id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": is_complete}, 200
+
+@tasks_bp.route("/<task_id>", methods=['PUT'])
+def update_task(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+    try:
+        task.title = request_body["title"]
+        task.description = request_body["description"]
+    except:
+        raise KeyError("Either task title or description is missing from your input")
+    db.session.commit()
 
     is_complete = False
     if task.completed_at:
