8ebf4d507c02d148ebb3a573375f2bbe42135939
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-09 16:37:57 -0800

    added route delete_goal()

--- app/routes/goals.py
+++ app/routes/goals.py
@@ -37,7 +37,7 @@ def read_one_goal(goal_id):
     one_goal = validate_model(Goal, goal_id)
     return make_response({"goal": one_goal.to_dict()}, 200)
 
-@bp.route("<goal_id>", methods=["PUT"])
+@bp.route("/<goal_id>", methods=["PUT"])
 def update_goal(goal_id):
     goal = validate_model(Goal, goal_id)
     request_body = request.get_json()
@@ -47,3 +47,12 @@ def update_goal(goal_id):
     db.session.commit()
 
     return make_response({"goal": goal.to_dict()}, 200)
+
+@bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response({"details": f'Goal {goal_id} "{goal.title}" successfully deleted'}, 200)
