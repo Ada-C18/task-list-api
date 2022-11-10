cb273e5078fd845aa502dfe4655fa03ac27c0d06
Jessica <jessicaambrizmadrigal@gmail.com>
2022-11-09 19:30:17 -0800

    Modified the classes

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,7 +4,7 @@ from app import db
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-    # task = db.relationship('Task',backref='Goal')
+    task = db.relationship('Task', backref='Goal', lazy=True)
 
 
     def to_dict(self):
