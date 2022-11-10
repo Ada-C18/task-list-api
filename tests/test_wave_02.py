e56384bcf02a9ba137776dcf48049c4bed61f592
Yael <yaelechols@gmail.com>
2022-11-10 13:09:20 -0800

    Adds tests for GET tasks by task_id in asc or desc order

--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -54,3 +54,53 @@ def test_get_tasks_sorted_desc(client, three_tasks):
             "is_complete": False,
             "title": "Answer forgotten email 📧"},
     ]
+
+def test_get_tasks_sorted_id_asc(client, three_tasks):
+    response = client.get("/tasks?sort=asc&by=task_id")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "description": "",
+            "id": 1,
+            "is_complete": False,
+            "title": "Water the garden 🌷"},
+        {
+            "description": "",
+            "id": 2,
+            "is_complete": False,
+            "title": "Answer forgotten email 📧"},    
+        {
+            "description": "",
+            "id": 3,
+            "is_complete": False,
+            "title": "Pay my outstanding tickets 😭"}
+    ]
+
+def test_get_tasks_sorted_id_desc(client, three_tasks):
+    response = client.get("/tasks?sort=desc&by=task_id")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "description": "",
+            "id": 3,
+            "is_complete": False,
+            "title": "Pay my outstanding tickets 😭"},
+        {
+            "description": "",
+            "id": 2,
+            "is_complete": False,
+            "title": "Answer forgotten email 📧"},       
+        {
+            "description": "",
+            "id": 1,
+            "is_complete": False,
+            "title": "Water the garden 🌷"}
+    ]
