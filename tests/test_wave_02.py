cbc633c658c2759818e8d0c347a09a8cfbfc1075
Yael <yaelechols@gmail.com>
2022-11-10 15:13:19 -0800

    Adds tests for GET tasks by explicit title sort param in asc or desc order

--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -1,4 +1,5 @@
 from app.models.task import Task
+from app.models.goal import Goal
 import pytest
 
 
@@ -104,3 +105,59 @@ def test_get_tasks_sorted_id_desc(client, three_tasks):
             "is_complete": False,
             "title": "Water the garden 🌷"}
     ]
+
+def test_get_tasks_sorted_title_id_asc(client, three_tasks):
+    response = client.get("/tasks?sort=asc&by=title")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "id": 2,
+            "title": "Answer forgotten email 📧",
+            "description": "",
+            "is_complete": False},
+        {
+            "id": 3,
+            "title": "Pay my outstanding tickets 😭",
+            "description": "",
+            "is_complete": False},
+        {
+            "id": 1,
+            "title": "Water the garden 🌷",
+            "description": "",
+            "is_complete": False}
+    ]
+
+def test_get_tasks_sorted_title_id_desc(client, three_tasks):
+    response = client.get("/tasks?sort=desc&by=title")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "description": "",
+            "id": 1,
+            "is_complete": False,
+            "title": "Water the garden 🌷"},
+        {
+            "description": "",
+            "id": 3,
+            "is_complete": False,
+            "title": "Pay my outstanding tickets 😭"},
+        {
+            "description": "",
+            "id": 2,
+            "is_complete": False,
+            "title": "Answer forgotten email 📧"},
+    ]
+
+def test_get_goals_sorted_asc(client, three_goals):
+    pass
+
+def test_get_goals_sorted_desc(client, three_goals):
+    pass
