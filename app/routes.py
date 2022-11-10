abfec1f13ecf7bd888a4e609c9499f124c3d2c7e
Sierra <sierra.r.jordan@gmail.com>
2022-11-10 02:37:52 -0800

    adds delete_task

--- app/routes.py
+++ app/routes.py
@@ -80,3 +80,11 @@ def update_task(task_id):
             "title": task.title,
             "description": task.description,
             "is_complete": is_complete}, 200
+
+@tasks_bp.route("/<task_id>", methods=['DELETE'])
+def delete_task(task_id):
+    task = validate_task(task_id)
+    title = task.title
+    db.session.delete(task)
+    db.session.commit()
+    return make_response(f"Task {title} successfully deleted"), 200
