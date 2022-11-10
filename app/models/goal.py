5169e25cc25385e83abbe9dfa332be6458f06a67
ivystrayed <ivystrayed@gmail.com>
2022-11-09 17:43:32 -0800

    created Goal model, blueprint, goal_db and copied task methods. still need to DRY and finish up the conversion from task to goal

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,5 +1,37 @@
 from app import db
+from flask import abort, make_response
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+
+    def to_dict(self):
+        goal_dict = {
+            "id" : self.id,
+            "title" : self.title,
+            }
+        return goal_dict
+
+    @classmethod
+    def from_dict(cls, request_body):
+        try:
+            goal = Goal(title = request_body["title"]) 
+            return goal
+        except:
+            abort(make_response({"details" : "Invalid data"}, 400))
+
+    @classmethod
+    def validate_goal_id(cls, goal_id):
+        try:
+            goal_id = int(goal_id)
+        except:
+            abort(make_response({"message" : f"task id: {goal_id} is invalid"}, 400))
+    
+        goal = Goal.query.get(goal_id)
+
+        if not goal:
+            abort(make_response({"message" : f"task {goal_id} not found"}, 404))
+    
+        return goal
+        
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import db
 from .models.task import Task
+from .models.goal import Goal
 from flask import Blueprint, request, make_response, jsonify, abort
 import sqlalchemy 
-import datetime
+
 
 
 task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
@@ -69,15 +70,9 @@ def mark_task_status(task_id, marker):
     task = Task.validate_task_id(task_id)
     eval("task." + marker + "()")
     
-
     db.session.commit()
 
     return {"task" : task.to_dict()}, 200
-    
-
-    
-
-
 
     # Delete
 @task_bp.route("/<task_id>", methods=["DELETE"])
@@ -90,3 +85,82 @@ def delete_task(task_id):
     return make_response({"details" : f"Task {task_id} \"{task.title}\" successfully deleted"}, 200)
 
 
+goal_bp = Blueprint("goal_bp", __name__, url_prefix="/goals")
+
+# all goal methods
+    # Create
+@goal_bp.route("", methods=["POST"])
+def create_goal():
+    
+    request_body = request.get_json()
+    new_goal = Goal.from_dict(request_body)
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return make_response({"goal" : new_goal.to_dict()}, 201)
+    
+    # Read
+@goal_bp.route("", methods=["GET"])
+def get_all_goals():
+    
+    sort_query = request.args.get("sort")
+    if sort_query:
+        sort_function = getattr(sqlalchemy, sort_query)
+        goal_list = Goal.query.order_by(sort_function(Goal.title))
+    else:
+        goal_list = Goal.query.all()
+    
+    response = []    
+    for goal in goal_list:
+        response.append(goal.to_dict())
+    
+    return jsonify(response), 200  
+
+
+
+# Individual goal methods
+
+    # Read
+@goal_bp.route("/<goal_id>", methods=["GET"])
+def get_specific_goal(goal_id): 
+    
+    goal = Goal.validate_goal_id(goal_id)
+    
+    return {"goal" : goal.to_dict()}, 200
+@goal_bp.route("", methods = ["GET"])
+def get_any_goal():
+    goal = Goal.query.get(1)
+    return{"goal" : goal.to_dict()}, 200
+
+    # Update
+@goal_bp.route ("/<goal_id>", methods=["PUT"])
+def update_goal(goal_id):
+    goal = Goal.validate_task_id(goal_id)
+    
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+
+    db.session.commit()
+
+    return {"goal" : goal.to_dict()}, 200
+
+# @goal_bp.route("<goal_id>/<marker>", methods=["PATCH"])
+# def mark_goal_status(goal_id, marker):
+#     goal = Goal.validate_goal_id(goal_id)
+#     eval("goal." + marker + "()")
+    
+#     db.session.commit()
+
+#     return {"goal" : goal.to_dict()}, 200
+
+    # Delete
+# @goal_bp.route("/<goal_id>", methods=["DELETE"])
+# def delete_goal(goal_id):
+#     goal = Goal.validate_goal_id(goal_id)
+
+#     db.session.delete(goal)
+#     db.session.commit()
+    
+#     return make_response({"details" : f"Task {goal_id} \"{goal.title}\" successfully deleted"}, 200)
