62326424a11340dcc87f25e9e6c719bfdcf3522b
Tami <tamiadetayo@gmail.com>
2022-11-09 20:05:05 -0800

    Connected goal and task tables

--- app/models/task.py
+++ app/models/task.py
@@ -6,6 +6,8 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default=None)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
+    goals = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
         task_as_dict = {}
