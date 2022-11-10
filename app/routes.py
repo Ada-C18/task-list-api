c39c07f6feb7515788f092e03a07caeff9a30d20
Sierra <sierra.r.jordan@gmail.com>
2022-11-10 05:51:55 -0800

    adds goal model and endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, request, jsonify, abort, make_response
 from . import db
 from .models.task import Task
+from .models.goal import Goal
 from datetime import datetime
+
+
+"""
+task routes
+"""
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
@@ -130,3 +136,72 @@ def mark_incomplete(task_id):
             "title": task.title,
             "description": task.description,
             "is_complete": is_complete}}, 200
+
+"""
+goals routes
+"""
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+def validate_goal(goal_id):
+    try:
+        goal_id = int(goal_id)
+    except:
+        abort(make_response({"msg": f"Goal {goal_id} invalid"}, 400))
+    goal = Goal.query.get(goal_id)
+    if not goal:
+        abort(make_response({"msg": f"Goal {goal_id} not found"}, 404))
+    return goal
+
+@goals_bp.route("", methods=['POST'])
+def add_goal():
+    request_body = request.get_json()
+    if 'title' not in request_body:
+        abort(make_response({"details": "Invalid data"}, 400))
+
+    new_goal = Goal.from_dict(request_body)
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return {"goal": {
+        "id": new_goal.goal_id,
+        "title": new_goal.title,
+    }}, 201
+
+@goals_bp.route("", methods=['GET'])
+def get_goals():
+    goals = Goal.query.all()
+
+    response = []
+    for goal in goals:
+        response.append({"id": goal.goal_id,
+                        "title": goal.title})
+
+    return jsonify(response), 200
+
+@goals_bp.route("/<goal_id>", methods=['GET'])
+def get_one_goal(goal_id):
+    goal = validate_goal(goal_id)
+
+    return {"goal": {"id": goal.goal_id,
+            "title": goal.title}}, 200
+
+@goals_bp.route("/<goal_id>", methods=['PUT'])
+def update_goal(goal_id):
+    goal = validate_goal(goal_id)
+    request_body = request.get_json()
+    try:
+        goal.title = request_body["title"]
+    except:
+        raise KeyError("Goal title is missing from your input")
+    db.session.commit()
+
+    return {"goal": {"id": goal.goal_id,
+            "title": goal.title}}, 200
+
+@goals_bp.route("/<goal_id>", methods=['DELETE'])
+def delete_goal(goal_id):
+    goal = validate_goal(goal_id)
+    title = goal.title
+    db.session.delete(goal)
+    db.session.commit()
+    return {"details": f'Goal {goal_id} "{title}" successfully deleted'}, 200
