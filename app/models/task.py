b203c8df2842be97a2ec70c57b12769c42c717c3
mhc <mhchau0+ada@gmail.com>
2022-11-09 21:22:10 -0500

    added relationships to task and goal models 2/2 included goal_id, lazy=True, nullable=True

--- app/models/task.py
+++ app/models/task.py
@@ -6,6 +6,7 @@ class Task(db.Model):
     title = db.Column(db.Text)
     description = db.Column(db.Text)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
     goal = db.relationship("Goal", back_populates="task")
     
     @classmethod
