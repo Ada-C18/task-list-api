da0f53d4e0bd1e463debf0806c699c8ec7053a4a
Diana Soriano <dssori@gmail.com>
2022-11-11 18:23:28 -0500

    correct class

--- app/models/task.py
+++ app/models/task.py
@@ -5,12 +5,12 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(80), index = True, unique = False) 
     description  = db.Column(db.String(80), index = True, unique = False) 
-    is_complete = db.Column(db.DateTime, index = True, unique = False, nullable = True)
+    completed_at = db.Column(db.DateTime, index = True, unique = False, nullable = True)
     
     def build_task_dict(self):
             return {
                 "id": self.id,
                 "title": self.name,
                 "description": self.description,
-                "is_complete": self.is_complete
+                "completed_at": self.completed_at
             }
