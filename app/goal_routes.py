7734813e16f704c3e42d7f49d9e7befc045e13ad
Paje <ejap99@gmail.com>
2022-11-12 15:52:43 -0600

    Completed

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -10,43 +10,37 @@ from app import db
 
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
+
 @goals_bp.route("", methods=["GET"])
 def retrieve_goals():
     goals = Goal.query.all()
     goals_response = []
 
     for goal in goals:
-        goals_response.append({
-            "id": goal.goal_id,
-            "title": goal.title
-        })
+        goals_response.append(goal.format_goal())
     return jsonify(goals_response)
+
 
 @goals_bp.route("", methods=["POST"])
 def create_goal():
     request_body = request.get_json()
-    
+
     if not request_body.get("title"):
         return {"details": "Invalid data"}, 400
 
-    new_goal = Goal(title = request_body["title"])
+    new_goal = Goal(title=request_body["title"])
     db.session.add(new_goal)
     db.session.commit()
 
-    return { "goal": {
-        "id": new_goal.goal_id,
-        "title": new_goal.title
-        }}, 201
+    return {"goal": {"id": new_goal.goal_id, "title": new_goal.title}}, 201
+
 
 @goals_bp.route("/<goal_id>", methods=["GET"])
 def handle_goal(goal_id):
     goal = validate_model_id(Goal, goal_id)
 
-    return {"goal": {
-        "id": goal.goal_id,
-        "title": goal.title,
-        }
-    }
+    return {"goal": goal.format_goal()}
+
 
 @goals_bp.route("/<goal_id>", methods=["PUT"])
 def update_goal(goal_id):
@@ -57,11 +51,8 @@ def update_goal(goal_id):
     goal.title = request_body["title"]
     db.session.commit()
 
-    return make_response({
-        "goal": {
-        "id": goal.goal_id,
-        "title": goal.title
-        }})
+    return make_response({"goal": {"id": goal.goal_id, "title": goal.title}})
+
 
 @goals_bp.route("/<goal_id>", methods=["DELETE"])
 def delete_goal(goal_id):
@@ -70,9 +61,10 @@ def delete_goal(goal_id):
     db.session.delete(goal)
     db.session.commit()
 
-    return make_response({
-        "details": f"Goal {goal_id} \"{goal.title}\" successfully deleted"
-        })
+    return make_response(
+        {"details": f'Goal {goal_id} "{goal.title}" successfully deleted'}
+    )
+
 
 @goals_bp.route("/<goal_id>/tasks", methods=["GET"])
 def add_task_to_goal(goal_id):
@@ -80,15 +72,10 @@ def add_task_to_goal(goal_id):
     tasks_response = []
     for task in Goal.query.get(goal_id).tasks:
         task.goal_id = goal.goal_id
-        tasks_response.append({
-                "id": task.task_id,
-                "goal_id": goal.goal_id,
-                "title": task.title,
-                "description": task.description,
-                "is_complete": (task.completed_at == True)
-            })
+        tasks_response.append(task.format_task_goal())
 
     return {"id": goal.goal_id, "title": goal.title, "tasks": tasks_response}
+
 
 @goals_bp.route("/<goal_id>/tasks", methods=["POST"])
 def retrieve_tasks_from_goal(goal_id):
