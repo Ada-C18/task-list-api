f376660d48752ca42b5f7de8ae2856ea7112286a
Diana Soriano <dssori@gmail.com>
2022-11-21 15:02:26 -0500

    updated read_one and read_all

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,7 +4,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80)) 
-    task_rel = db.relationship("Task", back_populates="task",lazy='True')
+    task_rel = db.relationship("Task", backref="task",lazy='True')
     task_id = db.Column(db.Integer, db.ForeignKey('task.id')) 
 
     def goal_dict(self):
