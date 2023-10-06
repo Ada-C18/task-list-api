b271dda2d7fb38591ce78134a30f54a6afc05f94
chemtecher <ammanier2@gmail.com>
2023-10-06 14:32:32 -0400

    added nullable=False to task_id, title, and description and removed nullable=True from completed_at

--- app/models/task.py
+++ app/models/task.py
@@ -3,10 +3,10 @@ from flask import abort, make_response
 
 #Child
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
-    description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime, nullable=True)
-    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
     goal = db.relationship("Goal", back_populates="tasks")
 
