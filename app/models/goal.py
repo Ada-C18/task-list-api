75a5ed971d3998d534f24673e5760ece775b3ad7
chemtecher <ammanier2@gmail.com>
2023-10-06 11:42:44 -0400

    added nullable=False to goal column to not allow storing a goal with an empty title

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,8 +3,6 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 #Parent 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     title = db.Column(db.String)
     tasks = db.relationship("Task", back_populates="goal", lazy = True)
-
-    
